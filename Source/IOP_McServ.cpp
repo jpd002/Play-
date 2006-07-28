63ef23cca2dd9641cc7d32df85d455382958603d
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2006-07-28 03:34:44 +0000

    git-svn-id: http://svn.purei.org/purei/trunk@70 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/IOP_McServ.cpp
+++ Source/IOP_McServ.cpp
@@ -1,10 +1,20 @@
 #include <assert.h>
 #include <stdio.h>
+#include <boost/filesystem/operations.hpp>
+#include "Config.h"
 #include "IOP_McServ.h"
 #include "PS2VM.h"
 
 using namespace IOP;
 using namespace Framework;
+using namespace boost;
+using namespace std;
+
+const char* CMcServ::m_sMcPathPreference[2] =
+{
+	"ps2.mc0.directory",
+	"ps2.mc1.directory",
+};
 
 CMcServ::CMcServ()
 {
@@ -22,6 +32,9 @@ void CMcServ::Invoke(uint32 nMethod, void* pArgs, uint32 nArgsSize, void* pRet, 
 	{
 	case 0x01:
 		GetInfo(pArgs, nArgsSize, pRet, nRetSize);
+		break;
+	case 0x0D:
+		GetDir(pArgs, nArgsSize, pRet, nRetSize);
 		break;
 	case 0xFE:
 		//Get version?
@@ -66,7 +79,7 @@ void CMcServ::GetInfo(void* pArgs, uint32 nArgsSize, void* pRet, uint32 nRetSize
 
 	if(nWantType)
 	{
-		pRetBuffer[0x00] = 1;
+		pRetBuffer[0x00] = 2;
 	}
 	if(nWantFreeSpace)
 	{
@@ -78,6 +91,46 @@ void CMcServ::GetInfo(void* pArgs, uint32 nArgsSize, void* pRet, uint32 nRetSize
 	}
 
 	((uint32*)pRet)[0] = 0;
+}
+
+void CMcServ::GetDir(void* pArgs, uint32 nArgsSize, void* pRet, uint32 nRetSize)
+{
+	struct CMD
+	{
+		uint32	nPort;
+		uint32	nSlot;
+		uint32	nFlags;
+		uint32	nMaxEntries;
+		uint32	nTableAddress;
+		char	sName[0x400];
+	};
+
+	uint32 nRet;
+
+	nRet = 0;
+
+	assert(sizeof(CMD) == 0x414);
+	assert(nArgsSize >= 0x414);
+
+	CMD* pCmd;
+	pCmd = reinterpret_cast<CMD*>(pArgs);
+
+	Log("GetDir(nPort = %i, nSlot = %i, nFlags = %i, nMaxEntries = %i, nTableAddress = 0x%0.8X, sName = %s);\r\n",
+		pCmd->nPort, pCmd->nSlot, pCmd->nFlags, pCmd->nMaxEntries, pCmd->nTableAddress, pCmd->sName);
+
+	assert(pCmd->nPort < 2);
+	pCmd->nPort &= 1;
+
+	filesystem::path McPath(CConfig::GetInstance()->GetPreferenceString(m_sMcPathPreference[pCmd->nPort]), filesystem::native);
+	McPath = filesystem::complete(McPath);
+
+	if(filesystem::exists(McPath))
+	{
+		CPathFinder PathFinder(McPath, reinterpret_cast<CPathFinder::ENTRY*>(&CPS2VM::m_pRAM[pCmd->nTableAddress]), pCmd->nMaxEntries, pCmd->sName);
+		nRet = PathFinder.Search();
+	}
+
+	((uint32*)pRet)[0] = nRet;
 }
 
 void CMcServ::GetVersionInformation(void* pArgs, uint32 nArgsSize, void* pRet, uint32 nRetSize)
@@ -105,4 +158,106 @@ void CMcServ::Log(const char* sFormat, ...)
 	va_end(Args);
 
 #endif
+}
+
+
+CMcServ::CPathFinder::CPathFinder(const filesystem::path& BasePath, ENTRY* pEntry, unsigned int nMax, const char* sFilter)
+{
+	m_BasePath	= BasePath;
+	m_pEntry	= pEntry;
+	m_nIndex	= 0;
+	m_nMax		= nMax;
+	m_sFilter	= sFilter;
+}
+
+CMcServ::CPathFinder::~CPathFinder()
+{
+	
+}
+
+unsigned int CMcServ::CPathFinder::Search()
+{
+	SearchRecurse(m_BasePath);
+	return m_nIndex;
+}
+
+void CMcServ::CPathFinder::SearchRecurse(const filesystem::path& Path)
+{
+	filesystem::directory_iterator itEnd;
+
+	for(filesystem::directory_iterator itElement(Path);
+		itElement != itEnd;
+		itElement++)
+	{
+		if(filesystem::is_directory(*itElement))
+		{
+			//Humm, can this thing return directories too??
+			SearchRecurse(*itElement);
+		}
+		else
+		{
+			string sRelativePath((*itElement).string());
+
+			//"Extract" a more appropriate relative path from the memory card point of view
+			sRelativePath.erase(0, m_BasePath.string().size());
+
+			//Attempt to match this against the filter
+			if(MatchesFilter(sRelativePath.c_str()))
+			{
+				//This fits... fill in the information
+
+				ENTRY* pEntry;
+				pEntry = &m_pEntry[m_nIndex];
+
+				pEntry->nSize = static_cast<uint32>(filesystem::file_size(*itElement));
+
+				strncpy(reinterpret_cast<char*>(pEntry->sName), sRelativePath.c_str(), 0x1F);
+				pEntry->sName[0x1F] = 0;
+
+				m_nIndex++;
+			}
+		}
+	}
+}
+
+//Based on an algorithm found on http://xoomer.alice.it/acantato/dev/wildcard/wildmatch.html
+bool CMcServ::CPathFinder::MatchesFilter(const char* sPath)
+{
+	const char* sPattern;
+	const char* s;
+	const char* p;
+	bool nStar;
+
+	sPattern = m_sFilter.c_str();
+	nStar = false;
+
+_loopStart:
+
+	for(s = sPath, p = sPattern; *s; s++, p++)
+	{
+		switch(*p)
+		{
+		case '*':
+			nStar = true;
+			sPath = s;
+			sPattern = p;
+			if((*++p) == 0) return true;
+			goto _loopStart;
+			break;
+		default:
+			if(toupper(*s) != toupper(*p))
+			{
+				goto _starCheck;
+			}
+			break;
+		}
+	}
+
+	if(*p == '*') p++;
+	return (*p) == 0;
+
+_starCheck:
+	if(!nStar) return false;
+	sPath++;
+	goto _loopStart;
 }
--- Source/IOP_McServ.h
+++ Source/IOP_McServ.h
@@ -2,6 +2,8 @@
 #define _IOP_MCSERV_H_
 
 #include "IOP_Module.h"
+#include <string>
+#include <boost/filesystem/path.hpp>
 
 namespace IOP
 {
@@ -9,11 +11,11 @@ namespace IOP
 	class CMcServ : public CModule
 	{
 	public:
-						CMcServ();
-		virtual			~CMcServ();
-		virtual void	Invoke(uint32, void*, uint32, void*, uint32);
-		virtual void	SaveState(Framework::CStream*);
-		virtual void	LoadState(Framework::CStream*);
+							CMcServ();
+		virtual				~CMcServ();
+		virtual void		Invoke(uint32, void*, uint32, void*, uint32);
+		virtual void		SaveState(Framework::CStream*);
+		virtual void		LoadState(Framework::CStream*);
 
 		enum MODULE_ID
 		{
@@ -21,9 +23,53 @@ namespace IOP
 		};
 
 	private:
-		void			GetInfo(void*, uint32, void*, uint32);
-		void			GetVersionInformation(void*, uint32, void*, uint32);
-		void			Log(const char*, ...);
+		void				GetInfo(void*, uint32, void*, uint32);
+		void				GetDir(void*, uint32, void*, uint32);
+		void				GetVersionInformation(void*, uint32, void*, uint32);
+		void				Log(const char*, ...);
+
+		static const char*	m_sMcPathPreference[2];
+
+		class CPathFinder
+		{
+		public:
+			struct ENTRY
+			{
+				struct TIME
+				{
+					uint8	nUnknown;
+					uint8	nSecond;
+					uint8	nMinute;
+					uint8	nHour;
+					uint8	nDay;
+					uint8	nMonth;
+					uint16	nYear;
+				};
+
+				TIME	CreationTime;
+				TIME	ModificationTime;
+				uint32	nSize;
+				uint16	nAttributes;
+				uint16	nReserved0;
+				uint32	nReserved1[2];
+				uint8	sName[0x20];
+			};
+										CPathFinder(const boost::filesystem::path&, ENTRY*, unsigned int, const char*);
+										~CPathFinder();
+
+			unsigned int				Search();
+
+		private:
+			void						SearchRecurse(const boost::filesystem::path&);
+			bool						MatchesFilter(const char*);
+
+			ENTRY*						m_pEntry;
+			boost::filesystem::path		m_BasePath;
+			std::string					m_sFilter;
+			unsigned int				m_nIndex;
+			unsigned int				m_nMax;
+
+		};
 	};
 
 }
