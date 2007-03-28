00bd1c1978456ef216d851d7c9601f6a0686db5a
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2007-03-28 00:43:39 +0000

    git-svn-id: http://svn.purei.org/purei/trunk@137 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/MipsAssemblerDefinitions.cpp
+++ Source/MipsAssemblerDefinitions.cpp
@@ -45,6 +45,41 @@ namespace MipsAssemblerDefinitions
         AssemblerFunctionType m_Assembler;
     };
 
+    //RdRsRt Parser
+    //-----------------------------
+    struct RdRsRt
+    {
+        typedef void (CMIPSAssembler::*AssemblerFunctionType) (unsigned int, unsigned int, unsigned int);
+
+        RdRsRt(AssemblerFunctionType Assembler) :
+        m_Assembler(Assembler)
+        {
+            
+        }
+
+        void operator ()(tokenizer<>& Tokens, tokenizer<>::iterator& itToken, CMIPSAssembler* pAssembler)
+        {
+            unsigned int nRT, nRS, nRD;
+
+            if(itToken == Tokens.end()) throw exception();
+            nRD = CMIPSAssembler::GetRegisterIndex((*(++itToken)).c_str());
+
+            if(itToken == Tokens.end()) throw exception();
+            nRS = CMIPSAssembler::GetRegisterIndex((*(++itToken)).c_str());
+
+            if(itToken == Tokens.end()) throw exception();
+            nRT = CMIPSAssembler::GetRegisterIndex((*(++itToken)).c_str());
+
+            if(nRT == -1) throw exception();
+            if(nRS == -1) throw exception();
+            if(nRD == -1) throw exception();
+
+            bind(m_Assembler, pAssembler, _1, _2, _3)(nRD, nRS, nRT);
+        }
+
+        AssemblerFunctionType m_Assembler;
+    };
+
     //RtImm Parser
     //-----------------------------
     struct RtImm
@@ -134,6 +169,7 @@ namespace MipsAssemblerDefinitions
     SpecInstruction<RtRsImm>    Instruction_ADDIU   = SpecInstruction<RtRsImm>("ADDIU", RtRsImm(&CMIPSAssembler::ADDIU));
     SpecInstruction<RtImm>      Instruction_LUI     = SpecInstruction<RtImm>("LUI", RtImm(&CMIPSAssembler::LUI));
     SpecInstruction<RtRsSa>     Instruction_SLL     = SpecInstruction<RtRsSa>("SLL", RtRsSa(&CMIPSAssembler::SLL));
+    SpecInstruction<RdRsRt>     Instruction_SLTU    = SpecInstruction<RdRsRt>("SLTU", RdRsRt(&CMIPSAssembler::SLTU));
     SpecInstruction<RtRsSa>     Instruction_SRA     = SpecInstruction<RtRsSa>("SRA", RtRsSa(&CMIPSAssembler::SRA));
 
     Instruction* g_Instructions[] = 
@@ -141,6 +177,7 @@ namespace MipsAssemblerDefinitions
         &Instruction_ADDIU,
         &Instruction_LUI,
         &Instruction_SLL,
+        &Instruction_SLTU,
         &Instruction_SRA,
         NULL,
     };
