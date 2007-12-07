ea88aa0b2e03d7c036df7125f463cab0b47b0fc3
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2007-12-07 21:54:11 +0000

    MacOSX
    git-svn-id: http://svn.purei.org/purei/trunk@186 b36208d7-6611-0410-8bec-b1987f11c4a2

---
+++ Source/macosxui/main.mm
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+#import "../PS2VM.h"
+
+int main(int argc, char *argv[])
+{
+//	CPS2VM* virtualMachine = new CPS2VM();
+//	virtualMachine->Initialize();
+    return NSApplicationMain(argc,  (const char **) argv);
+}
