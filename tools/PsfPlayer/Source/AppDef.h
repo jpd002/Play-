428b5488b64324d292bb559f5f83170fda9bee87
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2009-02-12 04:18:48 +0000

    PsfPlayer: Updated version.
    git-svn-id: http://svn.purei.org/purei/trunk@479 b36208d7-6611-0410-8bec-b1987f11c4a2

--- tools/PsfPlayer/Source/AppDef.h
+++ tools/PsfPlayer/Source/AppDef.h
@@ -4,7 +4,7 @@
 //Don't put tabs here since it will screw up the installer package
 
 #define APP_NAME _T("PsfPlayer")
-#define APP_VERSION (24)
-#define APP_VERSIONSTR _T("0.24")
+#define APP_VERSION (25)
+#define APP_VERSIONSTR _T("0.25")
 
 #endif
