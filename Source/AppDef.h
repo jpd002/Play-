666bb407b6178fb1f66830d3b3a5fb2f52ed4901
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2009-04-21 00:12:53 +0000

    Starting version 0.26
    git-svn-id: http://svn.purei.org/purei/trunk@507 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/AppDef.h
+++ Source/AppDef.h
@@ -4,7 +4,7 @@
 //Don't put tabs here since it will screw up the installer package
 
 #define APP_NAME _T("Purei")
-#define APP_VERSION (25)
-#define APP_VERSIONSTR _T("0.25")
+#define APP_VERSION (26)
+#define APP_VERSIONSTR _T("0.26")
 
 #endif
