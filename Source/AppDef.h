8ed169d9af7d45f81af2bd32fd09b8c361568475
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2011-06-01 01:52:50 +0000

    Play! - Version 0.30
    git-svn-id: http://svn.purei.org/purei/trunk@768 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/AppDef.h
+++ Source/AppDef.h
@@ -4,7 +4,7 @@
 //Don't put tabs here since it will screw up the installer package
 
 #define APP_NAME _T("Play!")
-#define APP_VERSION (26)
-#define APP_VERSIONSTR _T("0.26")
+#define APP_VERSION (30)
+#define APP_VERSIONSTR _T("0.30")
 
 #endif
