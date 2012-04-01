2010f54acab51fc4732ff7b6d70cbefde18187bf
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-04-01 01:31:02 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@881 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/win32ui/ControllerSettingsWnd.cpp
+++ Source/win32ui/ControllerSettingsWnd.cpp
@@ -10,62 +10,57 @@
 #include "Types.h"
 #include "placeholder_def.h"
 
-#define CLSNAME     _T("ContollerSettingsWnd")
+#define CLSNAME		_T("ContollerSettingsWnd")
 #define WNDSTYLE	(WS_CAPTION | WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_SYSMENU)
 #define WNDSTYLEEX	(WS_EX_DLGMODALFRAME)
 
-using namespace std;
-using namespace std::tr1;
-using namespace PS2;
-using namespace Framework;
-
-CControllerSettingsWnd::CControllerSettingsWnd(HWND parent, DirectInput::CManager* directInputManager) :
-CModalWindow(parent),
-m_directInputManager(directInputManager),
-m_autoConfigButton(NULL),
-m_bindingList(NULL),
-m_samplingEnabled(true)
+CControllerSettingsWnd::CControllerSettingsWnd(HWND parent, DirectInput::CManager* directInputManager)
+: CModalWindow(parent)
+, m_directInputManager(directInputManager)
+, m_autoConfigButton(NULL)
+, m_bindingList(NULL)
+, m_samplingEnabled(true)
 {
-    if(!DoesWindowClassExist(CLSNAME))
-    {
-        WNDCLASSEX wc;
-        memset(&wc, 0, sizeof(WNDCLASSEX));
-        wc.cbSize			= sizeof(WNDCLASSEX);
-        wc.hCursor			= LoadCursor(NULL, IDC_ARROW);
-        wc.hbrBackground	= (HBRUSH)(COLOR_WINDOW); 
-        wc.hInstance		= GetModuleHandle(NULL);
-        wc.lpszClassName	= CLSNAME;
-        wc.lpfnWndProc		= CWindow::WndProc;
-        wc.style			= CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-        RegisterClassEx(&wc);
-    }
+	if(!DoesWindowClassExist(CLSNAME))
+	{
+		WNDCLASSEX wc;
+		memset(&wc, 0, sizeof(WNDCLASSEX));
+		wc.cbSize			= sizeof(WNDCLASSEX);
+		wc.hCursor			= LoadCursor(NULL, IDC_ARROW);
+		wc.hbrBackground	= (HBRUSH)(COLOR_WINDOW); 
+		wc.hInstance		= GetModuleHandle(NULL);
+		wc.lpszClassName	= CLSNAME;
+		wc.lpfnWndProc		= CWindow::WndProc;
+		wc.style			= CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+		RegisterClassEx(&wc);
+	}
 
-    Create(WNDSTYLEEX, CLSNAME, _T("Controller Settings"), WNDSTYLE, Win32::CRect(0, 0, 550, 400), parent, NULL);
+	Create(WNDSTYLEEX, CLSNAME, _T("Controller Settings"), WNDSTYLE, Framework::Win32::CRect(0, 0, 550, 400), parent, NULL);
 	SetClassPtr();
 
-    m_bindingList       = new Win32::CListView(m_hWnd, Win32::CRect(0, 0, 1, 1), LVS_REPORT | LVS_NOSORTHEADER);
-    m_ok                = new Win32::CButton(_T("OK"), m_hWnd, Win32::CRect(0, 0, 1, 1));
-    m_cancel            = new Win32::CButton(_T("Cancel"), m_hWnd, Win32::CRect(0, 0, 1, 1));
-    m_autoConfigButton  = new Win32::CButton(_T("Auto Config"), m_hWnd, Win32::CRect(0, 0, 1, 1));
+	m_bindingList		= new Framework::Win32::CListView(m_hWnd, Framework::Win32::CRect(0, 0, 1, 1), LVS_REPORT | LVS_NOSORTHEADER);
+	m_ok				= new Framework::Win32::CButton(_T("OK"), m_hWnd, Framework::Win32::CRect(0, 0, 1, 1));
+	m_cancel			= new Framework::Win32::CButton(_T("Cancel"), m_hWnd, Framework::Win32::CRect(0, 0, 1, 1));
+	m_autoConfigButton	= new Framework::Win32::CButton(_T("Auto Config"), m_hWnd, Framework::Win32::CRect(0, 0, 1, 1));
 
-    m_bindingList->SetExtendedListViewStyle(m_bindingList->GetExtendedListViewStyle() | LVS_EX_FULLROWSELECT);
+	m_bindingList->SetExtendedListViewStyle(m_bindingList->GetExtendedListViewStyle() | LVS_EX_FULLROWSELECT);
 
-    m_layout = 
-        VerticalLayoutContainer(
-            Win32::CLayoutWindow::CreateCustomBehavior(100, 100, 1, 1, m_bindingList) +
-            HorizontalLayoutContainer(
-                Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_autoConfigButton) +
-                CLayoutStretch::Create() +
-                Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_ok) +
-                Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_cancel)
-            )
-        );
+	m_layout = 
+		Framework::VerticalLayoutContainer(
+			Framework::Win32::CLayoutWindow::CreateCustomBehavior(100, 100, 1, 1, m_bindingList) +
+			Framework::HorizontalLayoutContainer(
+				Framework::Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_autoConfigButton) +
+				Framework::CLayoutStretch::Create() +
+				Framework::Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_ok) +
+				Framework::Win32::CLayoutWindow::CreateButtonBehavior(100, 23, m_cancel)
+			)
+		);
 
-    RefreshLayout();
-    PopulateList();
-    UpdateBindings();
+	RefreshLayout();
+	PopulateList();
+	UpdateBindings();
 
-    SetTimer(m_hWnd, NULL, 50, NULL);
+	SetTimer(m_hWnd, NULL, 50, NULL);
 }
 
 CControllerSettingsWnd::~CControllerSettingsWnd()
@@ -75,57 +70,57 @@ CControllerSettingsWnd::~CControllerSettingsWnd()
 
 void CControllerSettingsWnd::RefreshLayout()
 {
-    RECT rc = GetClientRect();
+	RECT rc = GetClientRect();
 
-    SetRect(&rc, rc.left + 10, rc.top + 10, rc.right - 10, rc.bottom - 10);
+	SetRect(&rc, rc.left + 10, rc.top + 10, rc.right - 10, rc.bottom - 10);
 
-    m_layout->SetRect(rc.left, rc.top, rc.right, rc.bottom);
-    m_layout->RefreshGeometry();
+	m_layout->SetRect(rc.left, rc.top, rc.right, rc.bottom);
+	m_layout->RefreshGeometry();
 
-    Redraw();
+	Redraw();
 }
 
 long CControllerSettingsWnd::OnTimer(WPARAM)
 {
-    if(m_samplingEnabled)
-    {
-        CInputConfig::InputEventHandler eventHandler(bind(&CControllerSettingsWnd::InputEventHandler, this, PLACEHOLDER_1, PLACEHOLDER_2));
-        m_directInputManager->ProcessEvents(
-            bind(&CInputConfig::TranslateInputEvent, &CInputConfig::GetInstance(), 
-            PLACEHOLDER_1, PLACEHOLDER_2, PLACEHOLDER_3, std::tr1::cref(eventHandler)));
-    }
-    return TRUE;
+	if(m_samplingEnabled)
+	{
+		CInputConfig::InputEventHandler eventHandler(bind(&CControllerSettingsWnd::InputEventHandler, this, PLACEHOLDER_1, PLACEHOLDER_2));
+		m_directInputManager->ProcessEvents(
+			std::bind(&CInputConfig::TranslateInputEvent, &CInputConfig::GetInstance(), 
+			std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::tr1::cref(eventHandler)));
+	}
+	return TRUE;
 }
 
 long CControllerSettingsWnd::OnCommand(unsigned short id, unsigned short cmd, HWND from)
 {
-    if(m_autoConfigButton && from == m_autoConfigButton->m_hWnd)
-    {
-        AutoConfigKeyboard();
-    }
-    if(m_ok && from == m_ok->m_hWnd)
-    {
-        CInputConfig::GetInstance().Save();
-        Destroy();
-    }
-    if(m_cancel && from == m_cancel->m_hWnd)
-    {
-        CInputConfig::GetInstance().Load();
-        Destroy();
-    }
-    return TRUE;
+	if(m_autoConfigButton && from == m_autoConfigButton->m_hWnd)
+	{
+		AutoConfigKeyboard();
+	}
+	if(m_ok && from == m_ok->m_hWnd)
+	{
+		CInputConfig::GetInstance().Save();
+		Destroy();
+	}
+	if(m_cancel && from == m_cancel->m_hWnd)
+	{
+		CInputConfig::GetInstance().Load();
+		Destroy();
+	}
+	return TRUE;
 }
 
 long CControllerSettingsWnd::OnNotify(WPARAM param, NMHDR* header)
 {
-    if(m_bindingList && m_bindingList->m_hWnd == header->hwndFrom)
-    {
-        if(header->code == NM_DBLCLK)
-        {
-            OnListItemDblClick();
-        }
-    }
-    return FALSE;
+	if(m_bindingList && m_bindingList->m_hWnd == header->hwndFrom)
+	{
+		if(header->code == NM_DBLCLK)
+		{
+			OnListItemDblClick();
+		}
+	}
+	return FALSE;
 }
 
 void CControllerSettingsWnd::AutoConfigKeyboard()
@@ -134,85 +129,85 @@ void CControllerSettingsWnd::AutoConfigKeyboard()
 	UpdateBindings();
 }
 
-void CControllerSettingsWnd::InputEventHandler(CControllerInfo::BUTTON button, uint32 value)
+void CControllerSettingsWnd::InputEventHandler(PS2::CControllerInfo::BUTTON button, uint32 value)
 {
-    UpdateButtonValue(button, value);
+	UpdateButtonValue(button, value);
 }
 
 void CControllerSettingsWnd::UpdateBindings()
 {
-    for(int i = 0; i < m_bindingList->GetItemCount(); i++)
-    {
-        CControllerInfo::BUTTON button = static_cast<CControllerInfo::BUTTON>(m_bindingList->GetItemData(i));
-        tstring description = CInputConfig::GetInstance().GetBindingDescription(m_directInputManager, button);
-        m_bindingList->SetItemText(i, 1, description.c_str());
-    }
+	for(int i = 0; i < m_bindingList->GetItemCount(); i++)
+	{
+		PS2::CControllerInfo::BUTTON button = static_cast<PS2::CControllerInfo::BUTTON>(m_bindingList->GetItemData(i));
+		std::tstring description = CInputConfig::GetInstance().GetBindingDescription(m_directInputManager, button);
+		m_bindingList->SetItemText(i, 1, description.c_str());
+	}
 }
 
-void CControllerSettingsWnd::UpdateButtonValue(CControllerInfo::BUTTON button, uint32 value)
+void CControllerSettingsWnd::UpdateButtonValue(PS2::CControllerInfo::BUTTON button, uint32 value)
 {
-    int listViewIndex = m_bindingList->FindItemData(button);
-    if(listViewIndex == -1) return;
-    if(CControllerInfo::IsAxis(button))
-    {
-        m_bindingList->SetItemText(listViewIndex, 2, boost::lexical_cast<tstring>(value).c_str());
-    }
-    else
-    {
-        m_bindingList->SetItemText(listViewIndex, 2, value ? _T("pressed") : _T(""));
-    }
+	int listViewIndex = m_bindingList->FindItemData(button);
+	if(listViewIndex == -1) return;
+	if(PS2::CControllerInfo::IsAxis(button))
+	{
+		m_bindingList->SetItemText(listViewIndex, 2, boost::lexical_cast<std::tstring>(value).c_str());
+	}
+	else
+	{
+		m_bindingList->SetItemText(listViewIndex, 2, value ? _T("pressed") : _T(""));
+	}
 }
 
 void CControllerSettingsWnd::OnListItemDblClick()
 {
-    int selection = m_bindingList->GetSelection();
-    if(selection == -1) return;
-    m_samplingEnabled = false;
-    {
-        CControllerInfo::BUTTON button = static_cast<CControllerInfo::BUTTON>(m_bindingList->GetItemData(selection));
-        if(button < CControllerInfo::MAX_BUTTONS)
-        {
-            CInputBindingSelectionWindow dialog(m_hWnd, m_directInputManager, button);
-            dialog.DoModal();
-            UpdateBindings();
-        }
-    }
-    m_samplingEnabled = true;
+	int selection = m_bindingList->GetSelection();
+	if(selection == -1) return;
+	m_samplingEnabled = false;
+	{
+		PS2::CControllerInfo::BUTTON button = static_cast<PS2::CControllerInfo::BUTTON>(m_bindingList->GetItemData(selection));
+		if(button < PS2::CControllerInfo::MAX_BUTTONS)
+		{
+			CInputBindingSelectionWindow dialog(m_hWnd, m_directInputManager, button);
+			dialog.DoModal();
+			UpdateBindings();
+		}
+	}
+	m_samplingEnabled = true;
 }
 
 void CControllerSettingsWnd::PopulateList()
 {
-    LVCOLUMN column;
+	LVCOLUMN column;
 
-    RECT rc = m_bindingList->GetClientRect();
+	RECT rc = m_bindingList->GetClientRect();
 
-    memset(&column, 0, sizeof(LVCOLUMN));
-    column.pszText  = _T("Button");
-    column.mask     = LVCF_TEXT | LVCF_WIDTH;
-    column.cx       = 1 * rc.right / 5;
-    m_bindingList->InsertColumn(0, &column);
+	memset(&column, 0, sizeof(LVCOLUMN));
+	column.pszText	= _T("Button");
+	column.mask		= LVCF_TEXT | LVCF_WIDTH;
+	column.cx		= 1 * rc.right / 5;
+	m_bindingList->InsertColumn(0, &column);
 
-    memset(&column, 0, sizeof(LVCOLUMN));
-    column.pszText  = _T("Binding");
-    column.mask     = LVCF_TEXT | LVCF_WIDTH;
-    column.cx       = 3 * rc.right / 5;
-    m_bindingList->InsertColumn(1, &column);
+	memset(&column, 0, sizeof(LVCOLUMN));
+	column.pszText	= _T("Binding");
+	column.mask		= LVCF_TEXT | LVCF_WIDTH;
+	column.cx		= 3 * rc.right / 5;
+	m_bindingList->InsertColumn(1, &column);
 
-    memset(&column, 0, sizeof(LVCOLUMN));
-    column.pszText  = _T("Current Value");
-    column.mask     = LVCF_TEXT | LVCF_WIDTH;
-    column.cx       = 1 * rc.right / 5;
-    m_bindingList->InsertColumn(2, &column);
+	memset(&column, 0, sizeof(LVCOLUMN));
+	column.pszText	= _T("Current Value");
+	column.mask		= LVCF_TEXT | LVCF_WIDTH;
+	column.cx		= 1 * rc.right / 5;
+	m_bindingList->InsertColumn(2, &column);
 
-    for(int i = CControllerInfo::MAX_BUTTONS - 1; i >= 0; i--)
-    {
-        tstring text = string_cast<tstring>(CControllerInfo::m_buttonName[i]);
-        LVITEM itm;
-        memset(&itm, 0, sizeof(LVITEM));
-        itm.mask		= LVIF_TEXT | LVIF_PARAM;
-        itm.pszText		= const_cast<TCHAR*>(text.c_str());
-        itm.lParam      = i;
-        m_bindingList->InsertItem(&itm);
-        UpdateButtonValue(static_cast<CControllerInfo::BUTTON>(i), 0);
-    }
+	for(int i = PS2::CControllerInfo::MAX_BUTTONS - 1; i >= 0; i--)
+	{
+		std::tstring text = string_cast<std::tstring>(PS2::CControllerInfo::m_buttonName[i]);
+		LVITEM itm;
+		memset(&itm, 0, sizeof(LVITEM));
+		itm.mask		= LVIF_TEXT | LVIF_PARAM;
+		itm.pszText		= const_cast<TCHAR*>(text.c_str());
+		itm.lParam		= i;
+		m_bindingList->InsertItem(&itm);
+		UpdateButtonValue(static_cast<PS2::CControllerInfo::BUTTON>(i), 0);
+	}
 }
