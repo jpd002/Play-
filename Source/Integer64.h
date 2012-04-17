c2aeae7914e72d74aa4d13cf433c074c385094f1
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-04-17 05:58:08 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@910 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/Integer64.h
+++ Source/Integer64.h
@@ -5,16 +5,16 @@
 
 struct INTEGER64
 {
-    union
-    {
-        uint64 q;
-        struct
-        {
-            uint32 d0;
-            uint32 d1;
-        };
-        uint32 d[2];
-    };
+	union
+	{
+		uint64 q;
+		struct
+		{
+			uint32 d0;
+			uint32 d1;
+		};
+		uint32 d[2];
+	};
 };
 
 #endif
