b710f9106e23b2f6aa9604e6f1e4ec5971c3ba1b
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-05-27 04:56:16 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@932 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/Ps2Const.h
+++ Source/Ps2Const.h
@@ -3,61 +3,61 @@
 
 namespace PS2
 {
-    enum EERAMSIZE
-    {
-	    EERAMSIZE = 0x02000000,
-    };
+	enum EERAMSIZE
+	{
+		EERAMSIZE = 0x02000000,
+	};
 
-    enum IOP_RAM_SIZE
-    {
-        IOP_RAM_SIZE = 0x00200000,
-    };
+	enum IOP_RAM_SIZE
+	{
+		IOP_RAM_SIZE = 0x00200000,
+	};
 
-    enum IOP_SCRATCH_SIZE
-    {
+	enum IOP_SCRATCH_SIZE
+	{
 		IOP_SCRATCH_SIZE = 0x00000400,
-    };
+	};
 
-    enum
-    {
-        IOP_CLOCK_FREQ = (44100 * 256 * 3),
-    };
+	enum
+	{
+		IOP_CLOCK_FREQ = (44100 * 256 * 3),
+	};
 
-    enum EEBIOSSIZE
-    {
-	    EEBIOSSIZE = 0x00400000,
-    };
+	enum EEBIOSSIZE
+	{
+		EEBIOSSIZE = 0x00400000,
+	};
 
-    enum SPRSIZE
-    {
-	    SPRSIZE = 0x00004000,
-    };
+	enum SPRSIZE
+	{
+		SPRSIZE = 0x00004000,
+	};
 
-    enum VUMEM0SIZE
-    {
+	enum VUMEM0SIZE
+	{
 		VUMEM0ADDR = 0x11004000,
-        VUMEM0SIZE = 0x00001000,
-    };
+		VUMEM0SIZE = 0x00001000,
+	};
 
-    enum MICROMEM0SIZE
-    {
-        MICROMEM0SIZE = 0x00001000,
-    };
+	enum MICROMEM0SIZE
+	{
+		MICROMEM0SIZE = 0x00001000,
+	};
 
-    enum VUMEM1SIZE
-    {
-	    VUMEM1SIZE = 0x00004000,
-    };
+	enum VUMEM1SIZE
+	{
+		VUMEM1SIZE = 0x00004000,
+	};
 
-    enum MICROMEM1SIZE
-    {
-	    MICROMEM1SIZE = 0x00004000,
-    };
+	enum MICROMEM1SIZE
+	{
+		MICROMEM1SIZE = 0x00004000,
+	};
 
-    enum
-    {
-        SPU_RAM_SIZE = 0x00200000,
-    };
+	enum
+	{
+		SPU_RAM_SIZE = 0x00200000,
+	};
 }
 
 #endif
