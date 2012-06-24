bd8ca82f5844da5a85c09f02883f653e47c2c2e6
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-06-24 23:10:12 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@954 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/iop/Iop_Thsema.cpp
+++ Source/iop/Iop_Thsema.cpp
@@ -2,17 +2,16 @@
 #include "IopBios.h"
 
 using namespace Iop;
-using namespace std;
 
-#define FUNCTION_CREATESEMAPHORE    "CreateSemaphore"
-#define FUNCTION_DELETESEMAPHORE    "DeleteSemaphore"
-#define FUNCTION_SIGNALSEMAPHORE    "SignalSemaphore"
-#define FUNCTION_ISIGNALSEMAPHORE   "iSignalSemaphore"
-#define FUNCTION_WAITSEMAPHORE      "WaitSemaphore"
+#define FUNCTION_CREATESEMAPHORE	"CreateSemaphore"
+#define FUNCTION_DELETESEMAPHORE	"DeleteSemaphore"
+#define FUNCTION_SIGNALSEMAPHORE	"SignalSemaphore"
+#define FUNCTION_ISIGNALSEMAPHORE	"iSignalSemaphore"
+#define FUNCTION_WAITSEMAPHORE		"WaitSemaphore"
 
-CThsema::CThsema(CIopBios& bios, uint8* ram) :
-m_bios(bios),
-m_ram(ram)
+CThsema::CThsema(CIopBios& bios, uint8* ram)
+: m_bios(bios)
+, m_ram(ram)
 {
 
 }
@@ -22,84 +21,84 @@ CThsema::~CThsema()
 
 }
 
-string CThsema::GetId() const
+std::string CThsema::GetId() const
 {
-    return "thsemap";
+	return "thsemap";
 }
 
-string CThsema::GetFunctionName(unsigned int functionId) const
+std::string CThsema::GetFunctionName(unsigned int functionId) const
 {
-    switch(functionId)
-    {
-    case 4:
-        return FUNCTION_CREATESEMAPHORE;
-        break;
-    case 5:
-        return FUNCTION_DELETESEMAPHORE;
-        break;
-    case 6:
-        return FUNCTION_SIGNALSEMAPHORE;
-        break;
-    case 7:
-        return FUNCTION_ISIGNALSEMAPHORE;
-        break;
-    case 8:
-        return FUNCTION_WAITSEMAPHORE; 
-        break;
-    default:
-    	return "unknown";
-        break;
-    }
+	switch(functionId)
+	{
+	case 4:
+		return FUNCTION_CREATESEMAPHORE;
+		break;
+	case 5:
+		return FUNCTION_DELETESEMAPHORE;
+		break;
+	case 6:
+		return FUNCTION_SIGNALSEMAPHORE;
+		break;
+	case 7:
+		return FUNCTION_ISIGNALSEMAPHORE;
+		break;
+	case 8:
+		return FUNCTION_WAITSEMAPHORE; 
+		break;
+	default:
+		return "unknown";
+		break;
+	}
 }
 
 void CThsema::Invoke(CMIPS& context, unsigned int functionId)
 {
-    switch(functionId)
-    {
-    case 4:
-        context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(CreateSemaphore(
-            reinterpret_cast<SEMAPHORE*>(&m_ram[context.m_State.nGPR[CMIPS::A0].nV0])
-            ));
-        break;
-    case 5:
-        context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(DeleteSemaphore(
-            context.m_State.nGPR[CMIPS::A0].nV0
-            ));
-        break;
-    case 6:
-        context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(SignalSemaphore(
-            context.m_State.nGPR[CMIPS::A0].nV0
-            ));
-        break;
-	case 7:
-        context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(iSignalSemaphore(
-            context.m_State.nGPR[CMIPS::A0].nV0
-            ));
+	switch(functionId)
+	{
+	case 4:
+		context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(CreateSemaphore(
+			reinterpret_cast<SEMAPHORE*>(&m_ram[context.m_State.nGPR[CMIPS::A0].nV0])
+			));
 		break;
-    case 8:
-        context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(WaitSemaphore(
-            context.m_State.nGPR[CMIPS::A0].nV0
-            ));
-        break;
-    default:
-        printf("%s(%0.8X): Unknown function (%d) called.\r\n", __FUNCTION__, context.m_State.nPC, functionId);
-        break;
-    }
+	case 5:
+		context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(DeleteSemaphore(
+			context.m_State.nGPR[CMIPS::A0].nV0
+			));
+		break;
+	case 6:
+		context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(SignalSemaphore(
+			context.m_State.nGPR[CMIPS::A0].nV0
+			));
+		break;
+	case 7:
+		context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(iSignalSemaphore(
+			context.m_State.nGPR[CMIPS::A0].nV0
+			));
+		break;
+	case 8:
+		context.m_State.nGPR[CMIPS::V0].nD0 = static_cast<int32>(WaitSemaphore(
+			context.m_State.nGPR[CMIPS::A0].nV0
+			));
+		break;
+	default:
+		printf("%s(%0.8X): Unknown function (%d) called.\r\n", __FUNCTION__, context.m_State.nPC, functionId);
+		break;
+	}
 }
 
 uint32 CThsema::CreateSemaphore(const SEMAPHORE* semaphore)
 {
-    return m_bios.CreateSemaphore(semaphore->initialCount, semaphore->maxCount);
+	return m_bios.CreateSemaphore(semaphore->initialCount, semaphore->maxCount);
 }
 
 uint32 CThsema::DeleteSemaphore(uint32 semaphoreId)
 {
-    return m_bios.DeleteSemaphore(semaphoreId);
+	return m_bios.DeleteSemaphore(semaphoreId);
 }
 
 uint32 CThsema::SignalSemaphore(uint32 semaphoreId)
 {
-    return m_bios.SignalSemaphore(semaphoreId, false);
+	return m_bios.SignalSemaphore(semaphoreId, false);
 }
 
 uint32 CThsema::iSignalSemaphore(uint32 semaphoreId)
@@ -109,5 +108,5 @@ uint32 CThsema::iSignalSemaphore(uint32 semaphoreId)
 
 uint32 CThsema::WaitSemaphore(uint32 semaphoreId)
 {
-    return m_bios.WaitSemaphore(semaphoreId);
+	return m_bios.WaitSemaphore(semaphoreId);
 }
--- Source/iop/Iop_Thsema.h
+++ Source/iop/Iop_Thsema.h
@@ -6,34 +6,34 @@
 
 namespace Iop
 {
-    class CThsema : public CModule
-    {
-    public:
-                        CThsema(CIopBios&, uint8*);
-        virtual         ~CThsema();
+	class CThsema : public CModule
+	{
+	public:
+						CThsema(CIopBios&, uint8*);
+		virtual			~CThsema();
 
-        std::string     GetId() const;
+		std::string		GetId() const;
 		std::string		GetFunctionName(unsigned int) const;
-        void            Invoke(CMIPS&, unsigned int);
+		void			Invoke(CMIPS&, unsigned int);
 
-    private:
-        struct SEMAPHORE
-        {
-            uint32      attributes;
-            uint32      options;
-            uint32      initialCount;
-            uint32      maxCount;
-        };
-
-        uint32          CreateSemaphore(const SEMAPHORE*);
-        uint32          DeleteSemaphore(uint32);
-        uint32          WaitSemaphore(uint32);
-        uint32          SignalSemaphore(uint32);
+	private:
+		struct SEMAPHORE
+		{
+			uint32		attributes;
+			uint32		options;
+			uint32		initialCount;
+			uint32		maxCount;
+		};
+		
+		uint32			CreateSemaphore(const SEMAPHORE*);
+		uint32			DeleteSemaphore(uint32);
+		uint32			WaitSemaphore(uint32);
+		uint32			SignalSemaphore(uint32);
 		uint32			iSignalSemaphore(uint32);
 
-        uint8*          m_ram;
-        CIopBios&       m_bios;
-    };
+		uint8*			m_ram;
+		CIopBios&		m_bios;
+	};
 }
 
 #endif
