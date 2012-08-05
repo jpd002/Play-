0c12127a18d9775c2cbd8da30ebaeb83bfc0218a
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-08-05 19:13:23 +0000

    Cleanup + compilation fixes for boost 1.50
    git-svn-id: http://svn.purei.org/purei/trunk@974 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/MailBox.cpp
+++ Source/MailBox.cpp
@@ -15,76 +15,76 @@ CMailBox::~CMailBox()
 
 bool CMailBox::IsPending() const
 {
-    return m_calls.size() != 0;
+	return m_calls.size() != 0;
 }
 
 void CMailBox::WaitForCall()
 {
 	boost::mutex::scoped_lock waitLock(m_waitMutex);
-    while(!IsPending())
-    {
-        m_waitCondition.wait(waitLock);
-    }
+	while(!IsPending())
+	{
+		m_waitCondition.wait(waitLock);
+	}
 }
 
 void CMailBox::WaitForCall(unsigned int timeOut)
 {
-    boost::mutex::scoped_lock waitLock(m_waitMutex);
-    if(IsPending()) return;
-    boost::xtime xt;
-    boost::xtime_get(&xt, boost::TIME_UTC);
-    xt.nsec += timeOut * 1000000;
-    m_waitCondition.timed_wait(waitLock, xt);
+	boost::mutex::scoped_lock waitLock(m_waitMutex);
+	if(IsPending()) return;
+	boost::xtime xt;
+	boost::xtime_get(&xt, boost::TIME_UTC_);
+	xt.nsec += timeOut * 1000000;
+	m_waitCondition.timed_wait(waitLock, xt);
 }
 
 void CMailBox::SendCall(const FunctionType& function, bool waitForCompletion)
 {
-    {
-        boost::mutex::scoped_lock waitLock(m_waitMutex);
+	{
+		boost::mutex::scoped_lock waitLock(m_waitMutex);
 		MESSAGE message;
 		message.function = function;
 		message.sync = waitForCompletion;
-        m_calls.push_back(message);
-        m_waitCondition.notify_all();
-    }
-    if(waitForCompletion)
-    {
-        boost::mutex::scoped_lock doneLock(m_doneNotifyMutex);
-        m_callDone = false;
+		m_calls.push_back(message);
+		m_waitCondition.notify_all();
+	}
+	if(waitForCompletion)
+	{
+		boost::mutex::scoped_lock doneLock(m_doneNotifyMutex);
+		m_callDone = false;
 		while(!m_callDone)
-        {
+		{
 #ifdef WIN32
-	        MSG wmmsg;
-            while(PeekMessage(&wmmsg, NULL, 0, 0, PM_REMOVE))
-		    {
-			    TranslateMessage(&wmmsg);
-			    DispatchMessage(&wmmsg);
-		    }
-            boost::xtime xt;
-            boost::xtime_get(&xt, boost::TIME_UTC);
-            xt.nsec += 100 * 1000000;
-            m_callFinished.timed_wait(doneLock, xt);
+			MSG wmmsg;
+			while(PeekMessage(&wmmsg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&wmmsg);
+				DispatchMessage(&wmmsg);
+			}
+			boost::xtime xt;
+			boost::xtime_get(&xt, boost::TIME_UTC_);
+			xt.nsec += 100 * 1000000;
+			m_callFinished.timed_wait(doneLock, xt);
 #else
-            m_callFinished.wait(doneLock);
+			m_callFinished.wait(doneLock);
 #endif
-        }
-    }
+		}
+	}
 }
 
 void CMailBox::ReceiveCall()
 {
-    MESSAGE message;
-    {
-        boost::mutex::scoped_lock waitLock(m_waitMutex);
-        if(!IsPending()) return;
-        message = *m_calls.begin();
-        m_calls.pop_front();
-    }
-    message.function();
+	MESSAGE message;
+	{
+		boost::mutex::scoped_lock waitLock(m_waitMutex);
+		if(!IsPending()) return;
+		message = *m_calls.begin();
+		m_calls.pop_front();
+	}
+	message.function();
 	if(message.sync)
-    {
-        boost::mutex::scoped_lock doneLock(m_doneNotifyMutex);
-        m_callDone = true;
-        m_callFinished.notify_all();
-    }
+	{
+		boost::mutex::scoped_lock doneLock(m_doneNotifyMutex);
+		m_callDone = true;
+		m_callFinished.notify_all();
+	}
 }
