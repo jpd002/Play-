92a891bcff1002aa31ba39312ca987b32b36e28a
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-09-26 02:21:37 +0000

    Added look_ctype_table and memcmp, plus some minor cleanup in SYSCLIB IOP module.
    git-svn-id: http://svn.purei.org/purei/trunk@1009 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/iop/Iop_Sysclib.h
+++ Source/iop/Iop_Sysclib.h
@@ -1,34 +1,36 @@
 #ifndef _IOP_SYSCLIB_H_
 #define _IOP_SYSCLIB_H_
 
-#include "IOP_Module.h"
-#include "IOP_Stdio.h"
+#include "Iop_Module.h"
+#include "Iop_Stdio.h"
 
 namespace Iop
 {
-    class CSysclib : public CModule
-    {
-    public:
-                        CSysclib(uint8*, CStdio&);
-        virtual         ~CSysclib();
+	class CSysclib : public CModule
+	{
+	public:
+						CSysclib(uint8*, CStdio&);
+		virtual			~CSysclib();
 
-        std::string     GetId() const;
+		std::string		GetId() const;
 		std::string		GetFunctionName(unsigned int) const;
-        void            Invoke(CMIPS&, unsigned int);
+		void			Invoke(CMIPS&, unsigned int);
 
-    private:
-        void            __memcpy(void*, const void*, unsigned int);
-        void            __memset(void*, int, unsigned int);
-        uint32          __sprintf(CMIPS& context);
-        uint32          __strlen(const char*);
-        uint32          __strcmp(const char*, const char*);
-        void            __strcpy(char*, const char*);
-        uint32          __strncmp(const char*, const char*, uint32);
-        void            __strncpy(char*, const char*, unsigned int);
-        uint32          __strtol(const char*, unsigned int);
-        uint8*          m_ram;
-        CStdio&         m_stdio;
-    };
+	private:
+		uint32			__look_ctype_table(uint32);
+		uint32			__memcmp(const void*, const void*, uint32);
+		void			__memcpy(void*, const void*, unsigned int);
+		void			__memset(void*, int, unsigned int);
+		uint32			__sprintf(CMIPS& context);
+		uint32			__strlen(const char*);
+		uint32			__strcmp(const char*, const char*);
+		void			__strcpy(char*, const char*);
+		uint32			__strncmp(const char*, const char*, uint32);
+		void			__strncpy(char*, const char*, unsigned int);
+		uint32			__strtol(const char*, unsigned int);
+		uint8*			m_ram;
+		CStdio&			m_stdio;
+	};
 }
 
 #endif
