9a93a24a9aed486926adaba2e62f8347d2f0d8ae
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-09-29 20:27:54 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@1011 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/MA_EE.h
+++ Source/MA_EE.h
@@ -7,17 +7,17 @@ class CMA_EE : public CMA_MIPSIV
 {
 public:
 										CMA_EE();
-	virtual                             ~CMA_EE();
+	virtual								~CMA_EE();
 
 protected:
 	typedef void (CMA_EE::*InstructionFuncConstant)();
 
 	void								SetupReflectionTables();
 
-	static InstructionFuncConstant      m_pOpMmi0[0x20];
-	static InstructionFuncConstant      m_pOpMmi1[0x20];
-	static InstructionFuncConstant      m_pOpMmi2[0x20];
-	static InstructionFuncConstant      m_pOpMmi3[0x20];
+	static InstructionFuncConstant		m_pOpMmi0[0x20];
+	static InstructionFuncConstant		m_pOpMmi1[0x20];
+	static InstructionFuncConstant		m_pOpMmi2[0x20];
+	static InstructionFuncConstant		m_pOpMmi3[0x20];
 
 	static void							ReflOpRdRt(MIPSReflection::INSTRUCTION*, CMIPS*, uint32, uint32, char*, unsigned int);
 	static void							ReflOpRsImm(MIPSReflection::INSTRUCTION*, CMIPS*, uint32, uint32, char*, unsigned int);
@@ -36,91 +36,91 @@ protected:
 
 private:
 
-	void							    PushVector(unsigned int);
-	void							    PullVector(unsigned int);
-    size_t                              GetLoOffset(unsigned int);
-    size_t                              GetHiOffset(unsigned int);
+	void								PushVector(unsigned int);
+	void								PullVector(unsigned int);
+	size_t								GetLoOffset(unsigned int);
+	size_t								GetHiOffset(unsigned int);
 
 	//General
-	void							    LQ();
-	void							    SQ();
+	void								LQ();
+	void								SQ();
 
 	//Special
-	void							    REEXCPT();
+	void								REEXCPT();
 
 	//RegImm
-	void							    MTSAB();
-	void							    MTSAH();
+	void								MTSAB();
+	void								MTSAH();
 
 	//Special2
-	void							    MADD();
-	void							    PLZCW();
-	void							    MMI0();
-	void							    MMI2();
-	void							    MFHI1();
-	void							    MTHI1();
-	void							    MFLO1();
-	void							    MTLO1();
-	void							    MULT1();
-	void							    MULTU1();
-	void							    DIV1();
-	void							    DIVU1();
-    void                                MADD1();
-	void							    MMI1();
-	void							    MMI3();
-	void							    PSLLH();
-	void							    PSRLH();
-	void							    PSRAH();
-    void                                PSLLW();
-    void                                PSRLW();
-    void                                PSRAW();
+	void								MADD();
+	void								PLZCW();
+	void								MMI0();
+	void								MMI2();
+	void								MFHI1();
+	void								MTHI1();
+	void								MFLO1();
+	void								MTLO1();
+	void								MULT1();
+	void								MULTU1();
+	void								DIV1();
+	void								DIVU1();
+	void								MADD1();
+	void								MMI1();
+	void								MMI3();
+	void								PSLLH();
+	void								PSRLH();
+	void								PSRAH();
+	void								PSLLW();
+	void								PSRLW();
+	void								PSRAW();
 
 	//Mmi0
-    void                                PADDW();
-	void							    PSUBW();
-	void							    PADDH();
-	void							    PCGTH();
-	void							    PMAXH();
-    void                                PADDB();
-	void							    PSUBB();
-	void							    PADDSW();
-	void							    PEXTLW();
-    void                                PPACW();
-	void							    PEXTLH();
-	void							    PPACH();
-	void							    PEXTLB();
-	void							    PPACB();
-	void							    PEXT5();
+	void								PADDW();
+	void								PSUBW();
+	void								PADDH();
+	void								PCGTH();
+	void								PMAXH();
+	void								PADDB();
+	void								PSUBB();
+	void								PADDSW();
+	void								PEXTLW();
+	void								PPACW();
+	void								PEXTLH();
+	void								PPACH();
+	void								PEXTLB();
+	void								PPACB();
+	void								PEXT5();
 
 	//Mmi1
-	void							    PCEQW();
+	void								PCEQW();
 	void								PMINW();
-	void							    PMINH();
-	void							    PADDUW();
-	void							    PEXTUW();
-	void							    PEXTUB();
-	void							    QFSRV();
+	void								PMINH();
+	void								PADDUW();
+	void								PEXTUW();
+	void								PEXTUB();
+	void								QFSRV();
 
 	//Mmi2
 	void								PMFHI();
-    void                                PMFLO();
+	void								PMFLO();
 	void								PMULTW();
-    void                                PDIVW();
-	void							    PCPYLD();
-	void							    PAND();
-	void							    PXOR();
-    void                                PMULTH();
-    void                                PEXEW();
-    void                                PROT3W();
+	void								PDIVW();
+	void								PCPYLD();
+	void								PAND();
+	void								PXOR();
+	void								PMULTH();
+	void								PEXEW();
+	void								PROT3W();
 
 	//Mmi3
-	void							    PCPYUD();
-	void							    POR();
-	void							    PNOR();
-	void							    PCPYH();
+	void								PCPYUD();
+	void								POR();
+	void								PNOR();
+	void								PCPYH();
 	void								PEXCW();
 
-    void                                Generic_MADD(unsigned int);
+	void								Generic_MADD(unsigned int);
 
 	//Reflection tables
 	static MIPSReflection::INSTRUCTION	m_cReflMmi[64];
