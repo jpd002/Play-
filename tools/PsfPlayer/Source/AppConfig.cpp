4944e085055d6f3b6084e0266fb8e8a93d915ca7
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-11-14 05:14:08 +0000

    Cleaned AppConfig.
    git-svn-id: http://svn.purei.org/purei/trunk@1039 b36208d7-6611-0410-8bec-b1987f11c4a2

--- tools/PsfPlayer/Source/AppConfig.cpp
+++ tools/PsfPlayer/Source/AppConfig.cpp
@@ -1,15 +1,11 @@
 #include "AppConfig.h"
 #include "PathUtils.h"
-#include "Utf8.h"
-#if !defined(WIN32)
-#include <pwd.h>
-#endif
 
-#define BASE_DATA_PATH          L"PsfPlayer Data Files"
-#define DEFAULT_CONFIG_PATH     (L"config.xml")
+#define BASE_DATA_PATH			(L"PsfPlayer Data Files")
+#define CONFIG_FILENAME			(L"config.xml")
 
-CAppConfig::CAppConfig() :
-CConfig(BuildConfigPath())
+CAppConfig::CAppConfig() 
+: CConfig(BuildConfigPath())
 {
 
 }
@@ -21,36 +17,13 @@ CAppConfig::~CAppConfig()
 
 Framework::CConfig::PathType CAppConfig::GetBasePath()
 {
-#if defined(WIN32)
-	return (Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH);
-#elif defined(MACOSX)
-	return (Utf8ToPath(Framework::PathUtils::GetHomePath().string().c_str()) / BASE_DATA_PATH);
-#else
-	return CConfig::PathType();
-#endif
-}
-
-Framework::CConfig::PathType CAppConfig::Utf8ToPath(const char* path)
-{
-	return CConfig::PathType(Framework::Utf8::ConvertFrom(path));
-}
-
-std::string CAppConfig::PathToUtf8(const CConfig::PathType& path)
-{
-	return Framework::Utf8::ConvertTo(path.wstring());
+	auto result = Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH;
+	return result;
 }
 
 Framework::CConfig::PathType CAppConfig::BuildConfigPath()
 {
-#if defined(MACOSX)
-	passwd* userInfo = getpwuid(getuid());
-	if(userInfo == NULL) return DEFAULT_CONFIG_PATH;
-	return wstring(Utf8::ConvertFrom(userInfo->pw_dir)) + L"/Library/Preferences/com.vapps.Purei.xml";
-#elif defined(WIN32)
-    CConfig::PathType userPath(GetBasePath());
+	auto userPath(GetBasePath());
 	Framework::PathUtils::EnsurePathExists(userPath);
-    return (userPath / L"config.xml");
-#else
-	return DEFAULT_CONFIG_PATH;
-#endif
+	return userPath / CONFIG_FILENAME;
 }
