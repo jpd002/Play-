9c2758e4f2fbc2dae6478591e9e9499400fd7943
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2012-12-30 23:07:53 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@1062 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/iop/Iop_Cdvdfsv.h
+++ Source/iop/Iop_Cdvdfsv.h
@@ -12,16 +12,16 @@ namespace Iop
 	class CCdvdfsv : public CModule
 	{
 	public:
-						    CCdvdfsv(CSifMan&, uint8*);
-		virtual			    ~CCdvdfsv();
+							CCdvdfsv(CSifMan&, uint8*);
+		virtual				~CCdvdfsv();
 
-        virtual std::string GetId() const;
-        virtual std::string GetFunctionName(unsigned int) const;
-        virtual void        Invoke(CMIPS&, unsigned int);
-//		virtual void	    SaveState(Framework::CStream*);
-//		virtual void	    LoadState(Framework::CStream*);
+		virtual std::string	GetId() const;
+		virtual std::string	GetFunctionName(unsigned int) const;
+		virtual void		Invoke(CMIPS&, unsigned int);
+//		virtual void		SaveState(Framework::CStream*);
+//		virtual void		LoadState(Framework::CStream*);
 
-        void                SetIsoImage(CISO9660*);
+		void				SetIsoImage(CISO9660*);
 
 		enum MODULE_ID
 		{
@@ -31,34 +31,34 @@ namespace Iop
 			MODULE_ID_4 = 0x80000595,
 			MODULE_ID_5 = 0x80000596,
 			MODULE_ID_6 = 0x80000597,
-            MODULE_ID_7 = 0x8000059A,
+			MODULE_ID_7 = 0x8000059A,
 			MODULE_ID_8 = 0x8000059C,
 		};
 
 	private:
-		void			    Invoke592(uint32, uint32*, uint32, uint32*, uint32, uint8*);
-		void			    Invoke593(uint32, uint32*, uint32, uint32*, uint32, uint8*);
-		void			    Invoke595(uint32, uint32*, uint32, uint32*, uint32, uint8*);
-		void			    Invoke597(uint32, uint32*, uint32, uint32*, uint32, uint8*);
-        void                Invoke59A(uint32, uint32*, uint32, uint32*, uint32, uint8*);
-		void			    Invoke59C(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke592(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke593(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke595(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke597(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke59A(uint32, uint32*, uint32, uint32*, uint32, uint8*);
+		void				Invoke59C(uint32, uint32*, uint32, uint32*, uint32, uint8*);
 
 		//Methods
-		void			    Read(uint32*, uint32, uint32*, uint32, uint8*);
-        void                ReadIopMem(uint32*, uint32, uint32*, uint32, uint8*);
-		void			    StreamCmd(uint32*, uint32, uint32*, uint32, uint8*);
-		void			    SearchFile(uint32*, uint32, uint32*, uint32, uint8*);
+		void				Read(uint32*, uint32, uint32*, uint32, uint8*);
+		void				ReadIopMem(uint32*, uint32, uint32*, uint32, uint8*);
+		void				StreamCmd(uint32*, uint32, uint32*, uint32, uint8*);
+		void				SearchFile(uint32*, uint32, uint32*, uint32, uint8*);
 
-		uint32              m_nStreamPos;
-        uint8*              m_iopRam;
-        CISO9660*           m_iso;
+		uint32				m_nStreamPos;
+		uint8*				m_iopRam;
+		CISO9660*			m_iso;
 
-        CSifModuleAdapter   m_module592;
-        CSifModuleAdapter   m_module593;
-        CSifModuleAdapter   m_module595;
-        CSifModuleAdapter   m_module597;
-        CSifModuleAdapter   m_module59A;
-        CSifModuleAdapter   m_module59C;
+		CSifModuleAdapter	m_module592;
+		CSifModuleAdapter	m_module593;
+		CSifModuleAdapter	m_module595;
+		CSifModuleAdapter	m_module597;
+		CSifModuleAdapter	m_module59A;
+		CSifModuleAdapter	m_module59C;
 	};
 
 }
