e6ed0f8bae0344b30c462a7d8330bbc323b4f1f8
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2013-02-24 05:09:00 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@1079 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/iop/Iop_Module.h
+++ Source/iop/Iop_Module.h
@@ -6,17 +6,17 @@
 
 namespace Iop
 {
-    class CModule
-    {
-    public:
-        virtual                 ~CModule() {}
-        virtual std::string     GetId() const = 0;
+	class CModule
+	{
+	public:
+		virtual					~CModule() {}
+		virtual std::string		GetId() const = 0;
 		virtual std::string		GetFunctionName(unsigned int) const = 0;
-        virtual void            Invoke(CMIPS&, unsigned int) = 0;
+		virtual void			Invoke(CMIPS&, unsigned int) = 0;
 
-    private:
+	private:
 
-    };
+	};
 };
 
 #endif
