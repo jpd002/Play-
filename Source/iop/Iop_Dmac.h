c4a86af5bfc7d761323550e13fc7d2be6cef5cbc
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2013-03-05 09:27:13 +0000

    Cleanup.
    git-svn-id: http://svn.purei.org/purei/trunk@1087 b36208d7-6611-0410-8bec-b1987f11c4a2

--- Source/iop/Iop_Dmac.h
+++ Source/iop/Iop_Dmac.h
@@ -8,13 +8,13 @@ namespace Iop
 {
 	class CIntc;
 
-    class CDmac
-    {
-    public:
-        enum
-        {
-            MAX_CHANNEL = 14,
-        };
+	class CDmac
+	{
+	public:
+		enum
+		{
+			MAX_CHANNEL = 14,
+		};
 
 		enum
 		{
@@ -28,46 +28,46 @@ namespace Iop
 			CH8_BASE	= 0x1F801500,
 		};
 
-        enum DMAC_ZONE1
-        {
-            DMAC_ZONE1_START    = 0x1F801080,
-            DMAC_ZONE1_END      = 0x1F8010FF,
+		enum DMAC_ZONE1
+		{
+			DMAC_ZONE1_START	= 0x1F801080,
+			DMAC_ZONE1_END		= 0x1F8010FF,
 			DMAC_ZONE2_START	= 0x1F801500,
 			DMAC_ZONE2_END		= 0x1F80150F,
 		};
 
-                        CDmac(uint8*, CIntc&);
-        virtual         ~CDmac();
+						CDmac(uint8*, CIntc&);
+		virtual			~CDmac();
 
-        void            Reset();
-        void			SetReceiveFunction(unsigned int, const Dmac::CChannel::ReceiveFunctionType&);
-        uint32          ReadRegister(uint32);
-        uint32          WriteRegister(uint32, uint32);
+		void			Reset();
+		void			SetReceiveFunction(unsigned int, const Dmac::CChannel::ReceiveFunctionType&);
+		uint32			ReadRegister(uint32);
+		uint32			WriteRegister(uint32, uint32);
 
 		void			AssertLine(unsigned int);
-        uint8*          GetRam();
+		uint8*			GetRam();
 
-        enum
+		enum
 		{
 			DPCR		= 0x1F8010F0,
 			DICR		= 0x1F8010F4
 		};
 
-    private:
+	private:
 		unsigned int	GetChannelIdFromAddress(uint32);
-        Dmac::CChannel* GetChannelFromAddress(uint32);
-        void            LogRead(uint32);
-        void            LogWrite(uint32, uint32);
+		Dmac::CChannel*	GetChannelFromAddress(uint32);
+		void			LogRead(uint32);
+		void			LogWrite(uint32, uint32);
 
-        Dmac::CChannel  m_channelSpu0;
+		Dmac::CChannel	m_channelSpu0;
 		Dmac::CChannel	m_channelSpu1;
-        Dmac::CChannel* m_channel[MAX_CHANNEL];
+		Dmac::CChannel* m_channel[MAX_CHANNEL];
 
-        uint32			m_DPCR;
+		uint32			m_DPCR;
 		uint32			m_DICR;
-        uint8*          m_ram;
+		uint8*			m_ram;
 		CIntc&			m_intc;
-    };
+	};
 }
 
 #endif
