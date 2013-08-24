001b5c0457f2e7854bb2700d4157ff9d1b36a937
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2013-08-24 05:56:08 +0000

    Fixed playback problems with OpenAL on MacOSX and iOS.
    git-svn-id: http://svn.purei.org/purei/trunk@1180 b36208d7-6611-0410-8bec-b1987f11c4a2

--- tools/PsfPlayer/Source/SH_OpenAL.cpp
+++ tools/PsfPlayer/Source/SH_OpenAL.cpp
@@ -5,13 +5,13 @@
 //#define LOGGING
 #define SAMPLE_RATE 44100
 
-using namespace std;
-
 ALCint g_attrList[] = 
 {
 	ALC_FREQUENCY,	SAMPLE_RATE,
 	0,				0
 };
+
+#define CHECK_AL_ERROR() { assert(alGetError() == AL_NO_ERROR); }
 
 CSH_OpenAL::CSH_OpenAL() :
 m_context(m_device, g_attrList),
@@ -21,6 +21,7 @@ m_mustSync(true)
 	m_context.MakeCurrent();
 	ALuint bufferNames[MAX_BUFFERS];
 	alGenBuffers(MAX_BUFFERS, bufferNames);
+	CHECK_AL_ERROR();
 	m_availableBuffers.insert(m_availableBuffers.begin(), bufferNames, bufferNames + MAX_BUFFERS);
 }
 
@@ -37,7 +38,10 @@ CSoundHandler* CSH_OpenAL::HandlerFactory()
 void CSH_OpenAL::Reset()
 {
 	m_source.Stop();
+	ALint sourceState = m_source.GetState();
+	assert(sourceState == AL_INITIAL || sourceState == AL_STOPPED);
 	RecycleBuffers();
+	assert(m_availableBuffers.size() == MAX_BUFFERS);
 }
 
 void CSH_OpenAL::RecycleBuffers()
@@ -47,6 +51,7 @@ void CSH_OpenAL::RecycleBuffers()
 	{
 		ALuint* bufferNames = reinterpret_cast<ALuint*>(alloca(sizeof(ALuint) * bufferCount));
 		alSourceUnqueueBuffers(m_source, bufferCount, bufferNames);
+		CHECK_AL_ERROR();
 		m_availableBuffers.insert(m_availableBuffers.begin(), bufferNames, bufferNames + bufferCount);
 	}
 }
@@ -64,16 +69,19 @@ void CSH_OpenAL::Write(int16* samples, unsigned int sampleCount, unsigned int sa
 	m_availableBuffers.pop_front();
 
 	alBufferData(buffer, AL_FORMAT_STEREO16, samples, sampleCount * sizeof(int16), sampleRate);
-
+	CHECK_AL_ERROR();
+	
 	alSourceQueueBuffers(m_source, 1, &buffer);
+	CHECK_AL_ERROR();
+	
 	if(m_availableBuffers.size() == 0)
 	{
-		ALint sourceState;
-		alGetSourcei(m_source, AL_SOURCE_STATE, &sourceState);
+		ALint sourceState = m_source.GetState();
 		if(sourceState != AL_PLAYING)
 		{
-			RecycleBuffers();
 			m_source.Play();
+			assert(m_source.GetState() == AL_PLAYING);
+			RecycleBuffers();
 		}
 	}
 }
