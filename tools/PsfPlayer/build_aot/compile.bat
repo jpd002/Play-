7242746394e58a0836b46610ce7b681e30f9e165
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2013-09-22 04:47:42 +0000

    PsfPlayer.Aot: Compile more platforms.
    git-svn-id: http://svn.purei.org/purei/trunk@1192 b36208d7-6611-0410-8bec-b1987f11c4a2

--- tools/PsfPlayer/build_aot/compile.bat
+++ tools/PsfPlayer/build_aot/compile.bat
@@ -1,2 +1,13 @@
-"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" x86 coff PsfBlocks.obj
+mkdir output
+pushd output
+mkdir coff
+mkdir macho
+pushd macho
+mkdir armv7
+mkdir i386
+popd
+popd
+"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" x86 coff .\output\coff\PsfBlocks.obj
+"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" x86 macho .\output\macho\i386\PsfBlocks.o
+"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" arm macho .\output\macho\armv7\PsfBlocks.o
 @pause
