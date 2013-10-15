cf34b25e486032915cae2e4bc177607fa22d4876
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2013-10-15 02:34:50 +0000

    PsfPlayer iOS: Added more information in the FileInfoView.
    git-svn-id: http://svn.purei.org/purei/trunk@1195 b36208d7-6611-0410-8bec-b1987f11c4a2

--- tools/PsfPlayer/Source/ios_ui/FileInfoViewController.mm
+++ tools/PsfPlayer/Source/ios_ui/FileInfoViewController.mm
@@ -1,4 +1,25 @@
 #import "FileInfoViewController.h"
+#import "NSStringUtils.h"
+
+enum
+{
+	SECTION_INFO,
+	SECTION_RAWTAGS,
+	SECTION_COUNT
+};
+
+enum
+{
+	INFO_SECTION_TITLE,
+	INFO_SECTION_ARTIST,
+	INFO_SECTION_GAME,
+	INFO_SECTION_YEAR,
+	INFO_SECTION_GENRE,
+	INFO_SECTION_COMMENT,
+	INFO_SECTION_COPYRIGHT,
+	INFO_SECTION_PSFBY,
+	INFO_SECTION_COUNT
+};
 
 @implementation FileInfoViewController
 
@@ -61,28 +82,49 @@
 
 -(NSInteger)numberOfSectionsInTableView: (UITableView*)tableView 
 {
-	return 1;
+	return SECTION_COUNT;
 }
 
 -(NSInteger)tableView: (UITableView*)tableView numberOfRowsInSection: (NSInteger)section 
 {
-	if(section == 0)
+	NSInteger rowCount = 0;
+	switch(section)
 	{
-		return m_rawTags.size();
+		case SECTION_INFO:
+			rowCount = INFO_SECTION_COUNT;
+			break;
+		case SECTION_RAWTAGS:
+			rowCount = m_rawTags.size();
+			break;
 	}
-	return 0;
+	return rowCount;
 }
 
 -(NSString*)tableView: (UITableView*)tableView titleForHeaderInSection: (NSInteger)section 
 {
 	NSString *sectionHeader = nil;
 	
-	if(section == 0) 
+	switch(section)
 	{
-		sectionHeader = @"Raw Tags";
+		case SECTION_INFO:
+			sectionHeader = @"Info";
+			break;
+		case SECTION_RAWTAGS:
+			sectionHeader = @"Raw Tags";
+			break;
 	}
 	
 	return sectionHeader;
+}
+
+-(NSString*)infoDisplayTextForItem: (const char*)itemName withTag: (const char*)tagName
+{
+	NSString* itemValue = @"";
+	if(m_tags.HasTag(tagName))
+	{
+		itemValue = stringWithWchar(m_tags.GetTagValue(tagName));
+	}
+	return [NSString stringWithFormat: @"%s: %@", itemName, itemValue];
 }
 
 -(UITableViewCell*)tableView: (UITableView*)tableView cellForRowAtIndexPath: (NSIndexPath*)indexPath 
@@ -95,7 +137,39 @@
 		cell = [[[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier: CellIdentifier] autorelease];
 	}
 	
-	if(indexPath.section == 0)
+	cell.textLabel.adjustsFontSizeToFitWidth = YES;
+	
+	if(indexPath.section == SECTION_INFO)
+	{
+		switch(indexPath.row)
+		{
+		case INFO_SECTION_TITLE:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Title" withTag: "title"];
+			break;
+		case INFO_SECTION_ARTIST:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Artist" withTag: "artist"];
+			break;
+		case INFO_SECTION_GAME:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Game" withTag: "game"];
+			break;
+		case INFO_SECTION_YEAR:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Year" withTag: "year"];
+			break;
+		case INFO_SECTION_GENRE:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Genre" withTag: "genre"];
+			break;
+		case INFO_SECTION_COMMENT:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Comment" withTag: "comment"];
+			break;
+		case INFO_SECTION_COPYRIGHT:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Copyright" withTag: "copyright"];
+			break;
+		case INFO_SECTION_PSFBY:
+			cell.textLabel.text = [self infoDisplayTextForItem: "Psf by" withTag: "psfby"];
+			break;
+		}
+	}
+	else if(indexPath.section == SECTION_RAWTAGS)
 	{
 		std::string rawTag = m_rawTags[indexPath.row];
 		cell.textLabel.text = [NSString stringWithUTF8String: rawTag.c_str()];
