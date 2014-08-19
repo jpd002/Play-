76ef6fb7fa359ef3a4d0dfb5047ae749b8800bd0
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-08-19 02:54:19 -0400

    Log direct EE to IOP transfers.

--- Source/SIF.cpp
+++ Source/SIF.cpp
@@ -193,6 +193,8 @@ uint32 CSIF::ReceiveDMA6(uint32 nSrcAddr, uint32 nSize, uint32 nDstAddr, bool is
 	else
 	{
 		assert(nDstAddr < PS2::IOP_RAM_SIZE);
+		CLog::GetInstance().Print(LOG_NAME, "WriteToIop(dstAddr = 0x%0.8X, srcAddr = 0x%0.8X, size = 0x%0.8X);\r\n", 
+			nDstAddr, nSrcAddr, nSize);
 		if(nDstAddr >= 0 && nDstAddr <= CMD_RECVADDR)
 		{
 			CLog::GetInstance().Print(LOG_NAME, "Warning: Trying to DMA in Bios Control Area.\r\n");
