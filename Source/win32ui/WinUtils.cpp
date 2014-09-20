4d8696885942719e990a1c8f754f9092329033a4
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-09-19 21:26:04 -0400

    Added PointsToPixels function.

--- Source/win32ui/WinUtils.cpp
+++ Source/win32ui/WinUtils.cpp
@@ -29,3 +29,20 @@ TCHAR WinUtils::FixSlashes(TCHAR nCharacter)
 {
 	return (nCharacter == _T('/')) ? _T('\\') : nCharacter;
 }
+
+int WinUtils::PointsToPixels(int pointsValue)
+{
+	int ydpi = GetDeviceCaps(GetDC(NULL), LOGPIXELSY);
+	return MulDiv(pointsValue, ydpi, 96);
+}
+
+Framework::Win32::CRect WinUtils::PointsToPixels(const Framework::Win32::CRect& pointsRect)
+{
+	int ydpi = GetDeviceCaps(GetDC(NULL), LOGPIXELSY);
+	Framework::Win32::CRect pixelsRect(0, 0, 0, 0);
+	pixelsRect.SetLeft(MulDiv(pointsRect.Left(), ydpi, 96));
+	pixelsRect.SetTop(MulDiv(pointsRect.Top(), ydpi, 96));
+	pixelsRect.SetRight(MulDiv(pointsRect.Right(), ydpi, 96));
+	pixelsRect.SetBottom(MulDiv(pointsRect.Bottom(), ydpi, 96));
+	return pixelsRect;
+}
