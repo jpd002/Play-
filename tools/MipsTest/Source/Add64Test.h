94162fd03eb72e20b33a0d773b2c20a47d7b91f4
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-09-29 01:18:52 -0400

    Cleanup.

--- tools/MipsTest/Source/Add64Test.h
+++ tools/MipsTest/Source/Add64Test.h
@@ -1,13 +1,9 @@
-#ifndef _ADD64TEST_H_
-#define _ADD64TEST_H_
+#pragma once
 
 #include "Test.h"
-#include "MIPSAssembler.h"
 
 class CAdd64Test : public CTest
 {
 public:
 	void		Execute(CTestVm&);
 };
-
-#endif
