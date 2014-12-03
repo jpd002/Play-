7442fa74cde786d40e777eb7760a7a2c12fd1a13
jpd002 <jpd002@b36208d7-6611-0410-8bec-b1987f11c4a2>
2014-12-03 07:25:55 +0000

    Android WIP.
    git-svn-id: http://svn.purei.org/purei/trunk@1601 b36208d7-6611-0410-8bec-b1987f11c4a2

---
+++ build_android/src/com/virtualapplications/play/MainActivity.java
@@ -0,0 +1,17 @@
+package com.virtualapplications.play;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+
+public class MainActivity extends Activity 
+{
+	@Override protected void onCreate(Bundle icicle) 
+	{
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+	}
+}
