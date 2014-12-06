342c0122b07c0003ad971c97447d2074d1f8d48b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-12-06 16:42:11 -0500

    Added VuTest project.

---
+++ tools/VuTest/TestVm.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "MIPS.h"
+#include "MA_VU.h"
+#include "VuExecutor.h"
+
+class CTestVm
+{
+public:
+						CTestVm();
+	virtual				~CTestVm();
+
+	void				Reset();
+	void				ExecuteTest(uint32);
+
+	CMIPS				m_cpu;
+	CVuExecutor			m_executor;
+	uint8*				m_vuMem = nullptr;
+	uint8*				m_microMem = nullptr;
+	CMA_VU				m_maVu;
+};
