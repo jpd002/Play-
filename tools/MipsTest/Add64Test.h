d1ce16f7b486a5f32bf96adbbe74742d8ea3ec27
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-12-06 17:31:07 -0500

    Moved MipsTest stuff.

---
+++ tools/MipsTest/Add64Test.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Test.h"
+
+class CAdd64Test : public CTest
+{
+public:
+	void		Execute(CTestVm&);
+};
--- tools/MipsTest/Source/Add64Test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Test.h"
-
-class CAdd64Test : public CTest
-{
-public:
-	void		Execute(CTestVm&);
-};
