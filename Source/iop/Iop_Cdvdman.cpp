ef1ab148ef3ea4f1f0223fdf1e2466c99c320720
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-03-07 16:13:39 -0500

    Cleanup.

--- Source/iop/Iop_Cdvdman.cpp
+++ Source/iop/Iop_Cdvdman.cpp
@@ -1,24 +1,22 @@
 #include "../Log.h"
 #include "Iop_Cdvdman.h"
 
-#define LOG_NAME            "iop_cdvdman"
+#define LOG_NAME			"iop_cdvdman"
 
-#define FUNCTION_CDREAD         "CdRead"
+#define FUNCTION_CDREAD			"CdRead"
 #define FUNCTION_CDSEEK			"CdSeek"
-#define FUNCTION_CDGETERROR     "CdGetError"
+#define FUNCTION_CDGETERROR		"CdGetError"
 #define FUNCTION_CDSEARCHFILE	"CdSearchFile"
-#define FUNCTION_CDSYNC         "CdSync"
-#define FUNCTION_CDGETDISKTYPE  "CdGetDiskType"
-#define FUNCTION_CDDISKREADY    "CdDiskReady"
+#define FUNCTION_CDSYNC			"CdSync"
+#define FUNCTION_CDGETDISKTYPE	"CdGetDiskType"
+#define FUNCTION_CDDISKREADY	"CdDiskReady"
 #define FUNCTION_CDREADCLOCK	"CdReadClock"
 #define FUNCTION_CDSTATUS		"CdStatus"
 
 using namespace Iop;
-using namespace std;
 
-CCdvdman::CCdvdman(uint8* ram) :
-m_ram(ram),
-m_image(NULL)
+CCdvdman::CCdvdman(uint8* ram)
+: m_ram(ram)
 {
 
 }
@@ -28,133 +26,133 @@ CCdvdman::~CCdvdman()
 
 }
 
-string CCdvdman::GetId() const
+std::string CCdvdman::GetId() const
 {
-    return "cdvdman";
+	return "cdvdman";
 }
 
-string CCdvdman::GetFunctionName(unsigned int functionId) const
+std::string CCdvdman::GetFunctionName(unsigned int functionId) const
 {
-    switch(functionId)
-    {
-    case 6:
-        return FUNCTION_CDREAD;
-        break;
+	switch(functionId)
+	{
+	case 6:
+		return FUNCTION_CDREAD;
+		break;
 	case 7:
 		return FUNCTION_CDSEEK;
 		break;
-    case 8:
-        return FUNCTION_CDGETERROR;
-        break;
+	case 8:
+		return FUNCTION_CDGETERROR;
+		break;
 	case 10:
 		return FUNCTION_CDSEARCHFILE;
 		break;
-    case 11:
-        return FUNCTION_CDSYNC;
-        break;
-    case 12:
-        return FUNCTION_CDGETDISKTYPE;
-        break;
-    case 13:
-        return FUNCTION_CDDISKREADY;
-        break;
+	case 11:
+		return FUNCTION_CDSYNC;
+		break;
+	case 12:
+		return FUNCTION_CDGETDISKTYPE;
+		break;
+	case 13:
+		return FUNCTION_CDDISKREADY;
+		break;
 	case 24:
 		return FUNCTION_CDREADCLOCK;
 		break;
 	case 28:
 		return FUNCTION_CDSTATUS;
 		break;
-    default:
-        return "unknown";
-        break;
-    }
+	default:
+		return "unknown";
+		break;
+	}
 }
 
 void CCdvdman::Invoke(CMIPS& ctx, unsigned int functionId)
 {
-    switch(functionId)
-    {
-    case 6:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdRead(
-            ctx.m_State.nGPR[CMIPS::A0].nV0,
-            ctx.m_State.nGPR[CMIPS::A1].nV0,
-            ctx.m_State.nGPR[CMIPS::A2].nV0,
-            ctx.m_State.nGPR[CMIPS::A3].nV0);
-        break;
+	switch(functionId)
+	{
+	case 6:
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdRead(
+			ctx.m_State.nGPR[CMIPS::A0].nV0,
+			ctx.m_State.nGPR[CMIPS::A1].nV0,
+			ctx.m_State.nGPR[CMIPS::A2].nV0,
+			ctx.m_State.nGPR[CMIPS::A3].nV0);
+		break;
 	case 7:
 		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdSeek(
 			ctx.m_State.nGPR[CMIPS::A0].nV0
 			);
 		break;
-    case 8:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdGetError();
-        break;
+	case 8:
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdGetError();
+		break;
 	case 10:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdSearchFile(
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdSearchFile(
 			ctx.m_State.nGPR[CMIPS::A0].nV0,
 			ctx.m_State.nGPR[CMIPS::A1].nV0);
 		break;
-    case 11:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdSync(ctx.m_State.nGPR[CMIPS::A0].nV0);
-        break;
-    case 12:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdGetDiskType();
-        break;
-    case 13:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdDiskReady(ctx.m_State.nGPR[CMIPS::A0].nV0);
-        break;
+	case 11:
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdSync(ctx.m_State.nGPR[CMIPS::A0].nV0);
+		break;
+	case 12:
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdGetDiskType();
+		break;
+	case 13:
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdDiskReady(ctx.m_State.nGPR[CMIPS::A0].nV0);
+		break;
 	case 24:
-        ctx.m_State.nGPR[CMIPS::V0].nV0 = CdReadClock(ctx.m_State.nGPR[CMIPS::A0].nV0);
+		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdReadClock(ctx.m_State.nGPR[CMIPS::A0].nV0);
 		break;
 	case 28:
 		ctx.m_State.nGPR[CMIPS::V0].nV0 = CdStatus();
 		break;
-    default:
-        CLog::GetInstance().Print(LOG_NAME, "Unknown function called (%d).\r\n", 
-            functionId);
-        break;
-    }
+	default:
+		CLog::GetInstance().Print(LOG_NAME, "Unknown function called (%d).\r\n", 
+			functionId);
+		break;
+	}
 }
 
 void CCdvdman::SetIsoImage(CISO9660* image)
 {
-    m_image = image;
+	m_image = image;
 }
 
 uint32 CCdvdman::CdRead(uint32 startSector, uint32 sectorCount, uint32 bufferPtr, uint32 modePtr)
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDREAD "(startSector = 0x%X, sectorCount = 0x%X, bufferPtr = 0x%0.8X, modePtr = 0x%0.8X);\r\n",
-        startSector, sectorCount, bufferPtr, modePtr);
-    if(modePtr != 0)
-    {
-        uint8* mode = &m_ram[modePtr];
-        //Does that make sure it's 2048 byte mode?
-        assert(mode[2] == 0);
-    }
-    if(m_image != NULL && bufferPtr != 0)
-    {
-        uint8* buffer = &m_ram[bufferPtr];
-        uint32 sectorSize = 2048;
-        for(unsigned int i = 0; i < sectorCount; i++)
-        {
-            m_image->ReadBlock(startSector + i, buffer);
-            buffer += sectorSize;
-        }
-    }
-    return 1;
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDREAD "(startSector = 0x%X, sectorCount = 0x%X, bufferPtr = 0x%0.8X, modePtr = 0x%0.8X);\r\n",
+		startSector, sectorCount, bufferPtr, modePtr);
+	if(modePtr != 0)
+	{
+		uint8* mode = &m_ram[modePtr];
+		//Does that make sure it's 2048 byte mode?
+		assert(mode[2] == 0);
+	}
+	if(m_image != NULL && bufferPtr != 0)
+	{
+		uint8* buffer = &m_ram[bufferPtr];
+		uint32 sectorSize = 2048;
+		for(unsigned int i = 0; i < sectorCount; i++)
+		{
+			m_image->ReadBlock(startSector + i, buffer);
+			buffer += sectorSize;
+		}
+	}
+	return 1;
 }
 
 uint32 CCdvdman::CdSeek(uint32 sector)
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDSEEK "(sector = 0x%X);\r\n",
-        sector);
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDSEEK "(sector = 0x%X);\r\n",
+		sector);
 	return 1;
 }
 
 uint32 CCdvdman::CdGetError()
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDGETERROR "();\r\n");
-    return 0;
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDGETERROR "();\r\n");
+	return 0;
 }
 
 uint32 CCdvdman::CdSearchFile(uint32 fileInfoPtr, uint32 namePtr)
@@ -216,23 +214,23 @@ uint32 CCdvdman::CdSearchFile(uint32 fileInfoPtr, uint32 namePtr)
 
 uint32 CCdvdman::CdSync(uint32 mode)
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDSYNC "(mode = %i);\r\n",
-        mode);
-    return 0;
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDSYNC "(mode = %i);\r\n",
+		mode);
+	return 0;
 }
 
 uint32 CCdvdman::CdGetDiskType()
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDGETDISKTYPE "();\r\n");
-    //0x14 = PS2DVD
-    return 0x14;
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDGETDISKTYPE "();\r\n");
+	//0x14 = PS2DVD
+	return 0x14;
 }
 
 uint32 CCdvdman::CdDiskReady(uint32 mode)
 {
-    CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDDISKREADY "(mode = %i);\r\n",
-        mode);
-    return 2;
+	CLog::GetInstance().Print(LOG_NAME, FUNCTION_CDDISKREADY "(mode = %i);\r\n",
+		mode);
+	return 2;
 }
 
 uint32 CCdvdman::CdReadClock(uint32 clockPtr)
--- Source/iop/Iop_Cdvdman.h
+++ Source/iop/Iop_Cdvdman.h
@@ -1,37 +1,34 @@
-#ifndef _IOP_CDVDMAN_H_
-#define _IOP_CDVDMAN_H_
+#pragma once
 
 #include "Iop_Module.h"
 #include "../ISO9660/ISO9660.h"
 
 namespace Iop
 {
-    class CCdvdman : public CModule
-    {
-    public:
-                                CCdvdman(uint8*);
-        virtual                 ~CCdvdman();
+	class CCdvdman : public CModule
+	{
+	public:
+								CCdvdman(uint8*);
+		virtual					~CCdvdman();
 
-        virtual std::string     GetId() const;
-		virtual std::string		GetFunctionName(unsigned int) const;
-        virtual void            Invoke(CMIPS&, unsigned int);
+		virtual std::string		GetId() const override;
+		virtual std::string		GetFunctionName(unsigned int) const override;
+		virtual void			Invoke(CMIPS&, unsigned int) override;
 
-        void                    SetIsoImage(CISO9660*);
+		void					SetIsoImage(CISO9660*);
 
-    private:
-        uint32                  CdRead(uint32, uint32, uint32, uint32);
+	private:
+		uint32					CdRead(uint32, uint32, uint32, uint32);
 		uint32					CdSeek(uint32);
-        uint32                  CdGetError();
+		uint32					CdGetError();
 		uint32					CdSearchFile(uint32, uint32);
-        uint32                  CdSync(uint32);
-        uint32                  CdGetDiskType();
-        uint32                  CdDiskReady(uint32);
+		uint32					CdSync(uint32);
+		uint32					CdGetDiskType();
+		uint32					CdDiskReady(uint32);
 		uint32					CdReadClock(uint32);
 		uint32					CdStatus();
 
-        CISO9660*               m_image;
-        uint8*                  m_ram;
-    };
+		CISO9660*				m_image = nullptr;
+		uint8*					m_ram = nullptr;
+	};
 };
-
-#endif
