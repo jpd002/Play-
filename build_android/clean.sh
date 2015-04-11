dfeb59672c2eadec08cc7b69e1188f52a8abaac9
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-04-11 00:40:56 -0400

    Update Android project before cleaning.

--- build_android/clean.sh
+++ build_android/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+$ANDROID_SDK_ROOT/tools/android.bat update project -p .
 $ANDROID_NDK_ROOT/ndk-build clean
 $ANDROID_NDK_ROOT/ndk-build clean NDK_DEBUG=1
 $ANT_HOME/bin/ant clean
