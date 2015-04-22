a01b96b81e641831dec006de4c9126a005376dea
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-04-22 03:45:13 -0400

    Moved BuildStatusInIT function.

--- Source/MA_VU_Lower.cpp
+++ Source/MA_VU_Lower.cpp
@@ -90,6 +90,75 @@ bool CMA_VU::CLower::IsLOI(CMIPS* ctx, uint32 address)
 	return (upperInstruction & 0x80000000) != 0;
 }
 
+void CMA_VU::CLower::BuildStatusInIT()
+{
+	//Helper for FSAND and FSOR.  Grabs STATUS flag into current m_nIT register.
+
+	VUShared::CheckMacFlagPipeline(m_codeGen, m_relativePipeTime);
+
+	//Reset result
+	m_codeGen->PushCst(0);
+	m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+
+	//Check Z flag
+	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2MF));
+	m_codeGen->PushCst(0x000F);
+	m_codeGen->And();
+	m_codeGen->PushCst(0);
+	m_codeGen->BeginIf(Jitter::CONDITION_NE);
+	{
+		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+		m_codeGen->PushCst(0x01);
+		m_codeGen->Or();
+		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+	}
+	m_codeGen->EndIf();
+
+	//Check S flag
+	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2MF));
+	m_codeGen->PushCst(0x00F0);
+	m_codeGen->And();
+	m_codeGen->PushCst(0);
+	m_codeGen->BeginIf(Jitter::CONDITION_NE);
+	{
+		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+		m_codeGen->PushCst(0x02);
+		m_codeGen->Or();
+		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+	}
+	m_codeGen->EndIf();
+
+	//Check ZS flag
+	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2SF));
+	m_codeGen->PushCst(0x000F);
+	m_codeGen->And();
+	m_codeGen->PushCst(0);
+	m_codeGen->BeginIf(Jitter::CONDITION_NE);
+	{
+		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+		m_codeGen->PushCst(0x40);
+		m_codeGen->Or();
+		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+	}
+	m_codeGen->EndIf();
+
+	//Check SS flag
+	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2SF));
+	m_codeGen->PushCst(0x00F0);
+	m_codeGen->And();
+	m_codeGen->PushCst(0);
+	m_codeGen->BeginIf(Jitter::CONDITION_NE);
+	{
+		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+		m_codeGen->PushCst(0x80);
+		m_codeGen->Or();
+		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
+	}
+	m_codeGen->EndIf();
+
+	//TODO: Check other flags
+}
+
 //////////////////////////////////////////////////
 //General Instructions
 //////////////////////////////////////////////////
@@ -259,74 +328,6 @@ void CMA_VU::CLower::FSSET()
 		m_codeGen->PushCst(0);
 		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2SF));
 	}
-}
-
-// Helper for FSAND and FSOR.  Grabs STATUS flag into current m_nIT register.
-void CMA_VU::CLower::BuildStatusInIT()
-{
-	VUShared::CheckMacFlagPipeline(m_codeGen, m_relativePipeTime);
-
-	//Reset result
-	m_codeGen->PushCst(0);
-	m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-
-	//Check Z flag
-	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2MF));
-	m_codeGen->PushCst(0x000F);
-	m_codeGen->And();
-	m_codeGen->PushCst(0);
-	m_codeGen->BeginIf(Jitter::CONDITION_NE);
-	{
-		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-		m_codeGen->PushCst(0x01);
-		m_codeGen->Or();
-		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-	}
-	m_codeGen->EndIf();
-
-	//Check S flag
-	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2MF));
-	m_codeGen->PushCst(0x00F0);
-	m_codeGen->And();
-	m_codeGen->PushCst(0);
-	m_codeGen->BeginIf(Jitter::CONDITION_NE);
-	{
-		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-		m_codeGen->PushCst(0x02);
-		m_codeGen->Or();
-		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-	}
-	m_codeGen->EndIf();
-
-	//Check ZS flag
-	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2SF));
-	m_codeGen->PushCst(0x000F);
-	m_codeGen->And();
-	m_codeGen->PushCst(0);
-	m_codeGen->BeginIf(Jitter::CONDITION_NE);
-	{
-		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-		m_codeGen->PushCst(0x40);
-		m_codeGen->Or();
-		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-	}
-	m_codeGen->EndIf();
-
-	//Check SS flag
-	m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2SF));
-	m_codeGen->PushCst(0x00F0);
-	m_codeGen->And();
-	m_codeGen->PushCst(0);
-	m_codeGen->BeginIf(Jitter::CONDITION_NE);
-	{
-		m_codeGen->PushRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-		m_codeGen->PushCst(0x80);
-		m_codeGen->Or();
-		m_codeGen->PullRel(offsetof(CMIPS, m_State.nCOP2VI[m_nIT]));
-	}
-	m_codeGen->EndIf();
-
-	//TODO: Check other flags
 }
 
 //16
