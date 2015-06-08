81e3ce282018f732e1810d43a8a7953d28a9f22c
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-06-08 01:39:23 -0400

    Made EE executor work on Android.

--- Source/ee/EeExecutor.cpp
+++ Source/ee/EeExecutor.cpp
@@ -1,3 +1,6 @@
+#ifndef _MSC_VER
+#include <sys/mman.h>
+#endif
 #include "EeExecutor.h"
 #include "../Ps2Const.h"
 #include "AlignedAlloc.h"
@@ -12,30 +15,39 @@ CEeExecutor::CEeExecutor(CMIPS& context, uint8* ram)
 
 	assert(g_eeExecutor == nullptr);
 	g_eeExecutor = this;
+
+#ifdef _MSC_VER
 	m_handler = AddVectoredExceptionHandler(TRUE, &CEeExecutor::HandleException);
 	assert(m_handler != NULL);
+#else
+	struct sigaction sigAction;
+	sigAction.sa_handler	= nullptr;
+	sigAction.sa_sigaction	= &HandleException;
+	sigAction.sa_flags		= SA_SIGINFO;
+	sigemptyset(&sigAction.sa_mask);
+	int result = sigaction(SIGSEGV, &sigAction, nullptr);
+	assert(result >= 0);
+#endif
 }
 
 CEeExecutor::~CEeExecutor()
 {
+#ifdef _MSC_VER
 	RemoveVectoredExceptionHandler(m_handler);
+#endif
 	g_eeExecutor = nullptr;
 }
 
 void CEeExecutor::Reset()
 {
-	DWORD oldProtect = 0;
-	BOOL result = VirtualProtect(m_ram, PS2::EE_RAM_SIZE, PAGE_READWRITE, &oldProtect);
-	assert(result == TRUE);
+	SetMemoryProtected(m_ram, PS2::EE_RAM_SIZE, false);
 	CMipsExecutor::Reset();
 }
 
 void CEeExecutor::ClearActiveBlocksInRange(uint32 start, uint32 end)
 {
 	uint32 rangeSize = end - start;
-	DWORD oldProtect = 0;
-	BOOL result = VirtualProtect(m_ram + start, rangeSize, PAGE_READWRITE, &oldProtect);
-	assert(result == TRUE);
+	SetMemoryProtected(m_ram + start, rangeSize, false);
 	CBasicBlock* currentBlock = nullptr;
 	if(m_context.m_State.nPC >= start && m_context.m_State.nPC < end)
 	{
@@ -54,12 +66,27 @@ CMipsExecutor::BasicBlockPtr CEeExecutor::BlockFactory(CMIPS& context, uint32 st
 	//so it keeps generating exceptions, making the game slower)
 	if(start >= 0x100000 && start < PS2::EE_RAM_SIZE)
 	{
-		DWORD oldProtect = 0;
-		BOOL result = VirtualProtect(m_ram + start, end - start + 4, PAGE_READONLY, &oldProtect);
-		assert(result == TRUE);
+		SetMemoryProtected(m_ram + start, end - start + 4, true);
 	}
 	return CMipsExecutor::BlockFactory(context, start, end);
 }
+
+void CEeExecutor::SetMemoryProtected(void* addr, size_t size, bool protect)
+{
+#ifdef _MSC_VER
+	DWORD oldProtect = 0;
+	BOOL result = VirtualProtect(addr, size, protect ? PAGE_READONLY : PAGE_READWRITE, &oldProtect);
+	assert(result == TRUE);
+#else
+	uintptr_t addrValue = reinterpret_cast<uintptr_t>(addr) & ~(m_pageSize - 1);
+	addr = reinterpret_cast<void*>(addrValue);
+	size = size + (m_pageSize - 1) & ~(m_pageSize - 1);
+	int result = mprotect(addr, size, protect ? PROT_READ : PROT_READ | PROT_WRITE);
+	assert(result >= 0);
+#endif
+}
+
+#ifdef _MSC_VER
 
 LONG WINAPI CEeExecutor::HandleException(_EXCEPTION_POINTERS* exceptionInfo)
 {
@@ -81,3 +108,25 @@ LONG CEeExecutor::HandleExceptionInternal(_EXCEPTION_POINTERS* exceptionInfo)
 	}
 	return EXCEPTION_CONTINUE_SEARCH;
 }
+
+#else
+
+void CEeExecutor::HandleException(int sigId, siginfo_t* sigInfo, void* baseContext)
+{
+	g_eeExecutor->HandleExceptionInternal(sigId, sigInfo, baseContext);
+}
+
+void CEeExecutor::HandleExceptionInternal(int sigId, siginfo_t* sigInfo, void* baseContext)
+{
+	if(sigId != SIGSEGV) return;
+	ptrdiff_t addr = reinterpret_cast<uint8*>(sigInfo->si_addr) - m_ram;
+	if(addr >= 0 && addr < PS2::EE_RAM_SIZE)
+	{
+		addr &= ~(m_pageSize - 1);
+		ClearActiveBlocksInRange(addr, addr + m_pageSize);
+		return;
+	}
+	signal(SIGSEGV, SIG_DFL);
+}
+
+#endif
--- Source/ee/EeExecutor.h
+++ Source/ee/EeExecutor.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#ifdef _MSC_VER
 #include <Windows.h>
+#endif
+
 #include "../MipsExecutor.h"
 
 class CEeExecutor : public CMipsExecutor
@@ -14,12 +17,19 @@ public:
 
 	BasicBlockPtr			BlockFactory(CMIPS&, uint32, uint32) override;
 
-	static LONG CALLBACK	HandleException(_EXCEPTION_POINTERS*);
-
 private:
-	LONG					HandleExceptionInternal(_EXCEPTION_POINTERS*);
-
 	uint8*					m_ram = nullptr;
 	uint32					m_pageSize = 0;
+
+	void					SetMemoryProtected(void*, size_t, bool);
+
+#ifdef _MSC_VER
+	static LONG CALLBACK	HandleException(_EXCEPTION_POINTERS*);
+	LONG					HandleExceptionInternal(_EXCEPTION_POINTERS*);
+
 	LPVOID					m_handler = NULL;
+#else
+	static void				HandleException(int, siginfo_t*, void*);
+	void					HandleExceptionInternal(int, siginfo_t*, void*);
+#endif
 };
