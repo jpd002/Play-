15aa5f86f6627fc22b902576ee444ffe408fd3c8
Matthew Garbet <LinkofHyruleUO@gmail.com>
2015-07-10 20:39:58 -0500

    Revert "Renamed file to ic_banner.png"
    This reverts commit 321a2fb0e0988c94013e171dd6d52a73dba55226.

--- build_android/jni/ExternalDependencies.mk
+++
@@ -1,4 +0,0 @@
-BOOST_PATH			:= /cygdrive/d/Matthew/Development/boost_1_58_0
-DEPENDENCIES_PATH	:= /cygdrive/d/Matthew/Development/Repositories/Play-Dependencies
-FRAMEWORK_PATH		:= /cygdrive/d/Matthew/Development/Repositories/Play--Framework
-CODEGEN_PATH		:= /cygdrive/d/Matthew/Development/Repositories/Play--CodeGen
