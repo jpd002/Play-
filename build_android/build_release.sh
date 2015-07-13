1914f0af60fe178c135c995f62cef06ac8ac83f6
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-07-13 15:05:00 +0100

    Fix Typo:MultiOS Build Support

--- build_android/build_release.sh
+++ build_android/build_release.sh
@@ -3,12 +3,12 @@ set -e
 
 case "`uname`" in
 	CYGWIN* | MINGW*)
-		$$ANDROID_SDK_ROOT/tools/android.bat update project -p .
+		$ANDROID_SDK_ROOT/tools/android.bat update project -p .
 		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=0
 		$ANT_HOME/bin/ant release
     ;;
   Darwin* | Linux*)
-		$$ANDROID_SDK_ROOT/tools/android update project -p .
+		$ANDROID_SDK_ROOT/tools/android update project -p .
 		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=0
 		$ANT_HOME/bin/ant release
     ;;
--- build_android/clean.sh
+++ build_android/clean.sh
@@ -2,13 +2,13 @@
 set -e
 case "`uname`" in
 	CYGWIN* | MINGW*)
-		$$ANDROID_SDK_ROOT/tools/android.bat update project -p .
+		$ANDROID_SDK_ROOT/tools/android.bat update project -p .
 		$ANDROID_NDK_ROOT/ndk-build clean
 		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=1
 		$ANT_HOME/bin/ant clean
     ;;
   Darwin* | Linux*)
-		$$ANDROID_SDK_ROOT/tools/android update project -p .
+		$ANDROID_SDK_ROOT/tools/android update project -p .
 		$ANDROID_NDK_ROOT/ndk-build clean
 		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=1
 		$ANT_HOME/bin/ant clean
