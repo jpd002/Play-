2147235133fc543d582ee18541ac2b93dae3c99b
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-07-13 14:44:48 +0100

    MultiOS Build Support

--- build_android/clean.sh
+++ build_android/clean.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-$ANDROID_SDK_ROOT/tools/android.bat update project -p .
-$ANDROID_NDK_ROOT/ndk-build clean
-$ANDROID_NDK_ROOT/ndk-build clean NDK_DEBUG=1
-$ANT_HOME/bin/ant clean
+case "`uname`" in
+	CYGWIN* | MINGW*)
+		$$ANDROID_SDK_ROOT/tools/android.bat update project -p .
+		$ANDROID_NDK_ROOT/ndk-build clean
+		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=1
+		$ANT_HOME/bin/ant clean
+    ;;
+  Darwin* | Linux*)
+		$$ANDROID_SDK_ROOT/tools/android update project -p .
+		$ANDROID_NDK_ROOT/ndk-build clean
+		$ANDROID_NDK_ROOT/ndk-build NDK_DEBUG=1
+		$ANT_HOME/bin/ant clean
+    ;;
+esac
