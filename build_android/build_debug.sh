8739fd7a9fa591ce0eeddbf3b96a4765be6288b8
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-07-14 01:23:49 -0400

    Updated Android build scripts.

--- build_android/build_debug.sh
+++ build_android/build_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 $ANDROID_SDK_ROOT/tools/android.bat update project -p .
-$ANT_HOME/bin/ant debug
+$ANT_HOME/bin/ant debug -Dndk.debug=1
--- build_android/build_release.sh
+++ build_android/build_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 $ANDROID_SDK_ROOT/tools/android.bat update project -p .
-$ANT_HOME/bin/ant release
+$ANT_HOME/bin/ant release -Dndk.debug=0
