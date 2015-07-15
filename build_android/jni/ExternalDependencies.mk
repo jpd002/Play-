26c0c37790f238744b1b4c5d60ac8f5b7fd732ed
TwistedUmbrella <twistedumbrella@gmail.com>
2015-07-14 23:10:13 -0400

    Restructure paths relative to the master project

---
+++ build_android/jni/ExternalDependencies.mk
@@ -0,0 +1,4 @@
+BOOST_PATH		:= $(LOCAL_PATH)/../../../boost_1_58_0
+DEPENDENCIES_PATH	:= $(LOCAL_PATH)/../../../Play-Dependencies
+FRAMEWORK_PATH		:= $(LOCAL_PATH)/../../../Play-Framework
+CODEGEN_PATH		:= $(LOCAL_PATH)/../../../Play-CodeGen
