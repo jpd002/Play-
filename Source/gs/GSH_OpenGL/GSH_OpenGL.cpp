c7230dc06761812bf37c8ead8b35c3690143fb7d
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-07-18 20:49:28 -0400

    Added missing blending functions.

--- Source/gs/GSH_OpenGL/GSH_OpenGL.cpp
+++ Source/gs/GSH_OpenGL/GSH_OpenGL.cpp
@@ -760,6 +760,23 @@ void CGSH_OpenGL::SetupBlendingFunction(uint64 alphaReg)
 		nFunction = GL_FUNC_REVERSE_SUBTRACT;
 		glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE, GL_ONE, GL_ZERO);
 	}
+	else if((alpha.nA == ALPHABLEND_ABD_ZERO) && (alpha.nB == ALPHABLEND_ABD_CS) && (alpha.nC == ALPHABLEND_C_FIX) && (alpha.nD == ALPHABLEND_ABD_CD))
+	{
+		//2021 -> Cd - Cs * FIX
+		nFunction = GL_FUNC_REVERSE_SUBTRACT;
+		glBlendColor(0, 0, 0, static_cast<float>(alpha.nFix) / 128.0f);
+		glBlendFuncSeparate(GL_CONSTANT_ALPHA, GL_ONE, GL_ONE, GL_ZERO);
+	}
+	else if((alpha.nA == ALPHABLEND_ABD_ZERO) && (alpha.nB == ALPHABLEND_ABD_CD) && (alpha.nC == ALPHABLEND_C_AS) && (alpha.nD == ALPHABLEND_ABD_CD))
+	{
+		//2101 -> Cd * (1 - As)
+		glBlendFuncSeparate(GL_ZERO, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
+	}
+	else if((alpha.nA == ALPHABLEND_ABD_ZERO) && (alpha.nB == ALPHABLEND_ABD_ZERO) && (alpha.nD == ALPHABLEND_ABD_CS))
+	{
+		//22*0 -> Cs (no blend)
+		glBlendFuncSeparate(GL_ONE, GL_ZERO, GL_ONE, GL_ZERO);
+	}
 	else if((alpha.nA == 2) && (alpha.nB == 2) && (alpha.nC == 2) && (alpha.nD == 1))
 	{
 		//Cd (no blend)
