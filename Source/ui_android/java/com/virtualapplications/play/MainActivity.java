76b5638b1eed53371b01f303cd1960bf3fcdc31e
Matthew Garbet <LinkofHyruleUO@gmail.com>
2015-08-11 22:24:45 -0600

    Update headers with plays logo and section header.

--- Source/ui_android/java/com/virtualapplications/play/MainActivity.java
+++ Source/ui_android/java/com/virtualapplications/play/MainActivity.java
@@ -426,8 +426,8 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 		android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
 		actionBar.setDisplayShowTitleEnabled(true);
-		actionBar.setTitle(R.string.app_name);
-		actionBar.setSubtitle(R.string.menu_title_shut);
+		actionBar.setIcon(R.drawable.ic_logo);
+		actionBar.setTitle(R.string.section_header);
 	}
 
 	
