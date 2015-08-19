4e2583b7498c967857f4e15c6f23f6478c096721
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-08-19 20:17:38 +0100

    Android: two classes in charge of setting up the IndexDB

---
+++ Source/ui_android/java/com/virtualapplications/play/database/GameIndexer.java
@@ -0,0 +1,195 @@
+package com.virtualapplications.play.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import com.virtualapplications.play.GameInfoStruct;
+import com.virtualapplications.play.NativeInterop;
+import com.virtualapplications.play.R;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+
+import static com.virtualapplications.play.MainActivity.IsLoadableExecutableFileName;
+import static com.virtualapplications.play.MainActivity.getExternalMounts;
+
+/**
+ * Based On https://github.com/LithidSoftware/android_Findex/blob/master/src/com/lithidsw/findex/utils/FileWalker.java
+ */
+public class GameIndexer {
+
+    private Context mContext;
+    String[] mediaTypes;
+    private IndexingDB db;
+
+
+    public GameIndexer(Context context){
+        this.mContext = context;
+        mediaTypes = mContext.getResources().getStringArray(R.array.disks);
+        db = new IndexingDB(mContext);
+    }
+
+    public void startupindexingscan() {
+        long startTime = System.currentTimeMillis();
+        List<String> paths = getpath();
+        if (paths.isEmpty()) {
+            HashSet<String> extStorage = getExternalMounts();
+            extStorage.add(Environment.getExternalStorageDirectory().getAbsolutePath());
+            if (extStorage != null && !extStorage.isEmpty()) {
+                for (String anExtStorage : extStorage) {
+                    String sdCardPath = anExtStorage.replace("mnt/media_rw", "storage");
+                    File file = new File(sdCardPath);
+                    walk(file, file.getAbsolutePath(), sdCardPath, 9999);
+                }
+            }
+            Log.i("INDEX", " 1st Runs");
+        } else {
+            for (String path : paths) {
+                File file = new File(path);
+                walk(file, file.getAbsolutePath(), path, 0);
+            }
+            Log.i("INDEX", " 2nd Runs");
+        }
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        Log.i("INDEX", "MY Time " + totalTime);
+    }
+
+    public void fullindexingscan() {
+            HashSet<String> extStorage = getExternalMounts();
+            extStorage.add(Environment.getExternalStorageDirectory().getAbsolutePath());
+            if (extStorage != null && !extStorage.isEmpty()) {
+                for (String anExtStorage : extStorage) {
+                    String sdCardPath = anExtStorage.replace("mnt/media_rw", "storage");
+                    File file = new File(sdCardPath);
+                    walk(file, file.getAbsolutePath(), sdCardPath, 9999);
+                }
+            }
+            Log.i("INDEX", " 1st Runs");
+
+    }
+
+    public void walk(File root, String folderPath, String storage, int depth) {
+        File[] list = root.listFiles();
+        if (list != null && !isNoMedia(list)) {
+            for (File f : list) {
+                if (f.isDirectory() && !f.isHidden() && !isNoMediaFolder(f, storage)) {
+                    if (depth <=0) continue;
+                    depth--;
+                    Log.e("INDEX", "PATH: " + f.getAbsolutePath());
+                    walk(f, f.getAbsolutePath(), storage, depth);
+                    depth++;
+                } else {
+                    if (!f.isHidden() && !f.isDirectory() && isCorrectExtension(f)) {
+                        if (!isIndexed(folderPath, f.getName())) {
+                            String name = f.getName();
+                            String serial = null;
+                            if (!IsLoadableExecutableFileName(f.getPath())) {
+                                serial = getSerial(f);
+                            }
+
+                            ContentValues values = new ContentValues();
+                            values.put(IndexingDB.KEY_DISKNAME, name);
+                            values.put(IndexingDB.KEY_PATH, folderPath);
+                            values.put(IndexingDB.KEY_SERIAL, serial);
+                            values.put(IndexingDB.KEY_SIZE, f.length());
+                            values.put(IndexingDB.KEY_LAST_PLAYED, f.lastModified());
+                            insert(values);
+                            //Log.i("INDEX", "Name: " + name + " PATH: " + folderPath + " Serial: " + serial + " Size " + f.length());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isCorrectExtension(File f) {
+        for (final String type : mediaTypes) {
+            if (!(f.getParentFile().getName().startsWith(".") || f.getName().startsWith("."))) {
+                if (StringUtils.endsWithIgnoreCase(f.getName(), "." + type)) {
+                    String serial = getSerial(f);
+                    return IsLoadableExecutableFileName(f.getPath()) ||
+                            (serial != null && !serial.equals(""));
+                }
+            }
+        }
+        return false;
+    }
+
+    public String getSerial(File game) {
+        String serial = NativeInterop.getDiskId(game.getPath());
+        Log.d("Play!", game.getName() + " [" + serial + "]");
+        return serial;
+    }
+
+    private boolean isIndexed(String absolutePath, String name) {
+        if (db == null){
+            if ((db = new IndexingDB(mContext)) == null){
+                Log.e("PLAY!", "Failed To Initiate IndexDB");
+                return true;
+            }
+        }
+        String selection = IndexingDB.KEY_DISKNAME + "=? AND " + IndexingDB.KEY_PATH + "=?";
+        String[] selectionArgs = {name, absolutePath};
+        return db.isIndexed(selection, selectionArgs);
+
+    }
+
+    private void insert(ContentValues values) {
+        if (db == null){
+            if ((db = new IndexingDB(mContext)) == null){
+                Log.e("PLAY!", "Failed To Initiate IndexDB");
+            }
+        }
+        db.insert(values);
+    }
+
+    private List<ContentValues> getindex() {
+        if (db == null){
+            if ((db = new IndexingDB(mContext)) == null){
+                Log.e("PLAY!", "Failed To Initiate IndexDB");
+            }
+        }
+        return db.getAllIndexList();
+    }
+
+    public List<GameInfoStruct> getindexGameInfoStruct() {
+        if (db == null){
+            if ((db = new IndexingDB(mContext)) == null){
+                Log.e("PLAY!", "Failed To Initiate IndexDB");
+            }
+        }
+        return db.getAllIndexGameInfoStruct();
+    }
+
+    private List<String> getpath() {
+        if (db == null){
+            if ((db = new IndexingDB(mContext)) == null){
+                Log.e("PLAY!", "Failed To Initiate IndexDB");
+            }
+        }
+        return db.getPaths();
+    }
+
+    private boolean isNoMedia(File[] files) {
+        for (File file : files) {
+            if (file.getName().equalsIgnoreCase(".nomedia")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isNoMediaFolder(File file, String storage) {
+        return file.getAbsolutePath().contains(storage + "/Android") ||
+                file.getAbsolutePath().contains(storage + "/LOST.DIR") ||
+                file.getAbsolutePath().contains(storage + "/data");
+
+    }
+}
---
+++ Source/ui_android/java/com/virtualapplications/play/database/IndexingDB.java
@@ -0,0 +1,209 @@
+package com.virtualapplications.play.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.virtualapplications.play.GameInfoStruct;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexingDB extends SQLiteOpenHelper {
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 2;
+
+    // Database Name
+    private static final String DATABASE_NAME = "GameIndex.db";
+
+
+    // Contacts Table Columns names
+    //private static final String KEY_ID = "id";
+    public static final String KEY_ID = "id";
+    public static final String KEY_DISKNAME = "DiskName";
+    public static final String KEY_GAMETITLE = "GameTitle";
+    public static final String KEY_PATH = "Path";
+    public static final String KEY_GAMEID = "GameID";
+    public static final String KEY_OVERVIEW = "Overview";
+    public static final String KEY_SERIAL = "Serial";
+    public static final String KEY_IMAGE = "Image";
+    public static final String KEY_SIZE = "Size";
+    public static final String KEY_LAST_PLAYED = "Last_Played";
+
+    private static final String TABLE_NAME = "Games";
+
+    public IndexingDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_STOPS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_DISKNAME + " TEXT,"
+                + KEY_PATH + " TEXT,"
+                + KEY_GAMEID + " INTEGER,"
+                + KEY_GAMETITLE + " TEXT,"
+                + KEY_OVERVIEW + " TEXT,"
+                + KEY_SERIAL + " TEXT,"
+                + KEY_IMAGE + " TEXT,"
+                + KEY_SIZE + " TEXT,"
+                + KEY_LAST_PLAYED + " INTEGER"
+                + ")";
+        db.execSQL(CREATE_STOPS_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    /**
+     * All CRUD(Create, Read, Update, Delete) Operations
+     */
+
+    void insert(ContentValues values) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Inserting Row
+        db.insert(TABLE_NAME, null, values);
+        db.close(); // Closing database connection
+    }
+
+    ContentValues getGame(String selection, String[] selectionArgs) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, null, selection,
+                selectionArgs, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        ContentValues values = new ContentValues();
+        for (String col : cursor.getColumnNames()){
+            values.put(col, cursor.getString(cursor.getColumnIndex(col)));
+        }
+
+        cursor.close();
+        return values;
+    }
+
+    boolean isIndexed(String selection, String[] selectionArgs) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, null, selection,
+                selectionArgs, null, null, null, null);
+        boolean isGame = false;
+        if (cursor != null && cursor.getCount() > 0)
+            isGame = true;
+
+        cursor.close();
+        return isGame;
+    }
+
+    public List<ContentValues> getAllIndexList() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<ContentValues> IndexList = new ArrayList<>();
+        Cursor cursor = db.query(TABLE_NAME, null, null,
+                null, null, null, null, null);
+        if (cursor != null)
+            if (cursor.moveToFirst()) {
+                do {
+                    ContentValues values = new ContentValues();
+                    for (String col : cursor.getColumnNames()){
+                        values.put(col, cursor.getString(cursor.getColumnIndex(col)));
+                    }
+                    IndexList.add(values);
+                } while (cursor.moveToNext());
+            }
+
+
+        cursor.close();
+        return IndexList;
+    }
+
+    public List<GameInfoStruct> getAllIndexGameInfoStruct() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<GameInfoStruct> IndexList = new ArrayList<>();
+        Cursor cursor = db.query(TABLE_NAME, null, null,
+                null, null, null, null, null);
+        if (cursor != null)
+            if (cursor.moveToFirst()) {
+                do {
+                    String IndexID= cursor.getString(cursor.getColumnIndex(KEY_ID));
+                    String GameID= cursor.getString(cursor.getColumnIndex(KEY_GAMEID));
+                    String GameTitle= cursor.getString(cursor.getColumnIndex(KEY_GAMETITLE));
+                    String FrontCoverLink= cursor.getString(cursor.getColumnIndex(KEY_IMAGE));
+                    String OverView= cursor.getString(cursor.getColumnIndex(KEY_OVERVIEW));
+                    String Path= cursor.getString(cursor.getColumnIndex(KEY_PATH));
+                    String DiskName= cursor.getString(cursor.getColumnIndex(KEY_DISKNAME));
+                    long last_played= cursor.getLong(cursor.getColumnIndex(KEY_LAST_PLAYED));
+                    GameInfoStruct values = new GameInfoStruct(GameID,GameTitle, OverView, FrontCoverLink);
+                    values.setFile(new File(Path, DiskName));
+                    values.setIndexID(IndexID);
+                    values.setlastplayed(last_played);
+                    IndexList.add(values);
+                } while (cursor.moveToNext());
+            }
+
+
+        cursor.close();
+        return IndexList;
+    }
+
+    public int updateIndex(ContentValues values, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // updating row
+        return db.update(TABLE_NAME, values, selection, selectionArgs);
+    }
+
+    public void deleteIndex(String selection, String[] selectionArgs) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, selection, selectionArgs);
+        db.close();
+    }
+
+
+    public int getIndexCount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, null, null,
+                null, null, null, null, null);
+        int count = 0;
+        if (cursor != null)
+            count = cursor.getCount();
+
+        cursor.close();
+        // return count
+        return count;
+    }
+
+    public List<String> getPaths() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(true, TABLE_NAME, new String[]{KEY_PATH}, null, null, KEY_PATH, null, null, null);
+        List<String> paths = new ArrayList<>();
+        if (cursor != null)
+            if (cursor.moveToFirst()) {
+                do {
+                    paths.add(cursor.getString(cursor.getColumnIndex(KEY_PATH)));
+                } while (cursor.moveToNext());
+            }
+
+        cursor.close();
+        return paths;
+    }
+
+}
