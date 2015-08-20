5db5bbf0e1964751f1e5140b37e523ea27d3b450
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-08-20 22:58:18 +0100

    Android: Sample user selected cover

--- Source/ui_android/java/com/virtualapplications/play/CoverEditActivity.java
+++ Source/ui_android/java/com/virtualapplications/play/CoverEditActivity.java
@@ -3,23 +3,20 @@ package com.virtualapplications.play;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.virtualapplications.play.database.GameInfo;
 
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 public class CoverEditActivity extends ActionBarActivity {
 
@@ -94,8 +91,13 @@ public class CoverEditActivity extends ActionBarActivity {
                 if(resultCode == RESULT_OK){
                     try {
                         final Uri imageUri = imageReturnedIntent.getData();
-                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                        selectedImage = BitmapFactory.decodeStream(imageStream);
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inJustDecodeBounds = true;
+                        BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri), null, options);
+
+                        options.inSampleSize = calculateInSampleSize(options);
+                        options.inJustDecodeBounds = false;
+                        selectedImage = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri), null, options);
                         default_cover = false;
                         ((ImageView)findViewById(R.id.game_icon)).setImageBitmap(selectedImage);
                     } catch (FileNotFoundException e) {
@@ -104,6 +106,31 @@ public class CoverEditActivity extends ActionBarActivity {
 
                 }
         }
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        View v = LayoutInflater.from(this).inflate(R.layout.game_list_item, null, false);
+        v.measure(0, 0);
+        int reqWidth = v.findViewById(R.id.game_icon).getMeasuredWidth();
+        int reqHeight = v.findViewById(R.id.game_icon).getMeasuredHeight();
+
+        // TODO: Find a calculated width and height without ImageView
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     @Override
@@ -123,14 +150,12 @@ public class CoverEditActivity extends ActionBarActivity {
                 gis.setDescription(((TextView) findViewById(R.id.editText2)).getText().toString(), getApplicationContext());
                 gi.removeBitmapFromMemCache(intent.getStringExtra("gameid"));
                 if (!default_cover && selectedImage != null){
-                    //TODO check the if statement
                     gi.removeBitmapFromMemCache(intent.getStringExtra("gameid"));
                     gi.saveImage(intent.getStringExtra("gameid") , "-custom", selectedImage);
                 } else if (default_cover) {
-                    //TODO delete custom if exist
                     gi.deleteImage(intent.getStringExtra("gameid") , "-custom");
                 }
-                setResult(RESULT_OK);
+                setResult(RESULT_OK, intent);
                 finish();
                 break;
             case R.id.action_clear_cancel:
