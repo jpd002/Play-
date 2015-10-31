e6dec01eb3b6788852d57ae4c89d383c8f9f1504
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-10-30 22:39:21 -0400

    Make sure input code ids are the same on both C++ and Java sides.
    Makes generic virtual pad implementation easier.

--- Source/ui_android/java/com/virtualapplications/play/InputManagerConstants.java
+++ Source/ui_android/java/com/virtualapplications/play/InputManagerConstants.java
@@ -4,24 +4,25 @@ public class InputManagerConstants
 {
 	//Using public static final ints here because enums can't be 
 	//translated to defines with javah
-	public static final int BUTTON_START 		= 0;
-	public static final int BUTTON_SELECT 		= 1;
-	public static final int BUTTON_CROSS 		= 2;
-	public static final int BUTTON_CIRCLE 		= 3;
-	public static final int BUTTON_SQUARE 		= 4;
-	public static final int BUTTON_TRIANGLE 	= 5;
-	public static final int BUTTON_UP 			= 6;
-	public static final int BUTTON_DOWN			= 7;
-	public static final int BUTTON_LEFT			= 8;
-	public static final int BUTTON_RIGHT		= 9;
-	public static final int BUTTON_L1			= 10;
-	public static final int BUTTON_L2			= 11;
-	public static final int BUTTON_L3			= 12;
-	public static final int BUTTON_R1			= 13;
-	public static final int BUTTON_R2			= 14;
-	public static final int BUTTON_R3			= 15;
-	public static final int ANALOG_LEFT_X		= 16;
-	public static final int ANALOG_LEFT_Y		= 17;
-	public static final int ANALOG_RIGHT_X		= 18;
-	public static final int ANALOG_RIGHT_Y		= 19;
+	public static final int ANALOG_LEFT_X   = 0;
+	public static final int ANALOG_LEFT_Y   = 1;
+	public static final int ANALOG_RIGHT_X  = 2;
+	public static final int ANALOG_RIGHT_Y  = 3;
+	public static final int BUTTON_UP       = 4;
+	public static final int BUTTON_DOWN     = 5;
+	public static final int BUTTON_LEFT     = 6;
+	public static final int BUTTON_RIGHT    = 7;
+	public static final int BUTTON_SELECT   = 8;
+	public static final int BUTTON_START    = 9;
+	public static final int BUTTON_SQUARE   = 10;
+	public static final int BUTTON_TRIANGLE = 11;
+	public static final int BUTTON_CIRCLE   = 12;
+	public static final int BUTTON_CROSS    = 13;
+	public static final int BUTTON_L1       = 14;
+	public static final int BUTTON_L2       = 15;
+	public static final int BUTTON_L3       = 16;
+	public static final int BUTTON_R1       = 17;
+	public static final int BUTTON_R2       = 18;
+	public static final int BUTTON_R3       = 19;
+	public static final int MAX             = 20;
 };
