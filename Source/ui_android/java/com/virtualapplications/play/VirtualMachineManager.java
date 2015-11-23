8d6e81576572312c285d5d983c9b1ef1be573e9b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-11-22 19:00:17 -0500

    Fixed code style.

--- Source/ui_android/java/com/virtualapplications/play/VirtualMachineManager.java
+++ Source/ui_android/java/com/virtualapplications/play/VirtualMachineManager.java
@@ -7,17 +7,19 @@ import java.io.File;
 
 import static com.virtualapplications.play.MainActivity.IsLoadableExecutableFileName;
 
-public class VirtualMachineManager{
-    static void launchDisk(Context mContext, File game) throws Exception {
-        if(IsLoadableExecutableFileName(game.getPath()))
-        {
-            NativeInterop.loadElf(game.getPath());
-        }
-        else
-        {
-            NativeInterop.bootDiskImage(game.getPath());
-        }
-        Intent intent = new Intent(mContext, EmulatorActivity.class);
-        mContext.startActivity(intent);
-    }
+public class VirtualMachineManager
+{
+	static void launchDisk(Context mContext, File game) throws Exception 
+	{
+		if(IsLoadableExecutableFileName(game.getPath()))
+		{
+			NativeInterop.loadElf(game.getPath());
+		}
+		else
+		{
+			NativeInterop.bootDiskImage(game.getPath());
+		}
+		Intent intent = new Intent(mContext, EmulatorActivity.class);
+		mContext.startActivity(intent);
+	}
 }
