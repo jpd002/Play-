578904c146e68bdd69ae9b84ab53a25ab525b1c2
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-01-01 18:34:44 -0500

    Added restriction on case where framebuffer can be used as texture with an offset.

--- Source/gs/GSH_OpenGL/GSH_OpenGL_Texture.cpp
+++ Source/gs/GSH_OpenGL/GSH_OpenGL_Texture.cpp
@@ -93,6 +93,7 @@ CGSH_OpenGL::TEXTURE_INFO CGSH_OpenGL::PrepareTexture(const TEX0& tex0)
 
 		//Another case: TEX0 is pointing to the start of a page within our framebuffer (BGDA does this)
 		else if(candidateFramebuffer->m_basePtr <= tex0.GetBufPtr() &&
+			candidateFramebuffer->m_width == tex0.GetBufWidth() &&
 			candidateFramebuffer->m_psm == tex0.nPsm)
 		{
 			uint32 framebufferOffset = tex0.GetBufPtr() - candidateFramebuffer->m_basePtr;
