bc3aeed2d85ffd565611717d5f6af483ad3e0cfc
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-08 09:44:41 -0500

    Cleanup.

--- Source/iop/Iop_Module.h
+++ Source/iop/Iop_Module.h
@@ -9,12 +9,10 @@ namespace Iop
 	class CModule
 	{
 	public:
-		virtual					~CModule() {}
+		virtual					~CModule() = default;
 		virtual std::string		GetId() const = 0;
 		virtual std::string		GetFunctionName(unsigned int) const = 0;
 		virtual void			Invoke(CMIPS&, unsigned int) = 0;
-
-	private:
 
 	};
 
