ae4d31d0d1fbbfc64d2d05ea9cb4d105c1fd7ecd
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-23 13:21:20 -0500

    More test

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,21 @@
 #/bin/bash
 
-travis_before_install() {
+travis_before_install() 
+{
+    cd ~
+    ls
     git clone https://github.com/jpd002/Play-Build.git Play-Build
-    cd Play-Build
+    pushd Play-Build
     git submodule update --init --recursive
     git submodule foreach "git checkout master"
+    #rm -rf Play
+    #mv ../Play- Play
+    popd
+}
+
+travis_script() 
+{
+   
 }
 
 set -e
