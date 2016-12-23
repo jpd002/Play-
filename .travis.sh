4d83dd1bafa158bae8fe80b4c5601ab7462b3c24
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-23 13:12:48 -0500

    Another update.

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,7 @@
 
 travis_before_install() {
     git clone https://github.com/jpd002/Play-Build.git Play-Build
+    cd Play-Build
     git submodule update --init --recursive
     git submodule foreach "git checkout master"
 }
