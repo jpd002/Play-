f9bd7aa44d508e465ae7030d7e8b3c564a10b2f8
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-23 14:35:40 -0500

    Test apt-get

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,8 @@
 
 travis_before_install() 
 {
+    sudo apt-get update -qq
+    sudo apt-get install cmake
     cd ..
     git clone https://github.com/jpd002/Play-Build.git Play-Build
     pushd Play-Build
