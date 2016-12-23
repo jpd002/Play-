c516fd162c472be84cac8e05bfd119b87de1b247
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-23 14:48:16 -0500

    Test Again.

--- .travis.sh
+++ .travis.sh
@@ -2,8 +2,8 @@
 
 travis_before_install() 
 {
-    sudo apt-get update -qq
-    sudo apt-get install cmake libboost-system-dev libboost-chrono-dev
+#    sudo apt-get update -qq
+#    sudo apt-get install cmake libboost-system-dev libboost-chrono-dev
     cd ..
     git clone https://github.com/jpd002/Play-Build.git Play-Build
     pushd Play-Build
