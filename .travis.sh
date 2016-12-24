42ffb8c47e2e3691dc9b4bd9c75d0af9cc470867
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-24 00:22:59 -0500

    Different packages.

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ travis_before_install()
 {
     sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
     sudo apt-get update
-    sudo apt-get install qt5-qmake qtbase5-dev
+    sudo apt-get install qt57base
     cd ..
     git clone https://github.com/jpd002/Play-Build.git Play-Build
     pushd Play-Build
