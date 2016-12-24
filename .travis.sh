ed2ae2abe60df24a7d3366060802cf8597d3a6b3
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-12-24 14:14:14 -0500

    Fix fail.

--- .travis.sh
+++ .travis.sh
@@ -3,27 +3,22 @@
 travis_before_install() 
 {
     sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
-    sudo apt-get update
-    sudo apt-get install qt57base
+    sudo apt-get update -qq
+    sudo apt-get install -qq qt57base
     cd ..
-    git clone https://github.com/jpd002/Play-Build.git Play-Build
+    git clone -q https://github.com/jpd002/Play-Build.git Play-Build
     pushd Play-Build
     git submodule update --init --recursive
-    git submodule foreach "git checkout master"
+    git submodule foreach "git checkout -q master"
     rm -rf Play
     mv ../Play- Play
     popd
 }
 
-travis_install()
+travis_script()
 {
     if [ "$CXX" = "g++" ]; then export CXX="g++-5" CC="gcc-5"; fi
     source /opt/qt57/bin/qt57-env.sh || true
-    qmake --version
-}
-
-travis_script()
-{
     cd build_unix
     ./build.sh
 }
