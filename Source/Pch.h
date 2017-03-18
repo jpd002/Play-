a865916964aa445b8825c8e478a9f21e1bac50c3
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-03-17 21:07:22 -0400

    Tweak precompiled header usage in CMake project.

---
+++ Source/Pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include <boost/signals2.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/filesystem.hpp>
+
+#include "Types.h"
+#include "Stream.h"
+#include "StdStream.h"
+#include "xml/Node.h"
+#include "make_unique.h"
