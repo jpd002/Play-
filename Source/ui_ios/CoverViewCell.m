69a57225a32448a0d6351b933e39eda3e5e4d069
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-10-30 13:21:29 -0400

    Cleanup.

--- Source/ui_ios/CoverViewCell.m
+++ Source/ui_ios/CoverViewCell.m
@@ -2,34 +2,29 @@
 
 @implementation CoverViewCell
 
-- (id)initWithFrame:(CGRect)frame
+-(id)initWithFrame: (CGRect)frame
 {
+	self = [super initWithFrame: frame];
 	
-	self = [super initWithFrame:frame];
-	
-	if (self) {
+	if(self)
+	{
 		// Initialization code
-		NSArray *arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"coverCell" owner:self options:nil];
+		NSArray* arrayOfViews = [[NSBundle mainBundle] loadNibNamed: @"coverCell" owner: self options: nil];
 		
-		if ([arrayOfViews count] < 1) {
+		if([arrayOfViews count] < 1)
+		{
 			return nil;
 		}
 		
-		if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]]) {
+		if(![[arrayOfViews objectAtIndex: 0] isKindOfClass: [UICollectionViewCell class]])
+		{
 			return nil;
 		}
 		
-		self = [arrayOfViews objectAtIndex:0];
-		
+		self = [arrayOfViews objectAtIndex: 0];
 	}
 	
 	return self;
-	
-}
-
-- (void)awakeFromNib
-{
-    // Initialization code
 }
 
 @end
