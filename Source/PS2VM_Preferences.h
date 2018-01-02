6b76b83b90039c3b26a76248ad15be1df5cdfcd0
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-01-02 13:01:06 -0500

    Rename directory devices preference names to allow smooth transition.

--- Source/PS2VM_Preferences.h
+++ Source/PS2VM_Preferences.h
@@ -2,6 +2,6 @@
 
 #define PREF_PS2_CDROM0_PATH       ("ps2.cdrom0.path.v2")
 
-#define PREF_PS2_HOST_DIRECTORY    ("ps2.host.directory")
-#define PREF_PS2_MC0_DIRECTORY     ("ps2.mc0.directory")
-#define PREF_PS2_MC1_DIRECTORY     ("ps2.mc1.directory")
+#define PREF_PS2_HOST_DIRECTORY    ("ps2.host.directory.v2")
+#define PREF_PS2_MC0_DIRECTORY     ("ps2.mc0.directory.v2")
+#define PREF_PS2_MC1_DIRECTORY     ("ps2.mc1.directory.v2")
