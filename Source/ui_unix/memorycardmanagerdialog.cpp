8a2b89fe5de69ee46b94fce6beac0d2431253820
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-01-10 14:58:25 -0500

    Apply codestyle.

--- Source/ui_unix/memorycardmanagerdialog.cpp
+++ Source/ui_unix/memorycardmanagerdialog.cpp
@@ -4,198 +4,202 @@
 
 #include "StdStream.h"
 #include "StdStreamUtils.h"
-#include <QMessageBox>
-#include <QDateTime>
 #include <AppConfig.h>
+#include <QDateTime>
+#include <QMessageBox>
 
-
-MemoryCardManagerDialog::MemoryCardManagerDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::MemoryCardManagerDialog),
-    m_MemoryCard0(CAppConfig::GetInstance().GetPreferenceString("ps2.mc0.directory")),
-    m_MemoryCard1(CAppConfig::GetInstance().GetPreferenceString("ps2.mc1.directory"))
+MemoryCardManagerDialog::MemoryCardManagerDialog(QWidget* parent)
+    : QDialog(parent)
+    , ui(new Ui::MemoryCardManagerDialog)
+    , m_MemoryCard0(CAppConfig::GetInstance().GetPreferenceString("ps2.mc0.directory"))
+    , m_MemoryCard1(CAppConfig::GetInstance().GetPreferenceString("ps2.mc1.directory"))
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
-    m_pMemoryCard[0] = &m_MemoryCard0;
-    m_pMemoryCard[1] = &m_MemoryCard1;
+	m_pMemoryCard[0] = &m_MemoryCard0;
+	m_pMemoryCard[1] = &m_MemoryCard1;
 
-    m_pCurrentMemoryCard = m_pMemoryCard[0];
+	m_pCurrentMemoryCard = m_pMemoryCard[0];
 
-    populateSaveList();
+	populateSaveList();
 
-    ui->label_name->setWordWrap(true);
+	ui->label_name->setWordWrap(true);
 }
 
 MemoryCardManagerDialog::~MemoryCardManagerDialog()
 {
-    delete ui;
+	delete ui;
 }
 
 void MemoryCardManagerDialog::on_import_saves_button_clicked()
 {
-    QFileDialog dialog(this);
-    dialog.setDirectory(m_lastpath);
-    dialog.setFileMode(QFileDialog::ExistingFiles);
-    dialog.setNameFilter(tr("All Supported types (*.psu *.sps *.xps *.max);;EMS Memory Adapter Save Dumps (*.psu);;Sharkport/X-Port Save Dumps (*.sps; *.xps);;Action Replay MAX Save Dumps (*.max);;All files (*.*)"));
-    if (dialog.exec())
-    {
-        QString fileName = dialog.selectedFiles().first();
-        m_lastpath = QFileInfo(fileName).path();
+	QFileDialog dialog(this);
+	dialog.setDirectory(m_lastpath);
+	dialog.setFileMode(QFileDialog::ExistingFiles);
+	dialog.setNameFilter(tr("All Supported types (*.psu *.sps *.xps *.max);;EMS Memory Adapter Save Dumps (*.psu);;Sharkport/X-Port Save Dumps (*.sps; *.xps);;Action Replay MAX Save Dumps (*.max);;All files (*.*)"));
+	if(dialog.exec())
+	{
+		QString fileName = dialog.selectedFiles().first();
+		m_lastpath = QFileInfo(fileName).path();
 
-        try
-        {
-            auto input(Framework::CStdStream(fileName.toStdString().c_str(), "r"));
-            CSaveImporter::ImportSave(input, m_pCurrentMemoryCard->GetBasePath(),
-                                      std::bind(&MemoryCardManagerDialog::OnImportOverwrite, this, std::placeholders::_1));
-        }
-        catch(const std::exception& Exception)
-        {
-            QString msg("Couldn't import save(s):\n\n%1");
-            QMessageBox messageBox;
-            messageBox.critical(this,"Error",msg.arg(Exception.what()));
-            messageBox.show();
-            return;
-        }
+		try
+		{
+			auto input(Framework::CStdStream(fileName.toStdString().c_str(), "r"));
+			CSaveImporter::ImportSave(input, m_pCurrentMemoryCard->GetBasePath(),
+			                          std::bind(&MemoryCardManagerDialog::OnImportOverwrite, this, std::placeholders::_1));
+		}
+		catch(const std::exception& Exception)
+		{
+			QString     msg("Couldn't import save(s):\n\n%1");
+			QMessageBox messageBox;
+			messageBox.critical(this, "Error", msg.arg(Exception.what()));
+			messageBox.show();
+			return;
+		}
 
-        m_pCurrentMemoryCard->RefreshContents();
-        populateSaveList();
-    }
+		m_pCurrentMemoryCard->RefreshContents();
+		populateSaveList();
+	}
 }
 
 CSaveImporterBase::OVERWRITE_PROMPT_RETURN MemoryCardManagerDialog::OnImportOverwrite(const boost::filesystem::path& filePath)
 {
-    std::string fileName = filePath.leaf().string();
-    QString msg("File %1 already exists.\n\nOverwrite?");
-    QMessageBox::StandardButton resBtn = QMessageBox::question(this, "Overwrite?",
-                                                               msg.arg(fileName.c_str()),
-                                                               QMessageBox::Yes | QMessageBox::No,
-                                                               QMessageBox::Yes);
-    return (resBtn == QMessageBox::Yes) ? CSaveImporterBase::OVERWRITE_YES : CSaveImporterBase::OVERWRITE_NO;
+	std::string                 fileName = filePath.leaf().string();
+	QString                     msg("File %1 already exists.\n\nOverwrite?");
+	QMessageBox::StandardButton resBtn = QMessageBox::question(this, "Overwrite?",
+	                                                           msg.arg(fileName.c_str()),
+	                                                           QMessageBox::Yes | QMessageBox::No,
+	                                                           QMessageBox::Yes);
+	return (resBtn == QMessageBox::Yes) ? CSaveImporterBase::OVERWRITE_YES : CSaveImporterBase::OVERWRITE_NO;
 }
 
 void MemoryCardManagerDialog::on_comboBox_currentIndexChanged(int index)
 {
-
-    m_pCurrentMemoryCard = m_pMemoryCard[index];
-    populateSaveList();
+	m_pCurrentMemoryCard = m_pMemoryCard[index];
+	populateSaveList();
 }
 
 void MemoryCardManagerDialog::populateSaveList()
 {
-    unsigned int nItemCount = static_cast<unsigned int>(m_pCurrentMemoryCard->GetSaveCount());
-    ui->savelistWidget->clear();
+	unsigned int nItemCount = static_cast<unsigned int>(m_pCurrentMemoryCard->GetSaveCount());
+	ui->savelistWidget->clear();
 
-    for(unsigned int i = 0; i < nItemCount; i++)
-    {
-        QString name = QString::fromWCharArray(m_pCurrentMemoryCard->GetSaveByIndex(i)->GetName());
-        ui->savelistWidget->addItem(name);
-    }
+	for(unsigned int i = 0; i < nItemCount; i++)
+	{
+		QString name = QString::fromWCharArray(m_pCurrentMemoryCard->GetSaveByIndex(i)->GetName());
+		ui->savelistWidget->addItem(name);
+	}
 }
 
 void MemoryCardManagerDialog::on_savelistWidget_currentRowChanged(int currentRow)
 {
-    int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
-    if (currentRow >=0 && currentRow <= nItemCount)
-    {
-        const CSave *save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
-        QDateTime* dt = new QDateTime;
-        dt->setTime_t(save->GetLastModificationTime());
-        QString datetime = dt->toUTC().toString("hh:mm - dd.MM.yyyy");
+	int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
+	if(currentRow >= 0 && currentRow <= nItemCount)
+	{
+		const CSave* save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
+		QDateTime*   dt = new QDateTime;
+		dt->setTime_t(save->GetLastModificationTime());
+		QString datetime = dt->toUTC().toString("hh:mm - dd.MM.yyyy");
 
-
-        ui->label_name->setText(QString::fromWCharArray(save->GetName()));
-        ui->label_name->setMinimumSize(ui->label_name->sizeHint());
-        ui->label_id->setText(QString(save->GetId()));
-        QString size("%1kb");
-        ui->label_size->setText(size.arg(QString::number(save->GetSize()/1000)));
-        ui->label_last_mod->setText(datetime);
-        ui->delete_save_button->setEnabled(true);
-        ui->export_save_button->setEnabled(true);
-    } else {
-        ui->label_name->setText("--");
-        ui->label_id->setText("--");
-        ui->label_size->setText("--");
-        ui->label_last_mod->setText("--");
-        ui->delete_save_button->setEnabled(false);
-        ui->export_save_button->setEnabled(false);
-    }
+		ui->label_name->setText(QString::fromWCharArray(save->GetName()));
+		ui->label_name->setMinimumSize(ui->label_name->sizeHint());
+		ui->label_id->setText(QString(save->GetId()));
+		QString size("%1kb");
+		ui->label_size->setText(size.arg(QString::number(save->GetSize() / 1000)));
+		ui->label_last_mod->setText(datetime);
+		ui->delete_save_button->setEnabled(true);
+		ui->export_save_button->setEnabled(true);
+	}
+	else
+	{
+		ui->label_name->setText("--");
+		ui->label_id->setText("--");
+		ui->label_size->setText("--");
+		ui->label_last_mod->setText("--");
+		ui->delete_save_button->setEnabled(false);
+		ui->export_save_button->setEnabled(false);
+	}
 }
 
 void MemoryCardManagerDialog::on_delete_save_button_clicked()
 {
-    QMessageBox::StandardButton resBtn = QMessageBox::question(this, "Overwrite?",
-                                                               "Are you sure you want to delete the currently selected entry",
-                                                               QMessageBox::Yes | QMessageBox::No,
-                                                               QMessageBox::Yes);
-    if (resBtn == QMessageBox::Yes)
-    {
-        int currentRow = ui->savelistWidget->currentRow();
-        int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
-        if (currentRow >= 0  && currentRow <= nItemCount)
-        {
-            const CSave *save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
-            boost::filesystem::remove_all(save->GetPath());
-            m_pCurrentMemoryCard->RefreshContents();
-            populateSaveList();
-        }
-    }
+	QMessageBox::StandardButton resBtn = QMessageBox::question(this, "Overwrite?",
+	                                                           "Are you sure you want to delete the currently selected entry",
+	                                                           QMessageBox::Yes | QMessageBox::No,
+	                                                           QMessageBox::Yes);
+	if(resBtn == QMessageBox::Yes)
+	{
+		int currentRow = ui->savelistWidget->currentRow();
+		int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
+		if(currentRow >= 0 && currentRow <= nItemCount)
+		{
+			const CSave* save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
+			boost::filesystem::remove_all(save->GetPath());
+			m_pCurrentMemoryCard->RefreshContents();
+			populateSaveList();
+		}
+	}
 }
 
 void MemoryCardManagerDialog::on_export_save_button_clicked()
 {
+	int currentRow = ui->savelistWidget->currentRow();
+	int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
+	if(currentRow >= 0 && currentRow <= nItemCount)
+	{
+		const CSave* save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
+		if(save != NULL)
+		{
+			QFileDialog dialog(this);
+			dialog.setAcceptMode(QFileDialog::AcceptSave);
+			dialog.setNameFilter(tr("EMS Memory Adapter Save Dumps (*.psu)"));
+			dialog.setDefaultSuffix("psu");
+			if(dialog.exec())
+			{
+				QString fileName = dialog.selectedFiles().first();
 
-    int currentRow = ui->savelistWidget->currentRow();
-    int nItemCount = static_cast<int>(m_pCurrentMemoryCard->GetSaveCount() - 1);
-    if (currentRow >= 0  && currentRow <= nItemCount)
-    {
-        const CSave *save = m_pCurrentMemoryCard->GetSaveByIndex(currentRow);
-        if(save != NULL)
-        {
-            QFileDialog dialog(this);
-            dialog.setAcceptMode(QFileDialog::AcceptSave);
-            dialog.setNameFilter(tr("EMS Memory Adapter Save Dumps (*.psu)"));
-            dialog.setDefaultSuffix("psu");
-            if (dialog.exec())
-            {
-                QString fileName = dialog.selectedFiles().first();
+				try
+				{
+					auto output(Framework::CreateOutputStdStream(fileName.toStdString()));
+					CSaveExporter::ExportPSU(output, save->GetPath());
+				}
+				catch(const std::exception& Exception)
+				{
+					QString     msg("Couldn't export save(s):\n\n%1");
+					QMessageBox messageBox;
+					messageBox.critical(this, "Error", msg.arg(Exception.what()));
+					messageBox.show();
+					return;
+				}
 
-                try
-                {
-                    auto output(Framework::CreateOutputStdStream(fileName.toStdString()));
-                    CSaveExporter::ExportPSU(output, save->GetPath());
-                }
-                catch(const std::exception& Exception)
-                {
-                    QString msg("Couldn't export save(s):\n\n%1");
-                    QMessageBox messageBox;
-                    messageBox.critical(this,"Error",msg.arg(Exception.what()));
-                    messageBox.show();
-                    return;
-                }
-
-                QString msg("Save exported successfully.");
-                QMessageBox messageBox;
-                messageBox.information(this,"Success",msg);
-                messageBox.show();
-                return;
-            } else {
-                QString msg("Save export Cancelled.");
-                QMessageBox messageBox;
-                messageBox.warning(this,"Cancelled",msg);
-                messageBox.show();
-                return;
-            }
-        } else {
-            QString msg("Save not found,\nPlease try again.");
-            QMessageBox messageBox;
-            messageBox.critical(this,"Error",msg);
-            messageBox.show();
-        }
-    } else {
-        QString msg("Invalid selection,\nPlease try again.");
-        QMessageBox messageBox;
-        messageBox.critical(this,"Error",msg);
-        messageBox.show();
-    }
+				QString     msg("Save exported successfully.");
+				QMessageBox messageBox;
+				messageBox.information(this, "Success", msg);
+				messageBox.show();
+				return;
+			}
+			else
+			{
+				QString     msg("Save export Cancelled.");
+				QMessageBox messageBox;
+				messageBox.warning(this, "Cancelled", msg);
+				messageBox.show();
+				return;
+			}
+		}
+		else
+		{
+			QString     msg("Save not found,\nPlease try again.");
+			QMessageBox messageBox;
+			messageBox.critical(this, "Error", msg);
+			messageBox.show();
+		}
+	}
+	else
+	{
+		QString     msg("Invalid selection,\nPlease try again.");
+		QMessageBox messageBox;
+		messageBox.critical(this, "Error", msg);
+		messageBox.show();
+	}
 }
--- Source/ui_unix/memorycardmanagerdialog.h
+++ Source/ui_unix/memorycardmanagerdialog.h
@@ -1,43 +1,44 @@
 #ifndef MEMORYCARDMANAGERDIALOG_H
 #define MEMORYCARDMANAGERDIALOG_H
 
+#include "saves/MemoryCard.h"
+#include "saves/SaveExporter.h"
+#include "saves/SaveImporter.h"
 #include <QDialog>
 #include <QDir>
 #include <boost/filesystem.hpp>
-#include "saves/SaveImporter.h"
-#include "saves/SaveExporter.h"
-#include "saves/MemoryCard.h"
 
-namespace Ui {
+namespace Ui
+{
 class MemoryCardManagerDialog;
 }
 
 class MemoryCardManagerDialog : public QDialog
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MemoryCardManagerDialog(QWidget *parent = 0);
-    ~MemoryCardManagerDialog();
+	explicit MemoryCardManagerDialog(QWidget* parent = 0);
+	~MemoryCardManagerDialog();
 
 private:
-    CSaveImporterBase::OVERWRITE_PROMPT_RETURN OnImportOverwrite(const boost::filesystem::path&);
-    void populateSaveList();
+	CSaveImporterBase::OVERWRITE_PROMPT_RETURN OnImportOverwrite(const boost::filesystem::path&);
+	void                                       populateSaveList();
 
-    Ui::MemoryCardManagerDialog *ui;
-    CMemoryCard									m_MemoryCard0;
-    CMemoryCard									m_MemoryCard1;
-    CMemoryCard*								m_pMemoryCard[2];
-    CMemoryCard*								m_pCurrentMemoryCard;
+	Ui::MemoryCardManagerDialog* ui;
+	CMemoryCard                  m_MemoryCard0;
+	CMemoryCard                  m_MemoryCard1;
+	CMemoryCard*                 m_pMemoryCard[2];
+	CMemoryCard*                 m_pCurrentMemoryCard;
 
-    QString                                     m_lastpath = QDir::homePath();
+	QString m_lastpath = QDir::homePath();
 
 private slots:
-    void on_import_saves_button_clicked();
-    void on_comboBox_currentIndexChanged(int index);
-    void on_savelistWidget_currentRowChanged(int currentRow);
-    void on_delete_save_button_clicked();
-    void on_export_save_button_clicked();
+	void on_import_saves_button_clicked();
+	void on_comboBox_currentIndexChanged(int index);
+	void on_savelistWidget_currentRowChanged(int currentRow);
+	void on_delete_save_button_clicked();
+	void on_export_save_button_clicked();
 };
 
 #endif // MEMORYCARDMANAGERDIALOG_H
