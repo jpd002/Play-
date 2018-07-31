669fd2268654ee5aef5841f176e5f7c6e489cc01
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-07-31 12:41:12 -0400

    Add ptrace call on iOS to simulate debugger.
    This allows JITed code to work even though the application is not running through Xcode.

--- Source/ui_ios/main.mm
+++ Source/ui_ios/main.mm
@@ -1,8 +1,19 @@
 #import <UIKit/UIKit.h>
+#import <dlfcn.h>
 #import "AppDelegate.h"
+
+#define PT_TRACE_ME 0
+typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
+
+static void SimulateDebugger()
+{
+	auto ptrace_ptr = reinterpret_cast<ptrace_ptr_t>(dlsym(RTLD_SELF, "ptrace"));
+	ptrace_ptr(PT_TRACE_ME, 0, NULL, 0);
+}
 
 int main(int argc, char * argv[])
 {
+	SimulateDebugger();
 	@autoreleasepool
 	{
 		return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
