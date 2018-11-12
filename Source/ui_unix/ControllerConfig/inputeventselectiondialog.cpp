e1601cd49437ea2ea090d36056da9097e3fd1d31
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-11-12 13:15:08 -0500

    Add sleep to prevent high CPU usage.

--- Source/ui_unix/ControllerConfig/inputeventselectiondialog.cpp
+++ Source/ui_unix/ControllerConfig/inputeventselectiondialog.cpp
@@ -270,6 +270,7 @@ void InputEventSelectionDialog::CountDownThreadLoop()
 		{
 			setCountDownLabelText(m_countingtext.arg(3));
 		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
 
