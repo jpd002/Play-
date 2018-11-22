780697232845944fc42d75350bfd5a69eccf2dae
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-11-21 19:55:38 -0500

    Make simulated axis binding work again.

--- Source/ui_unix/ControllerConfig/inputeventselectiondialog.cpp
+++ Source/ui_unix/ControllerConfig/inputeventselectiondialog.cpp
@@ -31,11 +31,13 @@ InputEventSelectionDialog::~InputEventSelectionDialog()
 void InputEventSelectionDialog::Setup(const char* text, CInputBindingManager* inputManager, CInputProviderQtKey* qtKeyInputProvider, PS2::CControllerInfo::BUTTON button)
 {
 	m_inputManager = inputManager;
-	m_inputManager->OverrideInputEventHandler([this] (auto target, auto value) { this->onInputEvent(target, value); } );
-	connect(this, SIGNAL(countdownComplete()), this, SLOT(completeSimpleBinding()));
-	m_qtKeyInputProvider = qtKeyInputProvider;
-	ui->bindinglabel->setText(m_bindingText.arg(text));
 	m_button = button;
+	m_buttonName = QString::fromUtf8(text);
+	m_qtKeyInputProvider = qtKeyInputProvider;
+	ui->bindinglabel->setText(m_bindingText.arg(m_buttonName));
+
+	m_inputManager->OverrideInputEventHandler([this] (auto target, auto value) { this->onInputEvent(target, value); } );
+	connect(this, SIGNAL(countdownComplete()), this, SLOT(confirmBinding()));
 }
 
 static bool IsAxisIdle(uint32 value)
@@ -62,34 +64,72 @@ void InputEventSelectionDialog::onInputEvent(const BINDINGTARGET& target, uint32
 		[this] ()
 		{
 			m_selectedTarget = BINDINGTARGET();
-			m_state = STATE::NONE;
+			m_state = STATE::WAITING;
 			m_bindingType = CInputBindingManager::BINDING_UNBOUND;
 			cancelCountdown();
 		};
 	
+	auto setSelectionSimulatedAxis =
+		[this] (const auto& target)
+		{
+			m_selectedTargetSimulatedAxis = target;
+			m_state = STATE::SIMULATEDAXIS_SELECTED;
+			auto targetDescription = m_inputManager->GetTargetDescription(target);
+			startCountdown(QString::fromStdString(targetDescription));
+		};
+	
+	auto resetSelectionSimulatedAxis =
+		[this] ()
+		{
+			m_selectedTargetSimulatedAxis = BINDINGTARGET();
+			m_state = STATE::SIMULATEDAXIS_WAITING;
+			cancelCountdown();
+		};
+
 	switch(m_state)
 	{
-	case STATE::NONE:
+	case STATE::WAITING:
 		//Check if we've pressed something
-		switch(target.keyType)
+		if(PS2::CControllerInfo::IsAxis(m_button))
 		{
-		case BINDINGTARGET::KEYTYPE::BUTTON:
-			if(value != 0)
+			switch(target.keyType)
 			{
-				setSelection(CInputBindingManager::BINDING_SIMPLE, target);
+			case BINDINGTARGET::KEYTYPE::BUTTON:
+				if(value != 0)
+				{
+					setSelection(CInputBindingManager::BINDING_SIMULATEDAXIS, target);
+				}
+				break;
+			case BINDINGTARGET::KEYTYPE::AXIS:
+				if(!IsAxisIdle(value))
+				{
+					setSelection(CInputBindingManager::BINDING_SIMPLE, target);
+				}
+				break;
 			}
-			break;
-		case BINDINGTARGET::KEYTYPE::AXIS:
-			if(!IsAxisIdle(value))
+		}
+		else
+		{
+			switch(target.keyType)
 			{
-				setSelection(CInputBindingManager::BINDING_SIMPLE, target);
-			}
-			break;
-		case BINDINGTARGET::KEYTYPE::POVHAT:
-			if(value < BINDINGTARGET::POVHAT_MAX)
-			{
-				m_bindingValue = value;
-				setSelection(CInputBindingManager::BINDING_POVHAT, target);
+			case BINDINGTARGET::KEYTYPE::BUTTON:
+				if(value != 0)
+				{
+					setSelection(CInputBindingManager::BINDING_SIMPLE, target);
+				}
+				break;
+			case BINDINGTARGET::KEYTYPE::AXIS:
+				if(!IsAxisIdle(value))
+				{
+					setSelection(CInputBindingManager::BINDING_SIMPLE, target);
+				}
+				break;
+			case BINDINGTARGET::KEYTYPE::POVHAT:
+				if(value < BINDINGTARGET::POVHAT_MAX)
+				{
+					m_bindingValue = value;
+					setSelection(CInputBindingManager::BINDING_POVHAT, target);
+				}
 			}
 		}
 		break;
@@ -116,6 +156,19 @@ void InputEventSelectionDialog::onInputEvent(const BINDINGTARGET& target, uint32
 			}
 		}
 		break;
+	case STATE::SIMULATEDAXIS_WAITING:
+		if((target.keyType == BINDINGTARGET::KEYTYPE::BUTTON) && (value != 0))
+		{
+			setSelectionSimulatedAxis(target);
+		}
+		break;
+	case STATE::SIMULATEDAXIS_SELECTED:
+		if(target != m_selectedTargetSimulatedAxis) break;
+		if(value == 0)
+		{
+			resetSelectionSimulatedAxis();
+		}
+		break;
 	}
 }
 
@@ -126,6 +179,7 @@ void InputEventSelectionDialog::keyPressEvent(QKeyEvent* ev)
 		reject();
 		return;
 	}
+	if(ev->isAutoRepeat()) return;
 	m_qtKeyInputProvider->OnKeyPress(ev->key());
 }
 
@@ -164,16 +218,31 @@ void InputEventSelectionDialog::updateCountdown()
 	}
 }
 
-void InputEventSelectionDialog::completeSimpleBinding()
+void InputEventSelectionDialog::confirmBinding()
 {
 	switch(m_bindingType)
 	{
-		case CInputBindingManager::BINDING_SIMPLE:
-			m_inputManager->SetSimpleBinding(0, m_button, m_selectedTarget);
-			break;
-		case CInputBindingManager::BINDING_POVHAT:
-			m_inputManager->SetPovHatBinding(0, m_button, m_selectedTarget, m_bindingValue);
-			break;
+	case CInputBindingManager::BINDING_SIMPLE:
+		m_inputManager->SetSimpleBinding(0, m_button, m_selectedTarget);
+		accept();
+		break;
+	case CInputBindingManager::BINDING_POVHAT:
+		m_inputManager->SetPovHatBinding(0, m_button, m_selectedTarget, m_bindingValue);
+		accept();
+		break;
+	case CInputBindingManager::BINDING_SIMULATEDAXIS:
+		if(m_state == STATE::SELECTED)
+		{
+			m_state = STATE::SIMULATEDAXIS_WAITING;
+			ui->countdownlabel->setText(m_countingText.arg(COUNTDOWN_SECS));
+			ui->bindinglabel->setText(m_nextbindingText.arg(m_buttonName));
+			ui->selectedbuttonlabel->setText(m_selectedkeyText.arg(QString("None")));
+		}
+		else if(m_state == STATE::SIMULATEDAXIS_SELECTED)
+		{
+			m_inputManager->SetSimulatedAxisBinding(0, m_button, m_selectedTarget, m_selectedTargetSimulatedAxis);
+			accept();
+		}
+		break;
 	}
-	accept();
 }
