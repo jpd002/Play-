cbebb25c17b4e53e25e1a8c16a475ff001420a18
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-11-28 18:37:31 -0500

    Relay EvDev events.

--- Source/ui_unix/unix/InputProviderEvDev.cpp
+++ Source/ui_unix/unix/InputProviderEvDev.cpp
@@ -4,11 +4,7 @@
 #define PROVIDER_ID 'evdv'
 
 CInputProviderEvDev::CInputProviderEvDev()
-{
-
-}
-
-CInputProviderEvDev::~CInputProviderEvDev()
+	: m_GPDL([this] (auto a, auto b, auto c, auto d, auto e){ this->OnEvDevInputEvent(a, b, c, d, e); })
 {
 
 }
@@ -21,4 +17,49 @@ uint32 CInputProviderEvDev::GetId() const
 std::string CInputProviderEvDev::GetTargetDescription(const BINDINGTARGET& target) const
 {
 	return string_format("EvDev: btn-%d", target.keyId);
+}
+
+void CInputProviderEvDev::OnEvDevInputEvent(GamePadDeviceId deviceId, int code, int value, int type, const input_absinfo* abs)
+{
+	BINDINGTARGET tgt;
+	tgt.providerId = PROVIDER_ID;
+	tgt.deviceId = deviceId;
+	tgt.keyId = code;
+	if(type == EV_MSC) 
+	{
+		return;
+	}
+	else if(type == EV_KEY)
+	{
+		tgt.keyType = BINDINGTARGET::KEYTYPE::BUTTON;
+		OnInput(tgt, value);
+	}
+	else if(type == EV_ABS)
+	{
+		int fixedValue = value;
+		if(code == ABS_HAT0X || code == ABS_HAT0Y)
+		{
+			tgt.keyType = BINDINGTARGET::KEYTYPE::POVHAT;
+			switch(value)
+			{
+			case 0:
+				fixedValue = BINDINGTARGET::POVHAT_MAX;
+				break;
+			case 1:
+				fixedValue = 0;
+				break;
+			case -1:
+				fixedValue = 4;
+				break;
+			default:
+				assert(false);
+				break;
+			}
+		}
+		else
+		{
+			tgt.keyType = BINDINGTARGET::KEYTYPE::AXIS;
+		}
+		OnInput(tgt, fixedValue);
+	}
 }
--- Source/ui_unix/unix/InputProviderEvDev.h
+++ Source/ui_unix/unix/InputProviderEvDev.h
@@ -1,17 +1,19 @@
 #pragma once
 
 #include "input/InputProvider.h"
+#include "GamePadUtils.h"
 #include "GamePadDeviceListener.h"
 
 class CInputProviderEvDev : public CInputProvider
 {
 public:
 	CInputProviderEvDev();
-	virtual ~CInputProviderEvDev();
+	virtual ~CInputProviderEvDev() = default;
 
 	uint32 GetId() const override;
 	std::string GetTargetDescription(const BINDINGTARGET&) const override;
 
 private:
+	void OnEvDevInputEvent(GamePadDeviceId, int, int, int, const input_absinfo*);
 	CGamePadDeviceListener m_GPDL;
 };
