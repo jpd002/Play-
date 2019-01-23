10274a959c36b29e1f63a98803195865f48bdcbd
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-01-23 02:41:49 +0000

    Qt Allow user to select resolution factor

--- Source/ui_qt/settingsdialog.cpp
+++ Source/ui_qt/settingsdialog.cpp
@@ -32,17 +32,11 @@ void SettingsDialog::changePage(QListWidgetItem* current, QListWidgetItem* previ
 
 void SettingsDialog::LoadPreferences()
 {
-	ui->checkBox_enable_highres->setChecked(CAppConfig::GetInstance().GetPreferenceBoolean(PREF_CGSH_OPENGL_ENABLEHIGHRESMODE));
+	ui->spinBox_resolution->setValue(CAppConfig::GetInstance().GetPreferenceInteger(PREF_CGSH_OPENGL_RESOLUTION_FACTOR));
 	ui->checkBox_force_bilinear_filtering->setChecked(CAppConfig::GetInstance().GetPreferenceBoolean(PREF_CGSH_OPENGL_FORCEBILINEARTEXTURES));
 
 	ui->checkBox_enable_audio->setChecked(CAppConfig::GetInstance().GetPreferenceBoolean(PREFERENCE_AUDIO_ENABLEOUTPUT));
 	ui->comboBox_presentation_mode->setCurrentIndex(CAppConfig::GetInstance().GetPreferenceInteger(PREF_CGSHANDLER_PRESENTATION_MODE));
-}
-
-void SettingsDialog::on_checkBox_enable_highres_clicked(bool checked)
-{
-
-	CAppConfig::GetInstance().SetPreferenceBoolean(PREF_CGSH_OPENGL_ENABLEHIGHRESMODE, checked);
 }
 
 void SettingsDialog::on_checkBox_force_bilinear_filtering_clicked(bool checked)
@@ -59,4 +53,9 @@ void SettingsDialog::on_checkBox_enable_audio_clicked(bool checked)
 void SettingsDialog::on_comboBox_presentation_mode_currentIndexChanged(int index)
 {
 	CAppConfig::GetInstance().SetPreferenceInteger(PREF_CGSHANDLER_PRESENTATION_MODE, index);
+}
+
+void SettingsDialog::on_spinBox_resolution_valueChanged(int val)
+{
+	CAppConfig::GetInstance().SetPreferenceInteger(PREF_CGSH_OPENGL_RESOLUTION_FACTOR, val);
 }
