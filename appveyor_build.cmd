881f30009f107c5473181f93f07c63ac898b6a0a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-02-27 18:36:15 -0500

    Yet another attempt.

---
+++ appveyor_build.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+mkdir build
+cd build
+
+if "%BUILD_PLAY%" == "ON" (
+	cmake .. -G"%BUILD_TYPE%" -T v141_xp -DUSE_QT=on -DCMAKE_PREFIX_PATH="C:\Qt\5.12\%QT_FLAVOR%"
+	cmake --build . --config %CONFIG_TYPE%
+
+	cd ..
+	"C:\Program Files (x86)\NSIS\makensis.exe" ./installer_win32/%INSTALLER_SCRIPT%
+	
+	cd ..
+	cd ..
+	move Play-Build\Play\installer_win32\*.exe %REPO_COMMIT_SHORT%
+)
+
+if "%BUILD_PSFPLAYER%" == "ON" (
+	cmake .. -G"%BUILD_TYPE%" -T v141_xp -BUILD_PLAY=off -DBUILD_PSFPLAYER=on	
+	cmake --build . --config %CONFIG_TYPE%
+	
+	cd ..
+	"C:\Program Files (x86)\NSIS\makensis.exe" ./tools/PsfPlayer/installer_win32/%INSTALLER_SCRIPT%
+
+	cd ..
+	cd ..
+	move Play-Build\Play\tools\PsfPlayer\installer_win32\*.exe %REPO_COMMIT_SHORT%
+)
