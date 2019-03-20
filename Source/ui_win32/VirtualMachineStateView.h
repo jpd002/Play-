0bc938fd5e7182b7d60dd8ad3cd6cbb2f237cfeb
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-03-20 17:54:34 -0400

    Add VirtualMachineStateView.

---
+++ Source/ui_win32/VirtualMachineStateView.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../VirtualMachine.h"
+
+class CVirtualMachineStateView
+{
+public:
+	virtual ~CVirtualMachineStateView() = default;
+	virtual void HandleMachineStateChange() {}
+	virtual void HandleRunningStateChange(CVirtualMachine::STATUS) {}
+};
