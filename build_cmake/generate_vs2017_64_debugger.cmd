fa79581f432077459ca0da5e6c03558792f58ba6
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-03-21 17:55:55 -0400

    Update project generation command lines.

--- build_cmake/generate_vs2017_64_debugger.cmd
+++ build_cmake/generate_vs2017_64_debugger.cmd
@@ -1,5 +1,5 @@
 @echo off
 mkdir build_debugger
 pushd build_debugger
-cmake ../.. -G "Visual Studio 15 2017 Win64" -T v141_xp -DBUILD_PSFPLAYER=on -DDEBUGGER_INCLUDED=on
+cmake ../.. -G "Visual Studio 15 2017 Win64" -T v141_xp -DUSE_QT=on -DBUILD_PSFPLAYER=on -DDEBUGGER_INCLUDED=on -DCMAKE_PREFIX_PATH="C:\Qt\5.12.1\msvc2017_64"
 popd
