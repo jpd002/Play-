19f77848fd7760be7052f36bc65036b17fa83308
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-04-08 20:27:54 -0400

    Add resolution factor selector on iOS.

--- Source/ui_ios/SettingsViewController.mm
+++ Source/ui_ios/SettingsViewController.mm
@@ -1,4 +1,5 @@
 #import "SettingsViewController.h"
+#import "ResolutionFactorSelectorViewController.h"
 #include "../AppConfig.h"
 #include "PreferenceDefs.h"
 #include "AppDef.h"
@@ -6,12 +7,18 @@
 
 @implementation SettingsViewController
 
+-(void)updateResolutionFactorLabel
+{
+	int factor = CAppConfig::GetInstance().GetPreferenceInteger(PREF_CGSH_OPENGL_RESOLUTION_FACTOR);
+	[resolutionFactor setText: [NSString stringWithFormat: @"%dx", factor]];
+}
+
 -(void)viewDidLoad
 {
 	[showFpsSwitch setOn: CAppConfig::GetInstance().GetPreferenceBoolean(PREFERENCE_UI_SHOWFPS)];
 	[showVirtualPadSwitch setOn: CAppConfig::GetInstance().GetPreferenceBoolean(PREFERENCE_UI_SHOWVIRTUALPAD)];
 
-	[enableHighResMode setOn: CAppConfig::GetInstance().GetPreferenceBoolean(PREF_CGSH_OPENGL_ENABLEHIGHRESMODE)];
+	[self updateResolutionFactorLabel];
 	[forceBilinearFiltering setOn: CAppConfig::GetInstance().GetPreferenceBoolean(PREF_CGSH_OPENGL_FORCEBILINEARTEXTURES)];
 	
 	[enableAudioOutput setOn: CAppConfig::GetInstance().GetPreferenceBoolean(PREFERENCE_AUDIO_ENABLEOUTPUT)];
@@ -25,12 +32,35 @@
 	CAppConfig::GetInstance().SetPreferenceBoolean(PREFERENCE_UI_SHOWFPS, showFpsSwitch.isOn);
 	CAppConfig::GetInstance().SetPreferenceBoolean(PREFERENCE_UI_SHOWVIRTUALPAD, showVirtualPadSwitch.isOn);
 	
-	CAppConfig::GetInstance().SetPreferenceBoolean(PREF_CGSH_OPENGL_ENABLEHIGHRESMODE, enableHighResMode.isOn);
 	CAppConfig::GetInstance().SetPreferenceBoolean(PREF_CGSH_OPENGL_FORCEBILINEARTEXTURES, forceBilinearFiltering.isOn);
 	
 	CAppConfig::GetInstance().SetPreferenceBoolean(PREFERENCE_AUDIO_ENABLEOUTPUT, enableAudioOutput.isOn);
 	
 	CAppConfig::GetInstance().Save();
+}
+
+-(void)tableView: (UITableView*)tableView didSelectRowAtIndexPath: (NSIndexPath*)indexPath
+{
+	[tableView deselectRowAtIndexPath: indexPath animated: YES];
+}
+
+-(void)prepareForSegue: (UIStoryboardSegue*)segue sender: (id)sender
+{
+	if([segue.destinationViewController isKindOfClass: [ResolutionFactorSelectorViewController class]])
+	{
+		ResolutionFactorSelectorViewController* selector = (ResolutionFactorSelectorViewController*)segue.destinationViewController;
+		selector.factor = CAppConfig::GetInstance().GetPreferenceInteger(PREF_CGSH_OPENGL_RESOLUTION_FACTOR);
+	}
+}
+
+-(IBAction)returnToSettings: (UIStoryboardSegue*)segue
+{
+	if([segue.sourceViewController isKindOfClass: [ResolutionFactorSelectorViewController class]])
+	{
+		ResolutionFactorSelectorViewController* selector = (ResolutionFactorSelectorViewController*)segue.sourceViewController;
+		CAppConfig::GetInstance().SetPreferenceInteger(PREF_CGSH_OPENGL_RESOLUTION_FACTOR, selector.factor);
+		[self updateResolutionFactorLabel];
+	}
 }
 
 @end
