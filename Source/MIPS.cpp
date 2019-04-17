42662b168faacf96046aa099c00bb6351e579805
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-04-17 13:10:01 -0400

    Cleanup.

--- Source/MIPS.cpp
+++ Source/MIPS.cpp
@@ -3,12 +3,15 @@
 #include "MIPS.h"
 #include "COP_SCU.h"
 
+// clang-format off
 const char* CMIPS::m_sGPRName[] =
-    {
-        "R0", "AT", "V0", "V1", "A0", "A1", "A2", "A3",
-        "T0", "T1", "T2", "T3", "T4", "T5", "T6", "T7",
-        "S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7",
-        "T8", "T9", "K0", "K1", "GP", "SP", "FP", "RA"};
+{
+    "R0", "AT", "V0", "V1", "A0", "A1", "A2", "A3",
+    "T0", "T1", "T2", "T3", "T4", "T5", "T6", "T7",
+    "S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7",
+    "T8", "T9", "K0", "K1", "GP", "SP", "FP", "RA"
+};
+// clang-format on
 
 CMIPS::CMIPS(MEMORYMAP_ENDIANESS nEnd)
 {
