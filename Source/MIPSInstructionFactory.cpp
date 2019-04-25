78ab94ddf19b48fe67577e1ae12cc1955da1a929
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-04-25 17:58:16 -0400

    Make the multiply by sizeof(void*) dynamic.
    Pointer size can vary.

--- Source/MIPSInstructionFactory.cpp
+++ Source/MIPSInstructionFactory.cpp
@@ -3,6 +3,7 @@
 #include "MIPSInstructionFactory.h"
 #include "MIPS.h"
 #include "offsetof_def.h"
+#include "BitManip.h"
 
 CMIPSInstructionFactory::CMIPSInstructionFactory(MIPS_REGSIZE nRegSize)
     : m_regSize(nRegSize)
@@ -89,14 +90,15 @@ void CMIPSInstructionFactory::ComputeMemAccessPageRef()
 {
 	auto rs = static_cast<uint8>((m_nOpcode >> 21) & 0x001F);
 	auto immediate = static_cast<uint16>((m_nOpcode >> 0) & 0xFFFF);
+	uint32 pointerMultiplyShift = __builtin_ctz(m_codeGen->GetCodeGen()->GetPointerSize());
 
 	m_codeGen->PushRelRef(offsetof(CMIPS, m_pageLookup));
 
 	m_codeGen->PushRel(offsetof(CMIPS, m_State.nGPR[rs].nV[0]));
 	m_codeGen->PushCst(static_cast<int16>(immediate));
 	m_codeGen->Add();
-	m_codeGen->Srl(12);
-	m_codeGen->Shl(3); //Multiply by sizeof(void*)
+	m_codeGen->Srl(12); //Divide by MIPS_PAGE_SIZE
+	m_codeGen->Shl(pointerMultiplyShift); //Multiply by sizeof(void*)
 	m_codeGen->AddRef();
 	m_codeGen->LoadRefFromRef();
 }
