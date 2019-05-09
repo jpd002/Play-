11c24a763ce2fde19d41a15ffe7442b4efceaec3
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-05-09 13:09:08 -0400

    Fix script path.

--- tools/PsfPlayer/build_aot/compile.bat
+++ tools/PsfPlayer/build_aot/compile.bat
@@ -8,8 +8,9 @@ mkdir armv7
 mkdir arm64
 popd
 popd
-"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" x86 coff .\output\coff\PsfBlocks.obj
-"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" x86 macho .\output\macho\i386\PsfBlocks.o
-"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" arm macho .\output\macho\armv7\PsfBlocks.o
-"..\build_win32\x64\ReleaseAot\PsfAot.exe" compile ".\blocks" arm64 macho .\output\macho\arm64\PsfBlocks.o
+set PSFAOT_PATH=..\..\..\build_cmake\build_psfaot\tools\PsfPlayer\Source\ui_aot\RelWithDebInfo
+"%PSFAOT_PATH%\PsfAot.exe" compile ".\blocks" x86 coff .\output\coff\PsfBlocks.obj
+"%PSFAOT_PATH%\PsfAot.exe" compile ".\blocks" x86 macho .\output\macho\i386\PsfBlocks.o
+"%PSFAOT_PATH%\PsfAot.exe" compile ".\blocks" arm macho .\output\macho\armv7\PsfBlocks.o
+"%PSFAOT_PATH%\PsfAot.exe" compile ".\blocks" arm64 macho .\output\macho\arm64\PsfBlocks.o
 @pause
