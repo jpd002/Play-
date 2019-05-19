76e3a3d4523eef08d9c1e569598f9ae34e9a647d
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-05-19 15:09:00 -0400

    Update build scripts.

--- build_cmake/generate_vs2017_64.cmd
+++ build_cmake/generate_vs2017_64.cmd
@@ -1,5 +1,5 @@
 @echo off
 mkdir build
 pushd build
-cmake ../.. -G "Visual Studio 15 2017 Win64" -T v141_xp -DUSE_QT=on -DBUILD_PSFPLAYER=on -DCMAKE_PREFIX_PATH="C:\Qt\5.12.1\msvc2017_64"
+cmake ../.. -G "Visual Studio 15 2017 Win64" -T v141_xp -DUSE_QT=on -DBUILD_PSFPLAYER=on -DCMAKE_PREFIX_PATH="C:\Qt\5.12.3\msvc2017_64"
 popd
