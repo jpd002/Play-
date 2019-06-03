988d5d7d2a7aa98ddc88e155e21054c28b40e542
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-06-03 04:13:35 +0100

    remove boost::erase_all
    (cherry picked from commit 034f64bf49faa3b9824fb4c6115c1dd8d4a7c762)

--- tools/ElfView/Source/WinMain.cpp
+++ tools/ElfView/Source/WinMain.cpp
@@ -1,11 +1,11 @@
 #include <windows.h>
 #include "ElfViewFrame.h"
-#include <boost/algorithm/string.hpp>
+#include "StringUtils.h"
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR params, int showCmd)
 {
 	std::string path(params);
-	boost::erase_all(path, "\"");
+	StringUtils::erase_all(path, "\"");
 	CElfViewFrame elfViewFrame(path.c_str());
 	elfViewFrame.Center();
 	elfViewFrame.Show(SW_SHOW);
