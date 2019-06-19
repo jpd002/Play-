b879995a1e66864042300c1d3c3ab402deeeffb6
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-06-19 19:04:21 -0400

    First working version of XInput support.

--- Source/ui_qt/win32/InputProviderXInput.cpp
+++ Source/ui_qt/win32/InputProviderXInput.cpp
@@ -1,8 +1,53 @@
 #include "InputProviderXInput.h"
+#include "string_format.h"
 #include <Windows.h>
 #include <Xinput.h>
 
 #define PROVIDER_ID 'xinp'
+
+const char* CInputProviderXInput::g_keyNames[CInputProviderXInput::KEYID_MAX] =
+{
+	"Left Thumb (X Axis)",
+	"Left Thumb (Y Axis)",
+	"Right Thumb (X Axis)",
+	"Right Thumb (Y Axis)",
+	"Left Trigger",
+	"Right Trigger",
+	"DPad Up",
+	"DPad Down",
+	"DPad Left",
+	"DPad Right",
+	"Start",
+	"Back",
+	"Left Thumb",
+	"Right Thumb",
+	"Left Shoulder",
+	"Right Shoulder",
+	"A",
+	"B",
+	"X",
+	"Y"
+};
+
+const CInputProviderXInput::KEYID CInputProviderXInput::g_buttonToKey[MAX_BUTTONS] =
+{
+	KEYID_DPAD_UP,
+	KEYID_DPAD_DOWN,
+	KEYID_DPAD_LEFT,
+	KEYID_DPAD_RIGHT,
+	KEYID_START,
+	KEYID_BACK,
+	KEYID_LTHUMB,
+	KEYID_RTHUMB,
+	KEYID_LSHOULDER,
+	KEYID_RSHOULDER,
+	KEYID_MAX,
+	KEYID_MAX,
+	KEYID_A,
+	KEYID_B,
+	KEYID_X,
+	KEYID_Y
+};
 
 CInputProviderXInput::CInputProviderXInput()
 {
@@ -22,29 +67,86 @@ uint32 CInputProviderXInput::GetId() const
 
 std::string CInputProviderXInput::GetTargetDescription(const BINDINGTARGET& target) const
 {
-	return "XInputTarget";
+	return string_format("XInput Device %d : %s", target.deviceId[0], g_keyNames[target.keyId]);
 }
 
 void CInputProviderXInput::PollDevices()
 {
 	while(m_pollingEnabled)
 	{
-		bool hasActiveDevice = false;
-		for(unsigned int i = 0; i < MAX_DEVICES; i++)
+		for(unsigned int deviceIndex = 0; deviceIndex < MAX_DEVICES; deviceIndex++)
 		{
-			auto& prevState = m_states[i];
+			auto& currState = m_states[deviceIndex];
+			if(currState.idleTime > 0)
+			{
+				currState.idleTime--;
+				continue;
+			}
 			XINPUT_STATE state;
-			DWORD result = XInputGetState(i, &state);
+			DWORD result = XInputGetState(deviceIndex, &state);
 			if(result == ERROR_SUCCESS)
 			{
-				prevState.connected = true;
-				hasActiveDevice = true;
+				if((currState.connected == false) || (currState.packetNumber != state.dwPacketNumber))
+				{
+					ReportAxis(deviceIndex, KEYID_LTHUMB_X, state.Gamepad.sThumbLX);
+					ReportAxis(deviceIndex, KEYID_LTHUMB_Y, state.Gamepad.sThumbLY);
+					ReportAxis(deviceIndex, KEYID_RTHUMB_X, state.Gamepad.sThumbRX);
+					ReportAxis(deviceIndex, KEYID_RTHUMB_Y, state.Gamepad.sThumbRY);
+					bool lTriggerState = state.Gamepad.bLeftTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD;
+					bool rTriggerState = state.Gamepad.bRightTrigger >= XINPUT_GAMEPAD_TRIGGER_THRESHOLD;
+					if(lTriggerState != currState.lTriggerPressed)
+					{
+						ReportButton(deviceIndex, KEYID_LTRIGGER, lTriggerState);
+						currState.lTriggerPressed = lTriggerState;
+					}
+					if(rTriggerState != currState.rTriggerPressed)
+					{
+						ReportButton(deviceIndex, KEYID_RTRIGGER, rTriggerState);
+						currState.rTriggerPressed = rTriggerState;
+					}
+					for(unsigned int keyIndex = 0; keyIndex < MAX_BUTTONS; keyIndex++)
+					{
+						auto keyId = g_buttonToKey[keyIndex];
+						if(keyId == KEYID_MAX) continue;
+						bool newState = (state.Gamepad.wButtons & (1 << keyIndex)) != 0;
+						bool oldState = (currState.buttonState & (1 << keyIndex));
+						if(newState != oldState)
+						{
+							ReportButton(deviceIndex, keyId, newState);
+						}
+					}
+					currState.buttonState = state.Gamepad.wButtons;
+					currState.packetNumber = state.dwPacketNumber;
+				}
+				currState.connected = true;
 			}
 			else
 			{
-				prevState.connected = false;
+				currState.idleTime = 100;
+				currState.connected = false;
 			}
 		}
-		Sleep(hasActiveDevice ? 16 : 1000);
+		Sleep(16);
 	}
+}
+
+void CInputProviderXInput::ReportButton(uint32 deviceId, KEYID keyId, bool pressed)
+{
+	BINDINGTARGET tgt;
+	tgt.providerId = PROVIDER_ID;
+	tgt.deviceId[0] = deviceId;
+	tgt.keyType = BINDINGTARGET::KEYTYPE::BUTTON;
+	tgt.keyId = keyId;
+	OnInput(tgt, pressed ? 1 : 0);
+}
+
+void CInputProviderXInput::ReportAxis(uint32 deviceId, KEYID keyId, int16 rawValue)
+{
+	BINDINGTARGET tgt;
+	tgt.providerId = PROVIDER_ID;
+	tgt.deviceId[0] = deviceId;
+	tgt.keyType = BINDINGTARGET::KEYTYPE::AXIS;
+	tgt.keyId = keyId;
+	uint32 cvtValue = ((static_cast<int32>(rawValue) + 0x8000) >> 8) & 0xFF;
+	OnInput(tgt, cvtValue);
 }
--- Source/ui_qt/win32/InputProviderXinput.h
+++ Source/ui_qt/win32/InputProviderXinput.h
@@ -16,17 +16,52 @@ private:
 	enum
 	{
 		MAX_DEVICES = 4,
+		MAX_BUTTONS = 16
+	};
+
+	enum KEYID
+	{
+		KEYID_LTHUMB_X,
+		KEYID_LTHUMB_Y,
+		KEYID_RTHUMB_X,
+		KEYID_RTHUMB_Y,
+		KEYID_LTRIGGER,
+		KEYID_RTRIGGER,
+		KEYID_DPAD_UP,
+		KEYID_DPAD_DOWN,
+		KEYID_DPAD_LEFT,
+		KEYID_DPAD_RIGHT,
+		KEYID_START,
+		KEYID_BACK,
+		KEYID_LTHUMB,
+		KEYID_RTHUMB,
+		KEYID_LSHOULDER,
+		KEYID_RSHOULDER,
+		KEYID_A,
+		KEYID_B,
+		KEYID_X,
+		KEYID_Y,
+		KEYID_MAX
 	};
 
 	struct DEVICE_STATE
 	{
 		bool connected = false;
+		uint32 idleTime = 0;
+		bool lTriggerPressed = false;
+		bool rTriggerPressed = false;
+		uint16 buttonState = 0;
+		uint32 packetNumber = 0;
 	};
 
 	void PollDevices();
+	void ReportButton(uint32, KEYID, bool);
+	void ReportAxis(uint32, KEYID, int16);
 
 	bool m_pollingEnabled = true;
 	std::thread m_pollingThread;
 
+	static const char* g_keyNames[KEYID_MAX];
+	static const KEYID g_buttonToKey[MAX_BUTTONS];
 	DEVICE_STATE m_states[MAX_DEVICES];
 };
