b8831283657ac7e5bfbf1b432fe2cb4488ed5f28
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-07-29 22:46:12 +0100

    Cleanup

--- Source/gs/GSH_OpenGL/GSH_OpenGL_Overlay.cpp
+++ Source/gs/GSH_OpenGL/GSH_OpenGL_Overlay.cpp
@@ -88,7 +88,8 @@ void CGSH_OpenGL::InitOverlay()
 		glGenTextures(1, &texture);
 		glBindTexture(GL_TEXTURE_2D, texture);
 
-		//Note the bitmap only holds alpha value, so we'll store it in in red bits
+		// Note the bitmap only holds 2 channel value (aka no colours, one channel for fill and one for the stroke)
+		// so we'll store them in in red and green bit
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, bitmap_stroke->width, bitmap_stroke->rows, 0, GL_RG, GL_UNSIGNED_BYTE, bitmap_mixed_buffer.data());
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
