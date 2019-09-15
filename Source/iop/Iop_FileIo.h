326ae81d23c621335d67ed0907038f13f1bb97ed
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-09-15 12:00:03 -0400

    Cleanup.

--- Source/iop/Iop_FileIo.h
+++ Source/iop/Iop_FileIo.h
@@ -38,10 +38,10 @@ namespace Iop
 
 		void SetModuleVersion(unsigned int);
 
-		virtual std::string GetId() const override;
-		virtual std::string GetFunctionName(unsigned int) const override;
-		virtual void Invoke(CMIPS&, unsigned int) override;
-		virtual bool Invoke(uint32, uint32*, uint32, uint32*, uint32, uint8*) override;
+		std::string GetId() const override;
+		std::string GetFunctionName(unsigned int) const override;
+		void Invoke(CMIPS&, unsigned int) override;
+		bool Invoke(uint32, uint32*, uint32, uint32*, uint32, uint8*) override;
 
 		void LoadState(Framework::CZipArchiveReader&);
 		void SaveState(Framework::CZipArchiveWriter&) const;
