8b6a89fbd41a1e885f051ee366b4e04b9fb48fc4
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-02 13:16:16 +0100

    Dynamically generate changelog from git history

--- appveyor_build.cmd
+++ appveyor_build.cmd
@@ -14,6 +14,12 @@ if "%BUILD_PLAY%" == "ON" (
 	c:\Qt\5.12\%QT_FLAVOR%\bin\windeployqt.exe ./Source/ui_qt/Release --no-system-d3d-compiler --no-quick-import --no-opengl-sw --no-compiler-runtime --no-translations	
 	
 	cd ..
+
+	echo "<style>table{border-collapse: collapse;}td,th{border: 1px solid #dddddd;text-align:left;padding:8px;white-space:nowrap;}tr:nth-child(even){background-color: #dddddd;}</style>" > Changelog.html
+	echo "<table> <tr> <th> Date </th> <th> Author </th> <th> Commit Hash </th> <th> Description </th> </tr>" >> Changelog.html
+	git log --date="format:%%Y-%%m-%%d" --pretty="format:<tr> <td> %%ad </td> <td> %%an </td> <td><a href=\"https://github.com/jpd002/Play-/commit/%%H\">%%h</a></td> <td>%%s</td> </tr>" >> Changelog.html
+	echo "</table>" >> Changelog.html
+
 	"C:\Program Files (x86)\NSIS\makensis.exe" ./installer_win32/%INSTALLER_SCRIPT%
 	
 	mkdir %REPO_COMMIT_SHORT%
