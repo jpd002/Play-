89f184728fb4761b6eed58274cf0656297a84a04
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-10-17 12:04:25 -0400

    Remove wchar_t literals.

--- Source/AppConfig.cpp
+++ Source/AppConfig.cpp
@@ -1,8 +1,8 @@
 #include "AppConfig.h"
 #include "PathUtils.h"
 
-#define BASE_DATA_PATH (L"Play Data Files")
-#define CONFIG_FILENAME (L"config.xml")
+#define BASE_DATA_PATH ("Play Data Files")
+#define CONFIG_FILENAME ("config.xml")
 
 CAppConfig::CAppConfig()
     : CConfig(BuildConfigPath())
