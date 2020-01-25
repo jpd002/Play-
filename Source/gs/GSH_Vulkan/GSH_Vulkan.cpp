52abeae925755c5b67331a12ad7597ab8c465f94
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2020-01-25 09:09:10 -0500

    Divide by Q for sprites.

--- Source/gs/GSH_Vulkan/GSH_Vulkan.cpp
+++ Source/gs/GSH_Vulkan/GSH_Vulkan.cpp
@@ -792,11 +792,16 @@ void CGSH_Vulkan::Prim_Sprite()
 			st[0] <<= m_vtxBuffer[1].st;
 			st[1] <<= m_vtxBuffer[0].st;
 
-			s[0] = st[0].nS;
-			s[1] = st[1].nS;
+			float q1 = rgbaq[1].nQ;
+			float q2 = rgbaq[0].nQ;
+			if(q1 == 0) q1 = 1;
+			if(q2 == 0) q2 = 1;
 
-			t[0] = st[0].nT;
-			t[1] = st[1].nT;
+			s[0] = st[0].nS / q1;
+			s[1] = st[1].nS / q2;
+
+			t[0] = st[0].nT / q1;
+			t[1] = st[1].nT / q2;
 		}
 	}
 
