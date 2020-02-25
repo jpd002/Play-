4aeadfab7ba0d7971f56705335aa1c80c5d9f4bc
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2020-02-24 19:20:56 -0500

    Another slightly different approach.
    Fixes Rogue Galaxy.

--- Source/ee/VuBasicBlock.h
+++ Source/ee/VuBasicBlock.h
@@ -24,5 +24,6 @@ private:
 	INTEGER_BRANCH_DELAY_INFO GetIntegerBranchDelayInfo() const;
 	bool CheckIsSpecialIntegerLoop(unsigned int) const;
 	void ComputeSkipFlagsHints(std::vector<uint32>&) const;
+	std::vector<uint32> ComputeFmacStallDelays() const;
 	static void EmitXgKick(CMipsJitter*);
 };
