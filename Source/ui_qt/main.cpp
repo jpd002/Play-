8336b8d420b677d287dc24b4482472e23011bfe7
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-10 05:06:45 +0100

    Update main.cpp
    (cherry picked from commit c474396b578d5fe4946a6cd0d8daf08ddb73511a)

--- Source/ui_qt/main.cpp
+++ Source/ui_qt/main.cpp
@@ -2,10 +2,13 @@
 #include <QCommandLineParser>
 #include "mainwindow.h"
 #include "QStringUtils.h"
+#include <QtGlobal>
 
 int main(int argc, char* argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 	QApplication a(argc, argv);
 
 	QCoreApplication::setApplicationName("Play!");
