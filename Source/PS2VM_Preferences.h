ef7190267866a0e2fa6fdb6a107b7808b84722b8
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-04-26 02:26:49 +0100

    add rom0 device

--- Source/PS2VM_Preferences.h
+++ Source/PS2VM_Preferences.h
@@ -2,6 +2,7 @@
 
 #define PREF_PS2_CDROM0_PATH ("ps2.cdrom0.path.v2")
 
+#define PREF_PS2_ROM0_DIRECTORY ("ps2.rom0.directory.v2")
 #define PREF_PS2_HOST_DIRECTORY ("ps2.host.directory.v2")
 #define PREF_PS2_MC0_DIRECTORY ("ps2.mc0.directory.v2")
 #define PREF_PS2_MC1_DIRECTORY ("ps2.mc1.directory.v2")
