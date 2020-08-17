c781a7f8480c0e1f69ace3159cd44870924ad4ab
jpd002 <jean-philip.desjardins@polymtl.ca>
2020-08-17 10:41:37 -0400

    Implement HIGHLIGHT texture function.

--- Source/gs/GSH_Vulkan/GSH_VulkanDraw.cpp
+++ Source/gs/GSH_Vulkan/GSH_VulkanDraw.cpp
@@ -1188,6 +1188,20 @@ Framework::Vulkan::CShaderModule CDraw::CreateFragmentShader(const PIPELINE_CAPS
 			case CGSHandler::TEX0_FUNCTION_DECAL:
 				//Nothing to do
 				break;
+			case CGSHandler::TEX0_FUNCTION_HIGHLIGHT:
+			{
+				auto tempColor = (textureColor->xyz() * inputColor->xyz() * NewFloat3(b, 2, 2, 2)) + inputColor->www();
+				if(caps.textureHasAlpha)
+				{
+					textureColor = NewFloat4(tempColor, inputColor->w() + textureColor->w());
+				}
+				else
+				{
+					textureColor = NewFloat4(tempColor, inputColor->w());
+				}
+				textureColor = Clamp(textureColor, NewFloat4(b, 0, 0, 0, 0), NewFloat4(b, 1, 1, 1, 1));
+			}
+			break;
 			case CGSHandler::TEX0_FUNCTION_HIGHLIGHT2:
 			{
 				auto tempColor = (textureColor->xyz() * inputColor->xyz() * NewFloat3(b, 2, 2, 2)) + inputColor->www();
