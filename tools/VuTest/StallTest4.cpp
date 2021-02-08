277b04d35fed1a81bdc868eee1f820b993afb01b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-02-08 18:03:03 -0500

    Style fix.

--- tools/VuTest/StallTest4.cpp
+++ tools/VuTest/StallTest4.cpp
@@ -11,48 +11,48 @@ void CStallTest4::Execute(CTestVm& virtualMachine)
 
 	{
 		CVuAssembler assembler(microMem);
-	
+
 		auto branchLabel = assembler.CreateLabel();
 
 		//pipeTime = 0, writing to VF19xy, result will be available in pipeTime 4
 		assembler.Write(
-			CVuAssembler::Upper::SUB(CVuAssembler::DEST_XY, CVuAssembler::VF19, CVuAssembler::VF1, CVuAssembler::VF23),
-			CVuAssembler::Lower::NOP());
+		    CVuAssembler::Upper::SUB(CVuAssembler::DEST_XY, CVuAssembler::VF19, CVuAssembler::VF1, CVuAssembler::VF23),
+		    CVuAssembler::Lower::NOP());
 
 		//pipeTime = 1, CLIP result will be available in pipeTime 5
 		assembler.Write(
-			CVuAssembler::Upper::CLIP(CVuAssembler::VF2, CVuAssembler::VF0),
-			CVuAssembler::Lower::IBEQ(CVuAssembler::VI15, CVuAssembler::VI0, branchLabel));
+		    CVuAssembler::Upper::CLIP(CVuAssembler::VF2, CVuAssembler::VF0),
+		    CVuAssembler::Lower::IBEQ(CVuAssembler::VI15, CVuAssembler::VI0, branchLabel));
 
 		//pipeTime = 2, CLIP result will be available in pipeTime 6
 		assembler.Write(
-			CVuAssembler::Upper::CLIP(CVuAssembler::VF15, CVuAssembler::VF0),
-			CVuAssembler::Lower::IADDIU(CVuAssembler::VI3, CVuAssembler::VI0, 0x3F));
+		    CVuAssembler::Upper::CLIP(CVuAssembler::VF15, CVuAssembler::VF0),
+		    CVuAssembler::Lower::IADDIU(CVuAssembler::VI3, CVuAssembler::VI0, 0x3F));
 
 		//pipeTime = 3, 4, reading VF19x, stalling till pipeTime 4
 		assembler.Write(
-			CVuAssembler::Upper::NOP(),
-			CVuAssembler::Lower::MTIR(CVuAssembler::VI10, CVuAssembler::VF19, CVuAssembler::FVF_X));
+		    CVuAssembler::Upper::NOP(),
+		    CVuAssembler::Lower::MTIR(CVuAssembler::VI10, CVuAssembler::VF19, CVuAssembler::FVF_X));
 
 		//pipeTime = 5
 		assembler.Write(
-			CVuAssembler::Upper::NOP(),
-			CVuAssembler::Lower::B(branchLabel));
+		    CVuAssembler::Upper::NOP(),
+		    CVuAssembler::Lower::B(branchLabel));
 
 		//pipeTime = 6
 		assembler.Write(
-			CVuAssembler::Upper::NOP(),
-			CVuAssembler::Lower::FCGET(CVuAssembler::VI10));
+		    CVuAssembler::Upper::NOP(),
+		    CVuAssembler::Lower::FCGET(CVuAssembler::VI10));
 
 		assembler.MarkLabel(branchLabel);
 
 		assembler.Write(
-			CVuAssembler::Upper::NOP() | CVuAssembler::Upper::E_BIT,
-			CVuAssembler::Lower::NOP());
+		    CVuAssembler::Upper::NOP() | CVuAssembler::Upper::E_BIT,
+		    CVuAssembler::Lower::NOP());
 
 		assembler.Write(
-			CVuAssembler::Upper::NOP(),
-			CVuAssembler::Lower::NOP());
+		    CVuAssembler::Upper::NOP(),
+		    CVuAssembler::Lower::NOP());
 	}
 
 	virtualMachine.m_cpu.m_State.nCOP2VI[15] = 0xFFFF;
