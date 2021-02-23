64b6843a9d5a7396253b0b07bf32f9e1c8146a05
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-02-23 09:41:35 +0000

    cleanup

--- build_docker/x86_64-cross_compile/armv7/toolchain.cmake
+++ build_docker/x86_64-cross_compile/armv7/toolchain.cmake
@@ -11,4 +11,4 @@ set(CMAKE_C_FLAGS "-I /usr/include/arm-linux-gnueabihf/ -I /usr/include/")
 
 set(CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/arm-linux-gnueabihf/;/usr/arm-linux-gnueabihf/")
 set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};/usr/include/;/usr/include/arm-linux-gnueabihf/")
-
+set(IS_CROSS_COMPILING TRUE)
