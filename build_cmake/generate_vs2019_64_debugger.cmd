f77eebb67fe092787e74d33d4cf2f3a8b947860b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-02-23 13:55:00 -0500

    Update batch files.

--- build_cmake/generate_vs2019_64_debugger.cmd
+++ build_cmake/generate_vs2019_64_debugger.cmd
@@ -1,5 +1,5 @@
 @echo off
 mkdir build_debugger_vs2019
 pushd build_debugger_vs2019
-cmake ../.. -G "Visual Studio 16 2019" -A x64 -DUSE_QT=on -DBUILD_PSFPLAYER=on -DDEBUGGER_INCLUDED=on -DCMAKE_PREFIX_PATH="C:\Qt\5.12.3\msvc2017_64"
+cmake ../.. -G "Visual Studio 16 2019" -A x64 -DUSE_QT=on -DBUILD_PSFPLAYER=on -DDEBUGGER_INCLUDED=on -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019_64"
 popd
