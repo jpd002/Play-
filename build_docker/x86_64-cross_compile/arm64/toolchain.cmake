dfd966de124b69deb77b23cca0e976ed35604974
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-02-24 17:28:18 +0000

    cleanup

--- build_docker/x86_64-cross_compile/arm64/toolchain.cmake
+++ build_docker/x86_64-cross_compile/arm64/toolchain.cmake
@@ -4,11 +4,11 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(CMAKE_C_COMPILER "aarch64-none-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "aarch64-none-linux-gnu-g++")
-set(CMAKE_CXX_LINK_FLAGS "-Wl,-rpath-link,/usr/aarch64-none-linux-gnu/libc/lib64/  -Wl,-rpath-link,/lib/aarch64-linux-gnu/ ${CMAKE_CXX_LINK_FLAGS}")
 
-set(CMAKE_CXX_FLAGS "-I /usr/include/aarch64-linux-gnu/ -I /usr/include/")
-set(CMAKE_C_FLAGS "-I /usr/include/aarch64-linux-gnu/ -I /usr/include/")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} -Wl,-rpath-link,/usr/lib/aarch64-linux-gnu/  -Wl,-rpath-link,/lib/aarch64-linux-gnu/ -Wl,-rpath-link,/usr/aarch64-linux-gnu/lib/")
 
-set(CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/aarch64-linux-gnu/;/usr/aarch64-linux-gnu/")
-set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};/usr/include/;/usr/include/aarch64-linux-gnu/")
-set(IS_CROSS_COMPILING TRUE)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I /usr/include/aarch64-linux-gnu/ -I /usr/include/")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I /usr/include/aarch64-linux-gnu/ -I /usr/include/")
+
+set(CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/aarch64-linux-gnu/;/lib/aarch64-linux-gnu/;/usr/aarch64-linux-gnu/lib/;")
+set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};/usr/include/;/usr/include/aarch64-linux-gnu/;")
--- build_docker/x86_64-cross_compile/armv7/toolchain.cmake
+++ build_docker/x86_64-cross_compile/armv7/toolchain.cmake
@@ -4,11 +4,11 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(CMAKE_C_COMPILER "arm-none-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-linux-gnueabihf-g++")
-set(CMAKE_CXX_LINK_FLAGS "-Wl,-rpath-link,/usr/arm-none-linux-gnueabihf/libc/lib/  -Wl,-rpath-link,/lib/arm-linux-gnueabihf/ ${CMAKE_CXX_LINK_FLAGS}")
 
-set(CMAKE_CXX_FLAGS "-I /usr/include/arm-linux-gnueabihf/ -I /usr/include/")
-set(CMAKE_C_FLAGS "-I /usr/include/arm-linux-gnueabihf/ -I /usr/include/")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} -Wl,-rpath-link,/usr/lib/arm-linux-gnueabihf/ -Wl,-rpath-link,/lib/arm-linux-gnueabihf/ -Wl,-rpath-link,/usr/arm-none-linux-gnueabihf/lib/")
 
-set(CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/arm-linux-gnueabihf/;/usr/arm-linux-gnueabihf/")
-set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};/usr/include/;/usr/include/arm-linux-gnueabihf/")
-set(IS_CROSS_COMPILING TRUE)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I /usr/include/arm-linux-gnueabihf/ -I /usr/include/")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I /usr/include/arm-linux-gnueabihf/ -I /usr/include/")
+
+set(CMAKE_SYSTEM_LIBRARY_PATH "/usr/lib/arm-linux-gnueabihf/;/lib/arm-linux-gnueabihf/;/usr/arm-none-linux-gnueabihf/lib/;")
+set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};/usr/include/;/usr/include/arm-linux-gnueabihf/;")
