c047f414b6a8ceebaf6fe7785562986162c61ae8
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-06-28 16:36:07 -0400

    Add missing descriptor type.

--- Source/gs/GSH_Vulkan/GSH_Vulkan.cpp
+++ Source/gs/GSH_Vulkan/GSH_Vulkan.cpp
@@ -414,6 +414,13 @@ void CGSH_Vulkan::CreateDescriptorPool()
 
 	{
 		VkDescriptorPoolSize poolSize = {};
+		poolSize.type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC;
+		poolSize.descriptorCount = 0x800;
+		poolSizes.push_back(poolSize);
+	}
+
+	{
+		VkDescriptorPoolSize poolSize = {};
 		poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 		poolSize.descriptorCount = 0x800;
 		poolSizes.push_back(poolSize);
