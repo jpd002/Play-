f6945f5df97351bced64cf57f469da4766c6d9eb
strikersix23 <Kalskiajg1254>
2021-07-14 09:45:46 -0500

    updated ps2vm.cpp

--- Source/PS2VM.cpp
+++ Source/PS2VM.cpp
@@ -433,7 +433,7 @@ void CPS2VM::ResetVM()
 		iopOs->GetIoman()->RegisterDevice("cdrom", Iop::CIoman::DevicePtr(new Iop::Ioman::COpticalMediaDevice(m_cdrom0)));
 		iopOs->GetIoman()->RegisterDevice("cdrom0", Iop::CIoman::DevicePtr(new Iop::Ioman::COpticalMediaDevice(m_cdrom0)));
 		iopOs->GetIoman()->RegisterDevice("hdd0", std::make_shared<Iop::Ioman::CHardDiskDevice>());
-
+ iopOs->GetIoman()->RegisterDevice("mass", std::make_shared<Iop::Ioman::CPreferenceDirectoryDevice>(PREF_PS2_MASS_DIRECTORY))
 		iopOs->GetLoadcore()->SetLoadExecutableHandler(std::bind(&CPS2OS::LoadExecutable, m_ee->m_os, std::placeholders::_1, std::placeholders::_2));
 	}
 
