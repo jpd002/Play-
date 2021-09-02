f287156fdf791cefd4e274c0eadf13825349082b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-09-02 16:18:53 -0400

    Improve build config.

--- build_cmake/generate_wasm.cmd
+++ build_cmake/generate_wasm.cmd
@@ -3,4 +3,4 @@ set PATH=%PATH%;C:\Utils
 call %EMSDK_LOCAL_ROOT%\emsdk_env.bat
 mkdir build_wasm
 pushd build_wasm
-emcmake cmake ../.. -G "Ninja" -DUSE_EMSCRIPTEN=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=OFF -DBUILD_PLAY=OFF -DBUILD_PSFPLAYER=ON -DUSE_QT=OFF
+emcmake cmake ../.. -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=OFF -DBUILD_PLAY=OFF -DBUILD_PSFPLAYER=ON -DUSE_QT=OFF
