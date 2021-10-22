e477632f0d88c622c5242d80d10ae904081964ef
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-10-22 09:05:33 -0400

    Add missing overrides.

---
+++ js/psfplayer_browser/config-overrides.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Extend/override the dev server configuration used by CRA
+    // See: https://github.com/timarney/react-app-rewired#extended-configuration-options
+    devServer: function(configFunction) {
+      return function(proxy, allowedHost) {
+        // Create the default config by calling configFunction with the proxy/allowedHost parameters
+        // Default config: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpackDevServer.config.js
+        const config = configFunction(proxy, allowedHost);
+  
+        config.headers = {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin'
+        }
+  
+        return config;
+      };
+    },
+  };
+  
