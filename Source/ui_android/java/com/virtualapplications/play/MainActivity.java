7ce7835f6211516b95218a27783a57b06af819e8
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-10-23 11:59:41 -0400

    Also set proper intent flags for migration source folder selection.

--- Source/ui_android/java/com/virtualapplications/play/MainActivity.java
+++ Source/ui_android/java/com/virtualapplications/play/MainActivity.java
@@ -620,6 +620,10 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerF
 				.setMessage(getString(R.string.migration_selectfolder))
 				.setPositiveButton(android.R.string.ok, (dialog, id) -> {
 					Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+					intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+					intent.addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+					intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+					intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
 					startActivityForResult(intent, g_dataFilesFolderPickerRequestCode);
 				})
 				.create()
