0ab1a065a847ab48cb95551eaecba2a27ed6327a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2021-11-01 09:38:37 -0400

    Update Node.js test app.

--- js/psfplayer_node/index.ts
+++ js/psfplayer_node/index.ts
@@ -9,18 +9,16 @@ function convertStringVectorToArray(strVector : any) {
 }
 
 var main = async function() {
-    let factory = require("../../build_cmake/build_wasm/tools/PsfPlayer/Source/js_ui/PsfPlayer.js");
+    let factory = require("../../../build_cmake/build_wasm/tools/PsfPlayer/Source/js_ui/PsfPlayer.js");
     let module = await factory();
     module.FS.mkdir("/work");
     module.FS.mount(module.NODEFS, { root: "."}, "/work");
 
-    let psfArchivePath = "work/Final Fantasy VII (EMU).zophar.zip";
+    let psfArchivePath = "work/psf/Valkyrie Profile (EMU).zophar.zip";
     let psfArchiveFileList = convertStringVectorToArray(module.getPsfArchiveFileList(psfArchivePath));
 
-    await module.ccall("initVm", "", [], [], { async: true });
-    await module.ccall("loadPsf", "", ['string', 'string'], [psfArchivePath, psfArchiveFileList[0]], { async: true });
-    while(true) {
-        await module.ccall("step", "", [], [], { async: true });
-    }
-    console.log("Finished execution.");
+    module.ccall("initVm");
+    module.loadPsf(psfArchivePath, psfArchiveFileList[0]);
+
+    console.log("Finished initialization.");
 }();
