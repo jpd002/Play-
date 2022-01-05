45f8e1016fd2fc95fa18d9d3f47ac29dd08df7b3
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2022-01-05 23:20:06 +0000

    add BootableModelProxy, will be used to filter the gaming list

---
+++ Source/ui_qt/BootableModelProxy.cpp
@@ -0,0 +1,25 @@
+#include "BootableModelProxy.h"
+#include "BootableModel.h"
+#include <regex>
+BootableModelProxy::BootableModelProxy(QObject* parent)
+	: QSortFilterProxyModel(parent)
+{
+}
+
+bool BootableModelProxy::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
+{
+	QModelIndex index = sourceModel()->index(sourceRow, 0, sourceParent);
+
+	QVariant data = sourceModel()->data(index);
+	if(data.canConvert<BootableCoverQVarient>())
+	{
+		BootableCoverQVarient bootablecover = qvariant_cast<BootableCoverQVarient>(data);
+		QString key = QString::fromStdString(bootablecover.GetKey());
+		QString title = QString::fromStdString(bootablecover.GetTitle());
+		QRegularExpression regex = filterRegularExpression();
+		regex.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+
+		return  key.contains(regex) ||  title.contains(regex);
+	}
+	return false;
+}
