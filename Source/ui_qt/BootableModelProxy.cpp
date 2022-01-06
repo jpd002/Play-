10053af36e6506ff37da8e6e83c8f016446a531c
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2022-01-06 23:57:59 +0000

    fix implementation on Qt5

--- Source/ui_qt/BootableModelProxy.cpp
+++ Source/ui_qt/BootableModelProxy.cpp
@@ -17,8 +17,15 @@ bool BootableModelProxy::filterAcceptsRow(int sourceRow, const QModelIndex& sour
 		QString key = QString::fromStdString(bootablecover.GetKey());
 		QString title = QString::fromStdString(bootablecover.GetTitle());
 		QString path = QString::fromStdString(bootablecover.GetPath());
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 		QRegularExpression regex = filterRegularExpression();
 		regex.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+#else
+		// QRegExp is deprecated in Qt6, while QRegularExpression is recommended over QRegExp since Qt5
+		// however QRegularExpression is producing incorrect results in Qt5
+		QRegExp regex = filterRegExp();
+		regex.setCaseSensitivity(Qt::CaseInsensitive);
+#endif
 
 		return key.contains(regex) || title.contains(regex) || path.contains(regex);
 	}
