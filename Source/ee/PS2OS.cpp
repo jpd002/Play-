8fa84001aae219c27881d27a67efe3228e0b444d
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-02-10 15:43:34 -0500

    Start TIMER2 on boot.

--- Source/ee/PS2OS.cpp
+++ Source/ee/PS2OS.cpp
@@ -288,7 +288,8 @@ void CPS2OS::Initialize()
 	m_ee.m_State.nPC = BIOS_ADDRESS_IDLETHREADPROC;
 	m_ee.m_State.nCOP0[CCOP_SCU::STATUS] |= (CMIPS::STATUS_IE | CMIPS::STATUS_EIE);
 
-	//The BIOS enables TIMER3 for alarm purposes, some games (ex.: SoulCalibur 3) assume timer is counting.
+	//The BIOS enables TIMER2 and TIMER3 for alarm purposes and others, some games (ex.: SoulCalibur 3, Ape Escape 2) assume timer is counting.
+	m_ee.m_pMemoryMap->SetWord(CTimer::T2_MODE, CTimer::MODE_COUNT_ENABLE | CTimer::MODE_EQUAL_FLAG | CTimer::MODE_OVERFLOW_FLAG);
 	m_ee.m_pMemoryMap->SetWord(CTimer::T3_MODE, CTimer::MODE_CLOCK_SELECT_EXTERNAL | CTimer::MODE_COUNT_ENABLE | CTimer::MODE_EQUAL_FLAG | CTimer::MODE_OVERFLOW_FLAG);
 }
 
