0ff3627e8d3ae6d145b66392761463da82e65bdc
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-02-22 13:39:23 -0500

    Fix build.

--- tools/PsfPlayer/Source/PsfLoader.cpp
+++ tools/PsfPlayer/Source/PsfLoader.cpp
@@ -124,7 +124,7 @@ void CPsfLoader::LoadPs2(CPsfVm& virtualMachine, const CPsfPathToken& filePath, 
 
 	{
 		auto execPath = std::string(g_psfDeviceName) + ":/psf2.irx";
-		auto moduleId = bios->LoadModule(execPath.c_str());
+		auto moduleId = bios->LoadModuleFromPath(execPath.c_str());
 		assert(moduleId >= 0);
 		bios->StartModule(moduleId, execPath.c_str(), nullptr, 0);
 	}
