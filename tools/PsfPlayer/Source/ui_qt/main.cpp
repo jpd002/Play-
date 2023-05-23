243df31d88d43f836aeabd1bd06761b6003ca3e6
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2023-05-23 11:55:07 -0400

    PsfPlayer: Enable DPI scaling on Qt.

--- tools/PsfPlayer/Source/ui_qt/main.cpp
+++ tools/PsfPlayer/Source/ui_qt/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char* argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 	QApplication a(argc, argv);
 
 	QCommandLineParser parser;
