2a761f07771c4b916f37d4eee45a937bfbb397b2
Aynshe <1908827+Aynshe@users.noreply.github.com>
2023-08-24 22:31:44 +0200

    Update AppConfig.cpp
    Add a "portable.txt" file to the root of Play to use the "Play Data Files" folder in the program folder. Or change the folder name and/or path by editing "portable.txt.

--- Source/AppConfig.cpp
+++ Source/AppConfig.cpp
@@ -1,8 +1,12 @@
 #include "AppConfig.h"
 #include "PathUtils.h"
+#include <iostream>
+#include <fstream>    // Pour la lecture du fichier texte
+#include <filesystem> // Pour les opérations sur le système de fichiers
 
-#define BASE_DATA_PATH ("Play Data Files")
 #define CONFIG_FILENAME ("config.xml")
+#define PORTABLE_BASE_DATA_PATH ("Play Data Files")
+#define BASE_DATA_PATH ("Play Data Files")
 
 CAppConfig::CAppConfig()
     : CConfig(BuildConfigPath())
@@ -11,13 +15,56 @@ CAppConfig::CAppConfig()
 
 Framework::CConfig::PathType CAppConfig::GetBasePath()
 {
-	auto result = Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH;
-	return result;
+	// Check the presence of the "portable.txt" file
+	if(std::filesystem::exists("portable.txt"))
+	{
+		// Read the "portable.txt" text file to get the base path
+		std::ifstream configFile("portable.txt");
+		std::string basePath;
+
+		if(configFile.is_open())
+		{
+			std::getline(configFile, basePath);
+			configFile.close();
+		}
+
+		// If "portable.txt" is empty, use "Play Data Files" in the application folder
+		if(basePath.empty())
+		{
+			// Just use the path specified in PORTABLE_BASE_DATA_PATH
+			basePath = PORTABLE_BASE_DATA_PATH;
+		}
+		else
+		{
+			// If "portable.txt" contains a path, use this path
+// relative or absolute path
+			if(basePath[0] == '\\' || basePath[0] == '/')
+			{
+				// Remove the first character if it is a path separator
+				basePath.erase(0, 1);
+			}
+		}
+
+		// Create the "Play Data Files" directory if it does not already exist
+		std::filesystem::create_directories(basePath);
+
+		return basePath; // Retournez le chemin de base sans le chemin complet
+	}
+	else
+	{
+		// If "portable.txt" is missing, use original code for base directory path
+		auto result = Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH;
+
+		// Create the "Play Data Files" directory if it does not already exist
+		Framework::PathUtils::EnsurePathExists(result);
+
+		return result;
+	}
 }
 
 Framework::CConfig::PathType CAppConfig::BuildConfigPath()
 {
-	auto userPath(GetBasePath());
-	Framework::PathUtils::EnsurePathExists(userPath);
-	return userPath / CONFIG_FILENAME;
+	auto basePath(GetBasePath());
+	// "config.xml" is located in the base path directory
+	return basePath / CONFIG_FILENAME;
 }
