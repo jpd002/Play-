0679518decdcd130e4475f9eb82ab886daab0b16
Aynshe <1908827+Aynshe@users.noreply.github.com>
2023-08-28 16:55:00 +0200

    Update AppConfig.cpp - portable mode
    Add a "portable.txt" file to the root of Play to use the "Play Data Files" folder in the program folder.

--- Source/AppConfig.cpp
+++ Source/AppConfig.cpp
@@ -1,8 +1,11 @@
 #include "AppConfig.h"
 #include "PathUtils.h"
+#include <iostream>
+#include <filesystem> // For file system operations
 
-#define BASE_DATA_PATH ("Play Data Files")
 #define CONFIG_FILENAME ("config.xml")
+#define PORTABLE_BASE_DATA_PATH ("Play Data Files")
+#define BASE_DATA_PATH ("Play Data Files")
 
 CAppConfig::CAppConfig()
     : CConfig(BuildConfigPath())
@@ -11,13 +14,34 @@ CAppConfig::CAppConfig()
 
 Framework::CConfig::PathType CAppConfig::GetBasePath()
 {
-	auto result = Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH;
-	return result;
+	// Check for the presence of the "portable.txt" file
+	if(std::filesystem::exists("portable.txt"))
+	{
+		//delete "read content portable.txt"
+		
+		// If "portable.txt" is present, simply use the path specified in PORTABLE_BASE_DATA_PATH
+		auto basePath = PORTABLE_BASE_DATA_PATH;
+
+		// Create the "Play Data Files" directory if it doesn't already exist
+		std::filesystem::create_directories(basePath);
+
+		return basePath; // Return the base path without the complete path
+	}
+	else
+	{
+		// If "portable.txt" is absent, use the original code for the base directory path
+		auto result = Framework::PathUtils::GetPersonalDataPath() / BASE_DATA_PATH;
+
+		// Create the "Play Data Files" directory if it doesn't already exist
+		Framework::PathUtils::EnsurePathExists(result);
+
+		return result;
+	}
 }
 
 Framework::CConfig::PathType CAppConfig::BuildConfigPath()
 {
-	auto userPath(GetBasePath());
-	Framework::PathUtils::EnsurePathExists(userPath);
-	return userPath / CONFIG_FILENAME;
+	auto basePath(GetBasePath());
+	// "config.xml" is located in the directory of the base path
+	return basePath / CONFIG_FILENAME;
 }
