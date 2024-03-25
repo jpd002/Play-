a045334a966799990e42f579e3fc5dff39ef4617
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2024-03-25 13:57:40 -0400

    macOS: Use notarytool for notarization.

--- .ci.macos.notarize.sh
+++ .ci.macos.notarize.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH=$(dirname "$0")
 APP_PATH=$SCRIPT_PATH/build/Source/ui_qt/Release/Play.app
 ZIP_PATH=$SCRIPT_PATH/Play.zip
 ditto -ck --sequesterRsrc --keepParent $APP_PATH $ZIP_PATH
-xcrun altool --notarize-app -f $ZIP_PATH -t osx -u $MACOS_NOTARIZE_APPLEID_USERNAME -p $MACOS_NOTARIZE_APPLEID_PASSWORD --primary-bundle-id com.virtualapplications.play
+xcrun notarytool submit $ZIP_PATH --apple-id $MACOS_NOTARIZE_APPLEID_USERNAME --team-id $MACOS_NOTARIZE_TEAMID --password $MACOS_NOTARIZE_APPLEID_PASSWORD --wait
