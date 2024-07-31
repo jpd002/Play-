53c16881c04ff0526c4d92be035259895627f8d0
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2024-07-31 17:08:19 -0400

    Fix assert.

--- Source/ui_shared/arcadedrivers/NamcoSys246Driver.cpp
+++ Source/ui_shared/arcadedrivers/NamcoSys246Driver.cpp
@@ -133,7 +133,7 @@ void CNamcoSys246Driver::PrepareEnvironment(CPS2VM* virtualMachine, const ARCADE
 			for(const auto& buttonDefPair : def.buttons)
 			{
 				const auto& buttonPair = buttonDefPair.second;
-				assert(buttonDefPair.first == -1);
+				assert(buttonPair.first == -1);
 				namcoArcadeModule->SetButton(buttonDefPair.first, buttonPair.second);
 			}
 			switch(def.inputMode)
