87cc8c9137acee724058d1ce12fdfdf898e7036f
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2025-05-13 19:53:26 -0400

    Move dongle search logic in its own function.

--- Source/ui_shared/arcadedrivers/NamcoSys246Driver.h
+++ Source/ui_shared/arcadedrivers/NamcoSys246Driver.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+#include "Types.h"
 #include "ArcadeDriver.h"
 
 class CNamcoSys246Driver : public CArcadeDriver
@@ -7,4 +9,7 @@ class CNamcoSys246Driver : public CArcadeDriver
 public:
 	void PrepareEnvironment(CPS2VM*, const ARCADE_MACHINE_DEF&) override;
 	void Launch(CPS2VM*, const ARCADE_MACHINE_DEF&) override;
+
+private:
+	std::vector<uint8> ReadDongleData(const ARCADE_MACHINE_DEF&);
 };
