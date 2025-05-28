a824a3dcddb976065908c22cd4b8ba2781cce33a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2025-05-27 21:13:36 -0400

    Reset FPL alloc bitmap upon creation.

--- Source/iop/IopBios.cpp
+++ Source/iop/IopBios.cpp
@@ -2591,8 +2591,9 @@ uint32 CIopBios::CreateFpl(uint32 paramPtr)
 		return -1;
 	}
 
+	uint32 blocksSize = param->blockCount * param->blockSize;
 	uint32 bitmapSize = (param->blockCount + 7) / 8;
-	uint32 totalSize = (param->blockCount * param->blockSize) + bitmapSize;
+	uint32 totalSize = blocksSize + bitmapSize;
 
 	uint32 poolPtr = m_sysmem->AllocateMemory(totalSize, 0, 0);
 	if(poolPtr == 0)
@@ -2607,6 +2608,9 @@ uint32 CIopBios::CreateFpl(uint32 paramPtr)
 	fpl->blockCount = param->blockCount;
 	fpl->blockSize = param->blockSize;
 	fpl->poolPtr = poolPtr;
+
+	auto bitmap = m_ram + fpl->poolPtr + blocksSize;
+	memset(bitmap, 0, bitmapSize);
 
 	return fplId;
 }
