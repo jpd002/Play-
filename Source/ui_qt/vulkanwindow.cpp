c0a912dc2cb30f53b995c19b522024318cd015d9
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2025-11-26 17:44:20 -0500

    Disable Qt6's Metal Layer transaction system.

--- Source/ui_qt/vulkanwindow.cpp
+++ Source/ui_qt/vulkanwindow.cpp
@@ -4,6 +4,12 @@
 VulkanWindow::VulkanWindow(QWindow* parent)
     : OutputWindow(parent)
 {
+#ifdef __APPLE__
+	//Qt6's MetalLayer has a transaction system that will cause annoying
+	//hangs when resizing or changing the window's visibility.
+	//https://github.com/qt/qtbase/blob/master/src/plugins/platforms/cocoa/qnsview_drawing.mm#L104
+	setenv("QT_MTL_NO_TRANSACTION", "1", true);
+#endif
 	setSurfaceType(QWindow::VulkanSurface);
 #ifdef __APPLE__
 	m_metalLayer = GetLayerFromView(reinterpret_cast<void*>(winId()));
