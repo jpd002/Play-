fd734194cf4bc17d9654394fdff088dca2a57657
Dmamss <129612706+Dmamss@users.noreply.github.com>
2026-01-04 21:58:00 +0100

    Update AppDelegate.mm
    Update for ios26

--- Source/ui_ios/AppDelegate.mm
+++ Source/ui_ios/AppDelegate.mm
@@ -9,8 +9,23 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOption
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
+    [[StikDebugJitService sharedService] initialize];
+
+    if ([[StikDebugJitService sharedService] isJitActive]) {
+        [[StikDebugJitService sharedService] setEnvironmentForJIT];
+    }
+- (BOOL)application:(UIApplication*)app openURL:(NSURL*)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id>*)options
+{
+    // Handle StikDebug callback
+    if ([[StikDebugJitService sharedService] handleCallbackURL:url]) {
+        return YES;
+    }
+ 
+    
+    return NO;
+}	
 	[EmulatorViewController registerPreferences];
 	CGSH_OpenGL::RegisterPreferences();
 	return YES;
