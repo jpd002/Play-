883f59569800eaf620bc2fbfe41f6ee9a5263da9
Dmamss <129612706+Dmamss@users.noreply.github.com>
2026-01-04 22:46:07 +0100

    Add files via upload

---
+++ Source/ui_ios/PlayJIT-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PlayJIT-Bridging-Header.h
+//  Play! iOS
+//
+//  Bridging header for Swift access to JIT functions
+//
+
+#ifndef PlayJIT_Bridging_Header_h
+#define PlayJIT_Bridging_Header_h
+
+#import "PlayJIT.h"
+
+#endif /* PlayJIT_Bridging_Header_h */
